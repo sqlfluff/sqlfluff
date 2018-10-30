762eb26f6a33392dfde68ea2794e0bc665c3c93f
Alan Cruickshank <alanmcruickshank@gmail.com>
2018-10-30 23:38:47 +0000

    add multi whitespace test

--- sqlfluff_recursive_test.py
+++ sqlfluff_recursive_test.py
@@ -10,3 +10,15 @@ def test__recursive__basic():
     assert isinstance(res, ChunkString)
     assert len(res) == 2
     assert res[0].chunk == 'SELECT'
+
+
+def test__recursive__multi_whitespace():
+    rl = RecursiveLexer()
+    pc = PositionedChunk('    SELECT    \n', 0, 1)
+    res, context = rl.lex(pc)
+    assert isinstance(res, ChunkString)
+    assert len(res) == 3
+    assert res[0].content == 'whitespace'
+    assert res[1].chunk == 'SELECT'
+
+
