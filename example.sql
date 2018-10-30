67a6137f8060b94197abcf4cb0d071611a106520
Alan Cruickshank <alanmcruickshank@gmail.com>
2018-10-30 11:07:10 +0000

    Initial SQL Linting

---
+++ example.sql
@@ -0,0 +1,6 @@
+-- Example SQL File - should pass
+SELECT
+    u.id,
+    u.code
+FROM user AS u
+WHERE u.batch = 3
--- sqlfluff.py
+++ sqlfluff.py
@@ -1,15 +1,67 @@
 """ The Main file for SQLFluff """
 
 import click
+import os
+import re
+
+
+def ordinalise(s):
+    return [ord(c) for c in s]
+
+
+class ParsedLine(object):
+    whitespace_regex = re.compile('\s+')
+    def __init__(self, line):
+        self.initial_string = None
+        self.terminal_string = None
+        self.core_string = None
+        #click.echo(len(line))
+        # Iterate through whitespace matches
+        for m in self.whitespace_regex.finditer(line):
+            if m:
+                #click.echo(m)
+                #click.echo(repr(m.group()))
+                match_start, match_end = m.span()
+                if match_start == 0:
+                    #click.echo("Initial String")
+                    self.initial_string = m.group()
+                elif match_end == len(line):
+                    #click.echo("Terminal Match")
+                    self.terminal_string = m.group()
+        self.core_string = line[len(self.initial_string or '') : len(line) - len(self.terminal_string or '')]
+        #click.echo(self.core_string)
+    
+    def __repr__(self):
+        return '<Parsed Line - Core:%r, Initial=%r, Terminal=%r>' % (self.core_string, self.initial_string, self.terminal_string)
+
 
 @click.command()
-@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--name', prompt='Your name',
-              help='The person to greet.')
-def hello(count, name):
-    """Simple program that greets NAME for a total of COUNT times."""
-    for x in range(count):
-        click.echo('Hello %s!' % name)
+@click.option('--dialect', default='ansi', help='The dialect of SQL to lint')
+@click.argument('paths', nargs=-1)
+def sqlfluff_lint(dialect, paths):
+    """Lint SQL files"""
+    click.echo('Linting... [Dialect: {0}]'.format(dialect))
+    click.echo(paths)
+    if len(paths) == 0:
+        # No paths specified - assume local
+        paths = ('.',)
+    for path in paths:
+        click.echo('Linting: {0}'.format(path))
+        # Iterate through files recursively in the specified directory (if it's a directory)
+        # or read the file directly if it's not
+    click.echo("Loading the example file...")
+    for fname in ['example.sql', 'example-tab.sql']:
+        click.echo(fname)
+        with open(fname) as f:
+            for line in f:
+                #click.echo("##############")
+                #click.echo(line)
+                #click.echo("###")
+                #click.echo(ordinalise(line))
+                #click.echo("###")
+                obj = ParsedLine(line)
+                click.echo(obj)
+                
 
 if __name__ == '__main__':
-    hello()
+    sqlfluff_lint()
