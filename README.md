6a27b66f129f3d635be4123a833ec281f63f5aaf
Alan Cruickshank <alan@designingoverload.com>
2018-11-14 10:52:20 +0000

    normalise paths before testing. Added CircleCI badge

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # SqlFluff
 ## The SQL Linter for humans
 
+[![CircleCI](https://circleci.com/gh/alanmcruickshank/sqlfluff/tree/master.svg?style=svg)](https://circleci.com/gh/alanmcruickshank/sqlfluff/tree/master)
+
 Bored of not having a good SQL linter that works with whichever dialiect you're
 working with? Fluff is an extensible and modular linter designed to help you write
 good SQL and catch errors and bad SQL before it hits your database.
--- test/linter_test.py
+++ test/linter_test.py
@@ -4,26 +4,30 @@ from sqlfluff.linter import Linter
 
 
 # ############## LINTER TESTS
+
+def normalise_paths(paths):
+    # NB Paths on difference platforms might look different, so this makes them comparable
+    return set([pth.replace("/", '.').replace("\\", ".") for pth in paths])
+
+
 def test__linter__path_from_paths__dir():
     lntr = Linter()
     paths = lntr.paths_from_path('test/fixtures/lexer')
     # NB This test might fail on Linux or Mac - should probably correct...
-    assert paths == set(['test\\fixtures\\lexer\\block_comment.sql', 'test\\fixtures\\lexer\\inline_comment.sql', 'test\\fixtures\\lexer\\basic.sql'])
+    assert normalise_paths(paths) == set(['test.fixtures.lexer.block_comment.sql', 'test.fixtures.lexer.inline_comment.sql', 'test.fixtures.lexer.basic.sql'])
 
 
 def test__linter__path_from_paths__file():
     lntr = Linter()
     paths = lntr.paths_from_path('test/fixtures/linter/indentation_errors.sql')
-    # NB This test might fail on Linux or Mac - should probably correct...
-    assert paths == set(['test/fixtures/linter/indentation_errors.sql'])
+    assert normalise_paths(paths) == set(['test.fixtures.linter.indentation_errors.sql'])
 
 
 def test__linter__path_from_paths_dot():
     lntr = Linter()
     paths = lntr.paths_from_path('.')
-    # NB This test might fail on Linux or Mac - should probably correct...
     # Use set theory to check that we get AT LEAST these files
-    assert set(paths) >= set(['test\\fixtures\\lexer\\block_comment.sql', 'test\\fixtures\\lexer\\inline_comment.sql', 'test\\fixtures\\lexer\\basic.sql'])
+    assert normalise_paths(paths) >= set(['test.fixtures.lexer.block_comment.sql', 'test.fixtures.lexer.inline_comment.sql', 'test.fixtures.lexer.basic.sql'])
 
 
 def test__linter__lint_file_indentation():
