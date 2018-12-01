cb0dbd578a67de8a39b31b9b1349b1d63133c8bd
Alan Cruickshank <alan@designingoverload.com>
2018-12-01 23:18:31 +0000

    add a little more testing of ansi

--- test/parser_dialect_ansi_test.py
+++ test/parser_dialect_ansi_test.py
@@ -3,7 +3,14 @@
 from sqlfluff.parser.dialects import ansi
 
 
-def test__parser__dialect_ansi_simple():
+def test__parser__dialect_ansi_simple_a():
     root_node, _ = ansi.parse('SelECt sdfsaasd    FrOM   asdfawwwww   GroUP BY  asdfawwwww.fasfekjhsf   order BY  something_else    ')
     tkns = root_node.tokens()
     assert ('groupby', 'GroUP BY') in tkns
+
+
+def test__parser__dialect_ansi_simple_b():
+    root_node, r = ansi.parse('SelECt     \n colsdanjn_As_   \n  \t   FrOM     blah__   ')
+    tkns = root_node.tokens()
+    assert ('object_literal', 'colsdanjn_As_') in tkns
+    assert r.line_no == 3
