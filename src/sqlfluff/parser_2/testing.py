15036f6e66e8f583ffe29e91b44d0b9fd6f0af2e
Alan Cruickshank <alanmcruickshank@gmail.com>
2019-07-26 15:23:39 +0100

    works for statements and comments

--- src/sqlfluff/parser_2/testing.py
+++ src/sqlfluff/parser_2/testing.py
@@ -29,9 +29,15 @@ class BaseSegment(object):
     
     def __repr__(self):
         if self.segments:
-            return "<{0}: {1!s}>".format(self.__class__.__name__, segments)
+            return "<{0}: {1!s}>".format(self.__class__.__name__, self.segments)
         else:
             return "<{0}: {1!r}>".format(self.__class__.__name__, self.raw)
+
+    def reconstruct(self):
+        if self.segments:
+            return "".join([seg.reconstruct() for seg in self.segments])
+        else:
+            return self.raw
 
 
 class FileSegment(BaseSegment):
@@ -53,7 +59,11 @@ class FileSegment(BaseSegment):
             Token('#', None, '\n')
         ]
 
+        statement_seperators = [';']
+
+        last_statement_pos = 0
         last_pos = 0
+        statement_stack = []
         segment_stack = []
         comment_entry = None
         string_entry = None
@@ -66,34 +76,60 @@ class FileSegment(BaseSegment):
             if not comment_entry and not string_entry:
                 for s in string_tokens:
                     if forward.startswith(s.start):
-                        print("Found string start at pos {0}!".format(pos))
+                        print("Found string start at pos {0}! [{1!r}]".format(pos, forward[:5]))
                         string_entry = TokenMemory(pos, s)
                         # Don't store the segment, because we're only looking for
                         # strings at this stage so we can ignore "false" comments
-                        break
+                        continue
                 for c in comment_tokens:
                     if forward.startswith(c.start):
-                        print("Found comment start at pos {0}!".format(pos))
+                        print("Found comment start at pos {0}! [{1!r}]".format(pos, forward[:5]))
                         comment_entry = TokenMemory(pos, c)
                         # check that we have a segment to add
                         if pos - 1 > last_pos:
                             segment_stack.append(CodeSegment(raw[last_pos:pos]))
                             last_pos = pos
-                        break
+                        continue
+                for e in statement_seperators:
+                    if forward.startswith(e):
+                        # Ok we've found a statement ender, not in a comment or a string
+                        print("Found statement end at pos {0}! [{1!r}]".format(pos, forward[:5]))
+                        # We need to end the current code segment FIRST
+                        if pos - 1 > last_pos:
+                            segment_stack.append(CodeSegment(raw[last_pos:pos]))
+                            last_pos = pos
+                        etl = len(e)
+                        statement_stack.append(
+                            StatementSegment(
+                                raw[last_statement_pos:pos],
+                                segments=segment_stack))
+                        statement_stack.append(
+                            StatementSperatorSegment(e)
+                        )
+                        segment_stack = []
+                        last_statement_pos = pos + etl
+                        last_pos = pos + etl
+                        skip = etl
+                        continue
                 # print(raw[pos:])
             elif string_entry:
                 # We're in a string, just look for the end, of THIS KIND of string
                 if string_entry.token.end and forward.startswith(string_entry.token.end):
+                    print("Found string end at pos {0}! [{1!r}]".format(pos, forward[:5]))
                     # End of segment
                     # Don't save, remember, we're only looking for comments!
                     string_entry = None
+                    continue
                 elif string_entry.token.terminator and forward.startswith(string_entry.token.terminator):
+                    print("Found string terminator at pos {0}! [{1!r}]".format(pos, forward[:5]))
                     # End of segment
                     # Don't save, remember, we're only looking for comments!
                     string_entry = None
+                    continue
             elif comment_entry:
                 # We're in a string, just look for the end, of THIS KIND of string
                 if comment_entry.token.end and forward.startswith(comment_entry.token.end):
+                    print("Found comment end at pos {0}! [{1!r}]".format(pos, forward[:5]))
                     # End of segment
                     if pos - 1 > last_pos:
                         etl = len(comment_entry.token.end)
@@ -101,12 +137,15 @@ class FileSegment(BaseSegment):
                         last_pos = pos + etl
                         skip = etl
                     comment_entry = None
+                    continue
                 elif comment_entry.token.terminator and forward.startswith(comment_entry.token.terminator):
+                    print("Found comment terminator at pos {0}! [{1!r}]".format(pos, forward[:5]))
                     # End of segment
                     if pos - 1 > last_pos:
                         segment_stack.append(CommentSegment(raw[last_pos:pos]))
                         last_pos = pos
                     comment_entry = None
+                    continue
             else:
                 raise NotImplementedError("Aaargh!")
         # ok we got to the end, what did we finish in?
@@ -129,7 +168,29 @@ class FileSegment(BaseSegment):
         else:
             raise ValueError("Huh? eaiuyawren")
         
-        self.segments = segment_stack
+        if len(segment_stack) > 0:
+            # Let's just terminate this as a statement
+            statement_stack.append(
+                StatementSegment(
+                    raw[last_statement_pos:pos],
+                    segments=segment_stack))
+            segment_stack = []
+            last_statement_pos = 0
+            last_pos = 0
+        
+        self.segments = statement_stack
+        return self
+
+class StatementSperatorSegment(BaseSegment):
+    type = 'statement_seperator'
+
+    def parse(self):
+        return self
+
+class StatementSegment(BaseSegment):
+    type = 'statement'
+
+    def parse(self):
         return self
 
 class CodeSegment(BaseSegment):
@@ -175,3 +236,4 @@ if __name__ == "__main__":
     fs = FileSegment(raw)
     parsed = fs.parse()
     print(parsed.segments)
+    print(parsed.reconstruct())
