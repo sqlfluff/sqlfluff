e51bfe90487900680a464b0f6e294f7c9022a576
Barry Hart <barrywhart@yahoo.com>
2019-12-15 13:04:26 -0500

    Linter errors

--- test/diff_quality_plugin_test.py
+++ test/diff_quality_plugin_test.py
@@ -1,7 +1,10 @@
+"""Tests for the SQLFluff integration with the "diff-quality" tool."""
+
 from sqlfluff import diff_quality_plugin
 
 
 def test_diff_quality_plugin():
+    """Test the plugin at least finds errors on the expected lines."""
     violation_reporter = diff_quality_plugin.diff_cover_report_quality()
     violations = violation_reporter.violations(
         'test/fixtures/linter/indentation_errors.sql')
