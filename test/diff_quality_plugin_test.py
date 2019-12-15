d8d15b42210a13439e612f60161239993643809e
Barry Hart <barrywhart@yahoo.com>
2019-12-15 13:11:22 -0500

    Skip diff-quality test in Python 3.4

--- test/diff_quality_plugin_test.py
+++ test/diff_quality_plugin_test.py
@@ -1,8 +1,19 @@
 """Tests for the SQLFluff integration with the "diff-quality" tool."""
 
-from sqlfluff import diff_quality_plugin
+import sys
+
+import pytest
+
+try:
+    from sqlfluff import diff_quality_plugin
+except ImportError:
+    pass
 
 
+@pytest.mark.skipif(
+    sys.version_info[:2] == (3, 4),
+    reason="requires diff_cover package, which does not support python3.4"
+)
 def test_diff_quality_plugin():
     """Test the plugin at least finds errors on the expected lines."""
     violation_reporter = diff_quality_plugin.diff_cover_report_quality()
