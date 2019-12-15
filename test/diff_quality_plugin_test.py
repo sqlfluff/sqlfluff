f49a156a42d9ef54bb9f40903d05909f15d037fb
Barry Hart <barrywhart@yahoo.com>
2019-12-15 13:21:09 -0500

    Improve diff-quality test to get higher coverage

--- test/diff_quality_plugin_test.py
+++ test/diff_quality_plugin_test.py
@@ -10,17 +10,20 @@ except ImportError:
     pass
 
 
+@pytest.mark.parametrize("sql_path,expected_violations_lines", [
+    ('test/fixtures/linter/indentation_errors.sql', list(range(2, 7))),
+    ('test/fixtures/linter/parse_error.sql', {1}),
+])
 @pytest.mark.skipif(
     sys.version_info[:2] == (3, 4),
     reason="requires diff_cover package, which does not support python3.4"
 )
-def test_diff_quality_plugin():
+def test_diff_quality_plugin(sql_path, expected_violations_lines):
     """Test the plugin at least finds errors on the expected lines."""
     violation_reporter = diff_quality_plugin.diff_cover_report_quality()
-    violations = violation_reporter.violations(
-        'test/fixtures/linter/indentation_errors.sql')
+    violations = violation_reporter.violations(sql_path)
     assert isinstance(violations, list)
     assert len(violations) > 0
     violations_lines = {v.line for v in violations}
-    for line in range(2, 7):
-        assert line in violations_lines
+    for expected_line in expected_violations_lines:
+        assert expected_line in violations_lines
