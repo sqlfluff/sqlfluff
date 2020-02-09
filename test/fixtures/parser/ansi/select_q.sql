5d5fe52927f40baee34fce23f97f64dac16ed4e5
Alan Cruickshank <alanmcruickshank@gmail.com>
2020-02-09 23:14:12 +0000

    Fix for left joins after expressions

---
+++ test/fixtures/parser/ansi/select_q.sql
@@ -0,0 +1,14 @@
+-- Unexpected Join Fail
+-- https://github.com/alanmcruickshank/sqlfluff/issues/163
+SELECT
+    data.id
+FROM
+    data
+JOIN
+    data_max
+ON
+    data.event_id = data_max.event_id
+LEFT JOIN
+    "other_db"."other_data" AS od
+ON
+    od.fid = data_max.fid
