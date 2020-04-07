7de1b338f953f27a777e3c95b1af7f246f351417
Jon Klippenstein <jonklippenstein@improbable.io>
2020-04-07 11:26:06 -0600

    Allow escaped quotes and raw/regex strings in BigQuery dialect.

--- src/sqlfluff/dialects/dialect_bigquery.py
+++ src/sqlfluff/dialects/dialect_bigquery.py
@@ -2,6 +2,8 @@
 
 This inherits from the ansi dialect, with changes as specified by
 https://cloud.google.com/bigquery/docs/reference/standard-sql/query-syntax
+and
+https://cloud.google.com/bigquery/docs/reference/standard-sql/lexical#string_and_bytes_literals
 """
 
 from ..parser import (NamedSegment, OneOf, Ref)
@@ -10,6 +12,14 @@ from .dialect_ansi import ansi_dialect
 
 
 bigquery_dialect = ansi_dialect.copy_as('bigquery')
+
+bigquery_dialect.patch_lexer_struct([
+    # Quoted literals can have r or b (case insensitive) prefixes, in any order, to indicate a
+    # raw/regex string or byte sequence, respectively.  Allow escaped quote characters inside
+    # strings by allowing \" with an optional even multiple of backslashes in front of it.
+    ("single_quote", "regex", r"([rR]?[bB]?|[bB]?[rR]?)?'((?<!\\)(\\{2})*\\'|[^'])*(?<!\\)(\\{2})*'", dict(is_code=True)),
+    ("double_quote", "regex", r'([rR]?[bB]?|[bB]?[rR]?)?"((?<!\\)(\\{2})*\\"|[^"])*(?<!\\)(\\{2})*"', dict(is_code=True))
+])
 
 bigquery_dialect.add(
     DoubleQuotedLiteralSegment=NamedSegment.make('double_quote', name='literal', type='quoted_literal')
