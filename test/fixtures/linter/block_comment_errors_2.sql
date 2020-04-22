af9a904538b0d44bae4752d6540a497a976d7f41
Katzmann1983 <jens@humrich.de>
2020-04-22 11:40:12 +0200

    Comment parsing inital commit

---
+++ test/fixtures/linter/block_comment_errors_2.sql
@@ -0,0 +1,11 @@
+--Multi-line comments start with /* and end with */.
+--
+--Any text between /* and */ will be ignored.
+/***/
+/* * */
+/*
+*/
+-- Unclosed comments:
+--/*** */ */
+--/***/ */
+--/*/**/*/
