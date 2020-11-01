a3d4f2613c75233e6312b2ab9719d645d2d22670
Alan <alanmcruickshank@gmail.com>
2020-11-01 16:11:00 +0000

    mypy on Matchable

--- src/sqlfluff/core/parser/matchable.py
+++ src/sqlfluff/core/parser/matchable.py
@@ -1,24 +1,22 @@
 """The definition of a matchable interface."""
 
 from abc import ABC
-from typing import Tuple, TYPE_CHECKING
+from typing import Tuple, Optional, TYPE_CHECKING
 
 
 if TYPE_CHECKING:
     from .context import ParseContext
+    from .match_result import MatchResult
 
 
 class Matchable(ABC):
     """A base object defining the matching interface."""
 
-    def is_optional(self):
+    def is_optional(self) -> bool:
         """Return whether this element is optional."""
-        pass
 
-    def simple(self, parse_context: "ParseContext"):
+    def simple(self, parse_context: "ParseContext") -> Optional[str]:
         """Try to obtain a simple response from the matcher."""
-        pass
 
-    def match(self, segments: Tuple, parse_context: "ParseContext"):
+    def match(self, segments: Tuple, parse_context: "ParseContext") -> MatchResult:
         """Match against this matcher."""
-        pass
