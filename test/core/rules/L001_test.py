25346c7d68e680f52433d2f8e906d7e28719b897
NiallRees <niall@niallrees.com>
2020-11-15 23:09:24 +0000

    Introduce RuleTestCase

--- test/core/rules/L001_test.py
+++ test/core/rules/L001_test.py
@@ -1,25 +1,24 @@
-# flake8: noqa
 """Tests for L001."""
-from .std_test import rules__test_helper
+import pytest
+
+from .std_test import rules__test_helper, RuleTestCase
+
+test_cases = []
+
+test_cases.append(
+    RuleTestCase(
+        desc="Test unnecessary whitespace is removed",
+        fail_str="""
+        SELECT 1     
+        """,  # noqa W291 so we keep the extra spaces
+        fix_str="""
+        SELECT 1
+        """,
+    )
+)
 
 
-test_cases = {
-    "pass": [],
-    "fail": [
-        {
-            "query":
-"""
-SELECT 1     
-""",
-            "fixed":
-"""
-SELECT 1
-""",
-            "configs": None
-        }
-    ],
-}
-
-
-def test__rule_L001():
-    rules__test_helper("L001", test_cases)
+@pytest.mark.parametrize("test_case", test_cases, ids=[t.desc for t in test_cases])
+def test__rule_L001(test_case):
+    """Run the tests."""
+    rules__test_helper("L001", test_case)
