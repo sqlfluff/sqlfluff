68d1ce738a55a02890b9c7214dc6efedfbf9bdd8
Alan <alanmcruickshank@gmail.com>
2020-11-21 17:40:00 +0000

    Rename a load of the tests

--- test/fixtures/linter/autofix/bigquery/003_templating/before.sql
+++ test/fixtures/linter/autofix/bigquery/003_templating/before.sql
@@ -1,23 +1,28 @@
 -- A subset of the hairy test.
 -- NOTE: This is not perfect, but reflects
 -- functionality as at Nov 2020. In future
--- the logic should be updated to lit this
+-- the logic should be updated to lint this
 -- better.
+
+-- Force indentation linting.
+-- sqlfluff: indentation: template_blocks_indent: force
+-- sqlfluff: rules: L003: lint_templated_tokens: True
+
 SELECT
     {{corr_states}}
     {% for action in considered_actions %}
-        , {{metric}}_{{action}}
-        , campaign_count_{{action}}
+    ,{{metric}}_{{action}}
+    ,campaign_count_{{action}}
     {% endfor %}
-FROM
+  FROM
     {% for action in considered_actions %}
     {% if loop.first %}
     {{action}}_raw_effect_sizes
     {% else %}
-    JOIN
-        {{action}}_raw_effect_sizes
-        USING
-            ({{corr_states}})
-{% endif %}
+  JOIN
+    {{action}}_raw_effect_sizes
+  USING
+    ({{corr_states}})
+    {% endif %}
     {% endfor %}
 CROSS JOIN action_states
