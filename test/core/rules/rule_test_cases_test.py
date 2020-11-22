e26a042aaf11cc44e5f2ac386be7bc44e5394ea1
NiallRees <niall@niallrees.com>
2020-11-22 02:18:32 +0000

    :card_file_box: Split up rule tests into yml files (#553)

---
+++ test/core/rules/rule_test_cases_test.py
@@ -0,0 +1,32 @@
+"""Runs the rule test cases."""
+import os
+import glob
+
+import pytest
+import oyaml as yaml
+
+from .std_test import rules__test_helper, RuleTestCase
+
+ids = []
+test_cases = []
+
+test_cases_path = os.path.join(
+    os.path.abspath(os.path.dirname(__file__)), "test_cases", "*.yml"
+)
+
+for path in sorted(glob.glob(test_cases_path)):
+    with open(path) as f:
+        raw = f.read()
+
+    y = yaml.safe_load(raw)
+
+    rule = y["rule"]
+    named_tests = {k: v for k, v in y.items() if k != "rule"}
+    ids.extend([rule + "_" + t for t in named_tests])
+    test_cases.extend([RuleTestCase(rule=rule, **v) for k, v in named_tests.items()])
+
+
+@pytest.mark.parametrize("test_case", test_cases, ids=ids)
+def test__rule_test_case(test_case):
+    """Run the tests."""
+    rules__test_helper(test_case)
