a3fa3155b3b1f745c4752e2c824a0dc98102fc20
Paul Wildenhain <wildenhaip@chop.edu>
2020-11-27 10:11:04 -0500

    :mag: Add typing for linter module

--- src/sqlfluff/core/rules/__init__.py
+++ src/sqlfluff/core/rules/__init__.py
@@ -1,10 +1,10 @@
 """init py for the new rules crawlers."""
 
 from .std import std_rule_set
-from .base import rules_logger  # noqa
+from .base import RuleSet, rules_logger  # noqa
 
 
-def get_ruleset(name="standard"):
+def get_ruleset(name: str = "standard") -> RuleSet:
     """Get a ruleset by name."""
     lookup = {std_rule_set.name: std_rule_set}
     # Return a copy in case someone modifies the register.
