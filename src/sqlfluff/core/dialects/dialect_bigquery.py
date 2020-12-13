6ae041101d2ac1dab3510ea9390b4acbf84a4a5b
Daniel Mateus Pires <dpires@earnestresearch.com>
2020-12-13 19:01:06 +0000

    Lint

--- src/sqlfluff/core/dialects/dialect_bigquery.py
+++ src/sqlfluff/core/dialects/dialect_bigquery.py
@@ -16,7 +16,6 @@ from ..parser import (
     Delimited,
     ReSegment,
     AnyNumberOf,
-    Anything,
     KeywordSegment,
     Indent,
 )
@@ -57,7 +56,7 @@ bigquery_dialect.add(
     DoubleQuotedLiteralSegment=NamedSegment.make(
         "double_quote", name="quoted_literal", type="literal", trim_chars=('"',)
     ),
-    StructKeywordSegment=KeywordSegment.make('struct', name="struct"),
+    StructKeywordSegment=KeywordSegment.make("struct", name="struct"),
 )
 
 # Add additional datetime units
@@ -89,25 +88,28 @@ class SelectClauseSegment(AnsiSelectClauseSegment):
     """In BigQuery, select * as struct is valid."""
 
     parse_grammar = Sequence(
-        'SELECT',
-        Ref('SelectClauseModifierSegment', optional=True),
+        "SELECT",
+        Ref("SelectClauseModifierSegment", optional=True),
         Indent,
         OneOf(
             Sequence(
-                'AS',
-                'STRUCT',
-                Ref('StarSegment'),
-                Ref('StarModifierSegment', optional=True),
+                "AS",
+                "STRUCT",
+                Ref("StarSegment"),
+                Ref("StarModifierSegment", optional=True),
             ),
             Delimited(
-                Ref('SelectTargetElementSegment'),
-                delimiter=Ref('CommaSegment'),
-                allow_trailing=True
+                Ref("SelectTargetElementSegment"),
+                delimiter=Ref("CommaSegment"),
+                allow_trailing=True,
             ),
         ),
     )
 
+
 class SelectTargetElementSegment(AnsiSelectTargetElementSegment):
+    """BigQuery also supports the special "Struct" construct."""
+
     parse_grammar = OneOf(
         # *, blah.*, blah.blah.*, etc.
         Ref("WildcardExpressionSegment"),
@@ -124,6 +126,7 @@ class SelectTargetElementSegment(AnsiSelectTargetElementSegment):
             Ref("AliasExpressionSegment", optional=True),
         ),
     )
+
 
 bigquery_dialect.replace(
     QuotedIdentifierSegment=NamedSegment.make(
@@ -146,7 +149,10 @@ bigquery_dialect.replace(
     ),
     FunctionNameSegment=ReSegment.make(
         # In BigQuery struct() has a special syntax, so we don't treat it as a function
-        r"[A-Z][A-Z0-9_]*", name="function_name", type="function_name", _anti_template=r"STRUCT"
+        r"[A-Z][A-Z0-9_]*",
+        name="function_name",
+        type="function_name",
+        _anti_template=r"STRUCT",
     ),
     SelectTargetElementSegment=SelectTargetElementSegment,
     SelectClauseSegment=SelectClauseSegment,
@@ -204,32 +210,32 @@ class ReplaceClauseSegment(BaseSegment):
     )
 
 
-
 @bigquery_dialect.segment()
 class StructSegment(BaseSegment):
     """Container of ordered fields each with a type (required) and field name (optional).
 
     https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types#constructing_a_struct
     """
-    type = 'struct'
+
+    type = "struct"
     match_grammar = Sequence(
-        'STRUCT',
+        "STRUCT",
         Bracketed(
             Delimited(
                 AnyNumberOf(
                     Sequence(
                         OneOf(
-                            Ref('LiteralGrammar'),
-                            Ref('FunctionSegment'),
-                            Ref('IntervalExpressionSegment'),
-                            Ref('ObjectReferenceSegment'),
-                            Ref('ExpressionSegment')
+                            Ref("LiteralGrammar"),
+                            Ref("FunctionSegment"),
+                            Ref("IntervalExpressionSegment"),
+                            Ref("ObjectReferenceSegment"),
+                            Ref("ExpressionSegment"),
                         ),
-                        Ref('AliasExpressionSegment', optional=True)
+                        Ref("AliasExpressionSegment", optional=True),
                     ),
                 ),
-                delimiter=Ref('CommaSegment'),
+                delimiter=Ref("CommaSegment"),
             ),
             optional=True,
-        )
+        ),
     )
--- src/sqlfluff/core/parser/grammar/not_exist.py
+++ src/sqlfluff/core/parser/grammar/not_exist.py
@@ -1,5 +1,8 @@
+"""NotExist grammar."""
+
 from .base import BaseGrammar
 from sqlfluff.core.parser.match_result import MatchResult
+
 
 class NotExist(BaseGrammar):
     """Lookahead/lookbehind which matches when the target does not match and vice-versa.
@@ -7,20 +10,14 @@ class NotExist(BaseGrammar):
     The Not grammar always returns segments as they were before the match() function ran.
     In other words, it does not "consume" segments.
     """
-    def expected_string(self, dialect=None, called_from=None):
-        return self.target.expected_string(dialect=dialect, called_from=called_from)
 
     def __init__(self, target, *args, **kwargs):
         self.target = self._resolve_ref(target)
         super(NotExist, self).__init__(*args, **kwargs)
 
-
     def match(self, segments, parse_context):
-        """Negative match."""
-        match_result = self.target.match(
-            segments,
-            parse_context
-        )
+        """Uses the target matcher and negates the result while leaving segments untouched."""
+        match_result = self.target.match(segments, parse_context)
 
         if match_result.matched_segments:
             return MatchResult.from_unmatched(segments)
