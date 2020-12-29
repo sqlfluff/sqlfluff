9bdad4665fbcbeed4de874eee9cca30dfecb2c7a
Barry Hart <barry.hart@mailchimp.com>
2020-12-29 16:51:15 -0500

    Simplify Postgres UDFs to what the grammar can currently handle

--- test/fixtures/parser/postgres/postgres_create_function.sql
+++ test/fixtures/parser/postgres/postgres_create_function.sql
@@ -2,13 +2,10 @@
 
 CREATE FUNCTION add(integer, integer) RETURNS integer
     AS 'select $1 + $2;'
-    LANGUAGE SQL
-    IMMUTABLE
-    RETURNS NULL ON NULL INPUT;
+    LANGUAGE SQL;
 
--- This one is particularly difficult because of the semicolons within the statement.
-CREATE OR REPLACE FUNCTION increment(i integer) RETURNS integer AS $$
-        BEGIN
-                RETURN i + 1;
-        END;
-$$ LANGUAGE plpgsql;
+CREATE OR REPLACE FUNCTION increment(i integer) RETURNS integer AS '
+    BEGIN
+        RETURN i + 1;
+    END;
+' LANGUAGE plpgsql;
