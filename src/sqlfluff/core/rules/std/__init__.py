48c640e478f68b2be9842f9ede1540b74060eac6
NiallRees <niall@niallrees.com>
2021-01-17 15:59:34 +0000

    Use import_module and add rule class to globals()

--- src/sqlfluff/core/rules/std/__init__.py
+++ src/sqlfluff/core/rules/std/__init__.py
@@ -3,12 +3,18 @@
 # noqa
 
 import os
+from importlib import import_module
 from glob import glob
 
 rules_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "L*.py")
 
 for module in sorted(glob(rules_path)):
     # Manipulate the module path to extract the filename without the .py
-    rule = module.split("/")[-1][:-3]
-    cmd = f"from sqlfluff.core.rules.std.{rule} import Rule_{rule}"
-    exec(cmd)
+    rule_id = module.split("/")[-1][:-3]
+    rule_class_name = f"Rule_{rule_id}"
+    rule_class = getattr(
+        import_module(f"sqlfluff.core.rules.std.{rule_id}"), rule_class_name
+    )
+    # Add the rule_classes to the module namespace to be imported in
+    # sqlfluff/core/rules/__init__.py
+    globals()[rule_class_name] = rule_class
