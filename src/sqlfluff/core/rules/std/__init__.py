38f717fcb7f4850c338708494d448935f75106fe
NiallRees <niall@niallrees.com>
2021-01-17 20:44:30 +0000

    Give helpful error message when rule name does not match L* format

--- src/sqlfluff/core/rules/std/__init__.py
+++ src/sqlfluff/core/rules/std/__init__.py
@@ -14,9 +14,12 @@ for module in sorted(glob(rules_path)):
     rule_id = os.path.splitext(os.path.basename(module))[0]
     # All rule classes are expected in the format of Rule_L*
     rule_class_name = f"Rule_{rule_id}"
-    rule_class = getattr(
-        import_module(f"sqlfluff.core.rules.std.{rule_id}"), rule_class_name
-    )
+    try:
+        rule_class = getattr(
+            import_module(f"sqlfluff.core.rules.std.{rule_id}"), rule_class_name
+        )
+    except ImportError:
+        raise (ImportError("Rule classes must be named in the format of L*."))
     # Add the rule_classes to the module namespace with globals() to be imported in
     # sqlfluff/core/rules/__init__.py
     # Sphinx automodule documentation looks at this module (see rules.rst) so
