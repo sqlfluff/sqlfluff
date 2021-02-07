9825dfb6b6bd460e45acb19cd2ceab495c0702f3
Barry Hart <barry.hart@mailchimp.com>
2021-02-07 14:30:14 -0500

    Refactor the code to trigger the rule in a cleaner way, add test case for fix

--- src/sqlfluff/core/rules/std/L015.py
+++ src/sqlfluff/core/rules/std/L015.py
@@ -1,6 +1,6 @@
 """Implementation of Rule L015."""
 
-from ..base import BaseCrawler, LintResult
+from ..base import BaseCrawler, LintFix, LintResult
 
 
 class Rule_L015(BaseCrawler):
@@ -30,10 +30,27 @@ class Rule_L015(BaseCrawler):
 
         Look for DISTINCT keyword immediately followed by open parenthesis.
         """
-        # We only trigger on start_bracket (open parenthesis)
-        if segment.name == "start_bracket":
-            filt_raw_stack = self.filter_meta(raw_stack)
-            if len(filt_raw_stack) > 0 and filt_raw_stack[-1].name == "DISTINCT":
-                # If we find DISTINCT followed by open_bracket, then bad.
-                return LintResult(anchor=segment)
-        return LintResult()
+        # We only trigger on expressions that begin with start_bracket (open
+        # parenthesis)
+        if segment.type == "expression":
+            segments_filt = self.filter_meta(segment.segments)
+            if len(segments_filt) and segments_filt[0].type == "start_bracket" and segments_filt[0].raw == "(":
+                filt_raw_stack = self.filter_meta(raw_stack)
+                if len(filt_raw_stack) and filt_raw_stack[-1].name == "DISTINCT":
+                    # If we find open_bracket immediately following DISTINCT,
+                    # then bad.
+                    fixes = []
+                    if len(segments_filt) >= 2:
+                        # Insert a single space after the open parenthesis being
+                        # removed.
+                        insert_str = " "
+                        fixes.append(LintFix("create", self.filter_meta(segment.segments)[1], [self.make_whitespace(
+                                    raw=insert_str, pos_marker=segment.pos_marker.advance_by(insert_str)
+                                )]))
+                    # Remove the parentheses.
+                    fixes += [
+                        LintFix("delete", segments_filt[0]),
+                        LintFix("delete", segments_filt[-1])
+                    ]
+                    return LintResult(anchor=segment, fixes=fixes)
+        return None
