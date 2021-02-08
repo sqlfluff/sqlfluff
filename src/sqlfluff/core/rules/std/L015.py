277c7de20823bed89abb527d6a951fdc1d03ab4b
Barry Hart <barry.hart@mailchimp.com>
2021-02-08 08:46:38 -0500

    PR review, tidying, performance

--- src/sqlfluff/core/rules/std/L015.py
+++ src/sqlfluff/core/rules/std/L015.py
@@ -32,26 +32,29 @@ class Rule_L015(BaseCrawler):
         """
         # We only trigger on expressions that begin with start_bracket (open
         # parenthesis)
-        if segment.type == "expression":
-            segments_filt = self.filter_meta(segment.segments)
-            if (
-                len(segments_filt)
-                and segments_filt[0].type == "start_bracket"
-                and segments_filt[0].raw == "("
-            ):
-                filt_raw_stack = self.filter_meta(raw_stack)
-                if len(filt_raw_stack) and filt_raw_stack[-1].name == "DISTINCT":
+        raw_stack_filtered = self.filter_meta(raw_stack)
+        if raw_stack_filtered and raw_stack_filtered[-1].name == "DISTINCT":
+            if segment.type == "expression":
+                segments_filtered = self.filter_meta(segment.segments)
+                if (
+                    segments_filtered
+                    and segments_filtered[0].raw == "("
+                    and segments_filtered[0].type == "start_bracket"
+                ):
                     # If we find open_bracket immediately following DISTINCT,
                     # then bad.
                     fixes = []
-                    if len(segments_filt) >= 2:
+                    # :TRICKY: Check for length > 2 because the "create" LintFix
+                    # is relative to segments_filtered[1], so this code would
+                    # throw an IndexError if somehow the length were < 2.
+                    if len(segments_filtered) >= 2:
                         # Insert a single space after the open parenthesis being
                         # removed.
                         insert_str = " "
                         fixes.append(
                             LintFix(
                                 "create",
-                                self.filter_meta(segment.segments)[1],
+                                self.filter_meta(segments_filtered)[1],
                                 [
                                     self.make_whitespace(
                                         raw=insert_str,
@@ -64,8 +67,8 @@ class Rule_L015(BaseCrawler):
                         )
                     # Remove the parentheses.
                     fixes += [
-                        LintFix("delete", segments_filt[0]),
-                        LintFix("delete", segments_filt[-1]),
+                        LintFix("delete", segments_filtered[0]),
+                        LintFix("delete", segments_filtered[-1]),
                     ]
                     return LintResult(anchor=segment, fixes=fixes)
         return None
