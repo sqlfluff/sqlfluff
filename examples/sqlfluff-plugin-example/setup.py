60d31bf03fbf29f0d2dbfb7044cac9b9ba269a2a
Daniel Mateus Pires <dpires@earnestresearch.com>
2021-02-12 22:19:19 +0000

    Add host plugin tests and change plugin package structure to follow sqlfluff's own 👨‍🔬

--- examples/sqlfluff-plugin-example/setup.py
+++ examples/sqlfluff-plugin-example/setup.py
@@ -3,9 +3,13 @@ from setuptools import setup
 
 setup(
     name="sqlfluff-plugin-example",
+    include_package_data=True,
+    package_dir={"": "src"},
+    packages=[
+        "example",
+    ],
     # TODO: Change me to the first release
     # that includes the pluggy work.
     install_requires="sqlfluff>=0.4.0a3",
-    entry_points={"sqlfluff": ["example = example"]},
-    py_modules=["example"],
+    entry_points={"sqlfluff": ["example = example.rules"]},
 )
