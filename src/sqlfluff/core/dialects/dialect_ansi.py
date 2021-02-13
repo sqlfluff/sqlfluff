190779c4c5182979d4fd7ad7e29d7af0be987f79
Alan <alanmcruickshank@gmail.com>
2021-02-13 10:49:33 +0000

    Fix parsing error with no spaces #607

--- src/sqlfluff/core/dialects/dialect_ansi.py
+++ src/sqlfluff/core/dialects/dialect_ansi.py
@@ -445,6 +445,7 @@ class ObjectReferenceSegment(BaseSegment):
         Ref("SingleIdentifierGrammar"),
         delimiter=OneOf(Ref("DotSegment"), Sequence(Ref("DotSegment"))),
         terminator=OneOf(
+            "ON", "AS", "USING",
             Ref("CommaSegment"),
             Ref("CastOperatorSegment"),
             Ref("StartSquareBracketSegment"),
