e070d58639ce853f6c3cf5e8a230f2e32a8455ab
Barry Hart <barry.hart@mailchimp.com>
2021-02-15 14:21:00 -0500

    Issue 770: Detect undocumented fix-compatible rules

--- test/core/rules/rule_test_cases_test.py
+++ test/core/rules/rule_test_cases_test.py
@@ -5,7 +5,9 @@ from glob import glob
 import pytest
 import oyaml as yaml
 
-from .std_test import rules__test_helper, RuleTestCase
+from sqlfluff.core.config import FluffConfig
+from sqlfluff.core.rules.doc_decorators import is_fix_compatible
+from .std_test import get_rule_from_set, rules__test_helper, RuleTestCase
 
 ids = []
 test_cases = []
@@ -28,4 +30,10 @@ for path in sorted(glob(test_cases_path)):
 @pytest.mark.parametrize("test_case", test_cases, ids=ids)
 def test__rule_test_case(test_case):
     """Run the tests."""
-    rules__test_helper(test_case)
+    res = rules__test_helper(test_case)
+    if res is not None and res != test_case.fail_str:
+        cfg = FluffConfig(configs=test_case.configs)
+        rule = get_rule_from_set(test_case.rule, config=cfg)
+        assert is_fix_compatible(
+            rule
+        ), f'Rule {test_case.rule} returned fixes but does not specify "@document_fix_compatible".'
