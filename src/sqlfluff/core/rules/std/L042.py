4fc1209fb8ac63da27188879aab063b0a71d47ee
Alan <alanmcruickshank@gmail.com>
2021-02-19 19:33:17 +0000

    Extend L042 to optionally lint FROM clauses

--- src/sqlfluff/core/rules/std/L042.py
+++ src/sqlfluff/core/rules/std/L042.py
@@ -4,7 +4,11 @@ from sqlfluff.core.rules.base import BaseCrawler, LintResult
 
 
 class Rule_L042(BaseCrawler):
-    """Join clauses should not contain subqueries. Use CTEs instead.
+    """Join/From clauses should not contain subqueries. Use CTEs instead.
+
+    By default this rule is configured to allow subqueries within `FROM`
+    clauses but not within `JOIN` clauses. If you prefer a stricter lint
+    then this is configurable.
 
     NB: Some dialects don't allow CTEs, and for those dialects
     this rule makes no sense and should be disabled.
@@ -35,30 +39,43 @@ class Rule_L042(BaseCrawler):
 
     """
 
+    config_keywords = ["forbid_subquery_in"]
+
+    _config_mapping = {
+        "join": ["join_clause"],
+        "from": ["from_clause"],
+        "both": ["join_clause", "from_clause"],
+    }
+
     def _eval(self, segment, **kwargs):
-        """Join clauses should not contain subqueries. Use CTEs instead.
+        """Join/From clauses should not contain subqueries. Use CTEs instead.
 
         NB: No fix for this routine because it would be very complex to
         implement reliably.
         """
-        if segment.is_type("join_clause"):
-            # Get the referenced table segment
-            table_expression = segment.get_child("table_expression")
-            if not table_expression:
-                return None  # There isn't one. We're done.
-            # Get the main bit
-            table_expression = table_expression.get_child("main_table_expression")
-            if not table_expression:
-                return None  # There isn't one. We're done.
+        parent_types = self._config_mapping[self.forbid_subquery_in]
+        for parent_type in parent_types:
+            if segment.is_type(parent_type):
+                # Get the referenced table segment
+                table_expression = segment.get_child("table_expression")
+                if not table_expression:
+                    return None  # There isn't one. We're done.
+                # Get the main bit
+                table_expression = table_expression.get_child("main_table_expression")
+                if not table_expression:
+                    return None  # There isn't one. We're done.
 
-            # If any of the following are found, raise an issue.
-            # If not, we're fine.
-            problem_children = [
-                "with_compound_statement",
-                "set_expression",
-                "select_statement",
-            ]
-            for seg_type in problem_children:
-                seg = table_expression.get_child(seg_type)
-                if seg:
-                    return LintResult(anchor=seg)
+                # If any of the following are found, raise an issue.
+                # If not, we're fine.
+                problem_children = [
+                    "with_compound_statement",
+                    "set_expression",
+                    "select_statement",
+                ]
+                for seg_type in problem_children:
+                    seg = table_expression.get_child(seg_type)
+                    if seg:
+                        return LintResult(
+                            anchor=seg,
+                            description=f"{parent_type} clauses should not contain subqueries. Use CTEs instead",
+                        )
