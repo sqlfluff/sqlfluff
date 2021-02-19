4a9b73903ab611b3ff3bafac88bfecadc71e001c
Barry Hart <barry.hart@mailchimp.com>
2021-02-18 20:16:52 -0500

    Refactor L020 to expose the select statement analysis publicly

--- src/sqlfluff/core/rules/std/L020.py
+++ src/sqlfluff/core/rules/std/L020.py
@@ -1,8 +1,19 @@
 """Implementation of Rule L020."""
 
 import itertools
+from typing import List, NamedTuple, Optional
 
+from sqlfluff.core.dialects.base import Dialect
+from sqlfluff.core.dialects.dialect_ansi import AliasInfo
+from sqlfluff.core.parser.segments.base import BaseSegment
 from sqlfluff.core.rules.base import BaseCrawler, LintResult
+
+class SelectStatementColumnsAndTables(NamedTuple):
+    table_aliases: List[AliasInfo]
+    value_table_function_aliases: List[AliasInfo]
+    reference_buffer: List[BaseSegment]
+    col_aliases: List[str]
+    using_cols: List[str]
 
 
 class Rule_L020(BaseCrawler):
@@ -78,6 +89,80 @@ class Rule_L020(BaseCrawler):
                 value_table_function_aliases.append(alias_info)
         return table_aliases, value_table_function_aliases
 
+    @classmethod
+    def get_select_statement_info(cls, segment: BaseSegment, dialect: Optional[Dialect], early_exit: bool=True) -> Optional[SelectStatementColumnsAndTables]:
+        assert segment.is_type("select_statement")
+        table_aliases, value_table_function_aliases = cls._get_aliases_from_select(
+            segment, dialect
+        )
+        if early_exit and not table_aliases and not value_table_function_aliases:
+            return None
+
+        # Iterate through all the references, both in the select clause, but also
+        # potential others.
+        sc = segment.get_child("select_clause")
+        reference_buffer = list(sc.recursive_crawl("object_reference"))
+        # Add any wildcard references
+        reference_buffer += list(sc.recursive_crawl("wildcard_identifier"))
+        for potential_clause in (
+                "where_clause",
+                "groupby_clause",
+                "having_clause",
+                "orderby_clause",
+        ):
+            clause = segment.get_child(potential_clause)
+            if clause:
+                reference_buffer += list(
+                    clause.recursive_crawl("object_reference"))
+        # PURGE any references which are in nested select statements
+        for ref in reference_buffer.copy():
+            ref_path = segment.path_to(ref)
+            # is it in a subselect? i.e. a select which isn't this one.
+            if any(
+                    seg.is_type("select_statement") and seg is not segment
+                    for seg in ref_path
+            ):
+                reference_buffer.remove(ref)
+
+        # Get all column aliases
+        col_aliases = []
+        for col_seg in list(sc.recursive_crawl("alias_expression")):
+            for seg in col_seg.segments:
+                if seg.is_type("identifier"):
+                    col_aliases.append(seg.raw)
+
+        # Get any columns referred to in a using clause, and extract anything
+        # from ON clauses.
+        using_cols = []
+        fc = segment.get_child("from_clause")
+        for join_clause in fc.recursive_crawl("join_clause"):
+            in_using_brackets = False
+            seen_using = False
+            for seg in join_clause.segments:
+                if seg.is_type("keyword") and seg.name == "USING":
+                    seen_using = True
+                elif seg.is_type("join_on_condition"):
+                    for on_seg in seg.segments:
+                        if on_seg.is_type("expression"):
+                            # Deal with expressions
+                            reference_buffer += list(
+                                seg.recursive_crawl("object_reference")
+                            )
+                elif seen_using and seg.is_type("start_bracket"):
+                    in_using_brackets = True
+                elif seen_using and seg.is_type("end_bracket"):
+                    in_using_brackets = False
+                    seen_using = False
+                elif in_using_brackets and seg.is_type("identifier"):
+                    using_cols.append(seg.raw)
+
+        return SelectStatementColumnsAndTables(
+            table_aliases=table_aliases,
+            value_table_function_aliases=value_table_function_aliases,
+            reference_buffer=reference_buffer,
+            col_aliases=col_aliases,
+            using_cols=using_cols)
+
     def _eval(self, segment, parent_stack, **kwargs):
         """Get References and Aliases and allow linting.
 
@@ -88,68 +173,8 @@ class Rule_L020(BaseCrawler):
         `_lint_references_and_aliases` method.
         """
         if segment.is_type("select_statement"):
-            table_aliases, value_table_function_aliases = self._get_aliases_from_select(
-                segment, kwargs.get("dialect")
-            )
-            if not table_aliases and not value_table_function_aliases:
-                return None
-
-            # Iterate through all the references, both in the select clause, but also
-            # potential others.
-            sc = segment.get_child("select_clause")
-            reference_buffer = list(sc.recursive_crawl("object_reference"))
-            # Add any wildcard references
-            reference_buffer += list(sc.recursive_crawl("wildcard_identifier"))
-            for potential_clause in (
-                "where_clause",
-                "groupby_clause",
-                "having_clause",
-                "orderby_clause",
-            ):
-                clause = segment.get_child(potential_clause)
-                if clause:
-                    reference_buffer += list(clause.recursive_crawl("object_reference"))
-            # PURGE any references which are in nested select statements
-            for ref in reference_buffer.copy():
-                ref_path = segment.path_to(ref)
-                # is it in a subselect? i.e. a select which isn't this one.
-                if any(
-                    seg.is_type("select_statement") and seg is not segment
-                    for seg in ref_path
-                ):
-                    reference_buffer.remove(ref)
-
-            # Get all column aliases
-            col_aliases = []
-            for col_seg in list(sc.recursive_crawl("alias_expression")):
-                for seg in col_seg.segments:
-                    if seg.is_type("identifier"):
-                        col_aliases.append(seg.raw)
-
-            # Get any columns referred to in a using clause, and extract anything
-            # from ON clauses.
-            using_cols = []
-            fc = segment.get_child("from_clause")
-            for join_clause in fc.recursive_crawl("join_clause"):
-                in_using_brackets = False
-                seen_using = False
-                for seg in join_clause.segments:
-                    if seg.is_type("keyword") and seg.name == "USING":
-                        seen_using = True
-                    elif seg.is_type("join_on_condition"):
-                        for on_seg in seg.segments:
-                            if on_seg.is_type("expression"):
-                                # Deal with expressions
-                                reference_buffer += list(
-                                    seg.recursive_crawl("object_reference")
-                                )
-                    elif seen_using and seg.is_type("start_bracket"):
-                        in_using_brackets = True
-                    elif seen_using and seg.is_type("end_bracket"):
-                        in_using_brackets = False
-                        seen_using = False
-                    elif in_using_brackets and seg.is_type("identifier"):
-                        using_cols.append(seg.raw)
+            select_info = self.get_select_statement_info(segment, kwargs.get(
+                'dialect'))
 
             # Work out if we have a parent select function
             parent_select = None
@@ -161,11 +186,11 @@ class Rule_L020(BaseCrawler):
             # Pass them all to the function that does all the work.
             # NB: Subclasses of this rules should override the function below
             return self._lint_references_and_aliases(
-                table_aliases,
-                value_table_function_aliases,
-                reference_buffer,
-                col_aliases,
-                using_cols,
+                select_info.table_aliases,
+                select_info.value_table_function_aliases,
+                select_info.reference_buffer,
+                select_info.col_aliases,
+                select_info.using_cols,
                 parent_select,
             )
         return None
