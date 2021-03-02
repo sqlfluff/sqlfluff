993e7f4daf5a0b6b7c73b3405332f964ef121f25
Barry Hart <barry.hart@mailchimp.com>
2021-03-02 09:56:50 -0500

    Fix parsing of "<" inside parentheses

--- src/sqlfluff/core/parser/grammar/base.py
+++ src/sqlfluff/core/parser/grammar/base.py
@@ -538,17 +538,29 @@ class BaseGrammar(Matchable):
                                 end_is_definite = end_definite[end_index]
                                 if not end_is_definite:
                                     # The end bracket whose type didn't match
-                                    # the innermost open brackert is not
+                                    # the innermost open bracket is not
                                     # definite. Assume it's not a bracket and
                                     # carry on.
                                     pre_seg_buff += pre
                                     pre_seg_buff += match.matched_segments
                                     seg_buff = match.unmatched_segments
                                 else:
-                                    raise SQLParseError(
-                                        f"Found unexpected end bracket!, was expecting {end_brackets[start_index]}, but got {matcher}",
-                                        segment=match.matched_segments[0],
-                                    )
+                                    # Definite end bracket does not match the
+                                    # innermost start bracket. Was the innermost
+                                    # start bracket definite? If yes, error. If
+                                    # no, assume it was not a bracket.
+                                    # Can we remove any brackets from the stack which aren't definites
+                                    # to resolve the issue?
+                                    for idx in range(len(bracket_stack) - 1, -1, -1):
+                                        if not bracket_stack[idx].is_definite:
+                                            del bracket_stack[idx]
+                                            # We don't change the string buffer, we assume that was ok.
+                                            break
+                                    else:
+                                        raise SQLParseError(
+                                            f"Found unexpected end bracket!, was expecting {end_brackets[start_index]}, but got {matcher}",
+                                            segment=match.matched_segments[0],
+                                        )
 
                         else:
                             raise RuntimeError("I don't know how we get here?!")
