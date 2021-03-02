c0e43d69b0661c1b9d4c70ef53a07059aecb8178
Barry Hart <barry.hart@mailchimp.com>
2021-03-02 08:19:46 -0500

    Pass 'where="src"' to find_packages()

--- plugins/sqlfluff-plugin-example/setup.py
+++ plugins/sqlfluff-plugin-example/setup.py
@@ -5,7 +5,7 @@ setup(
     name="sqlfluff-plugin-example",
     include_package_data=True,
     package_dir={"": "src"},
-    packages=find_packages(),
+    packages=find_packages(where="src"),
     install_requires="sqlfluff>=0.4.0",
     entry_points={"sqlfluff": ["example = example.rules"]},
 )
