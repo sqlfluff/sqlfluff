d4c68254a3ea33186a3fcfdfd6b85cb440669b06
Barry Hart <barry.hart@mailchimp.com>
2021-03-02 13:07:31 -0500

    Two separate "template parameters": Logical name and root module name

--- plugins/sqlfluff-plugin-example/setup.py
+++ plugins/sqlfluff-plugin-example/setup.py
@@ -1,18 +1,24 @@
 """Setup file for example plugin."""
 from setuptools import find_packages, setup
 
-# Change this name in your plugin, e.g. company name or plugin purpose.
-PLUGIN_NAME = "example"
+# Change these names in your plugin, e.g. company name or plugin purpose.
+PLUGIN_LOGICAL_NAME = "example"
+PLUGIN_ROOT_MODULE = "example"
 
 setup(
-    name="sqlfluff-plugin-{plugin_name}".format(plugin_name=PLUGIN_NAME),
+    name="sqlfluff-plugin-{plugin_logical_name}".format(
+        plugin_logical_name=PLUGIN_LOGICAL_NAME
+    ),
     include_package_data=True,
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires="sqlfluff>=0.4.0",
     entry_points={
         "sqlfluff": [
-            "{plugin_name} = {plugin_name}.rules".format(plugin_name=PLUGIN_NAME)
+            "{plugin_logical_name} = {plugin_root_module}.rules".format(
+                plugin_logical_name=PLUGIN_LOGICAL_NAME,
+                plugin_root_module=PLUGIN_ROOT_MODULE,
+            )
         ]
     },
 )
