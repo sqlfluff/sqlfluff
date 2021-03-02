764ee3201fddc4275267a787b89e13d42fd1cead
Barry Hart <barry.hart@mailchimp.com>
2021-03-02 08:23:28 -0500

    Add a bit of templating

--- plugins/sqlfluff-plugin-example/setup.py
+++ plugins/sqlfluff-plugin-example/setup.py
@@ -1,11 +1,14 @@
 """Setup file for example plugin."""
 from setuptools import find_packages, setup
 
+# Change this name in your plugin, e.g. company name or plugin purpose.
+PLUGIN_NAME = "example"
+
 setup(
-    name="sqlfluff-plugin-example",
+    name="sqlfluff-plugin-{plugin_name}".format(plugin_name=PLUGIN_NAME),
     include_package_data=True,
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires="sqlfluff>=0.4.0",
-    entry_points={"sqlfluff": ["example = example.rules"]},
+    entry_points={"sqlfluff": ["{plugin_name} = {plugin_name}.rules"]},
 )
