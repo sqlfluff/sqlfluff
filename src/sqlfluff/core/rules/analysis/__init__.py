734daf38ab9a74cd28c0aedac4b712b9c56839ad
Barry Hart <barry.hart@mailchimp.com>
2021-03-03 12:32:02 -0500

    Try again disabling Flake8 warnings

--- src/sqlfluff/core/rules/analysis/__init__.py
+++ src/sqlfluff/core/rules/analysis/__init__.py
@@ -2,5 +2,7 @@
 from sqlfluff.core.rules.analysis.select import (
     get_aliases_from_select,
     get_select_statement_info,
-)  # noqa: F401
-from sqlfluff.core.rules.analysis.select_crawler import SelectCrawler  # noqa: F401
+)  # flake8: noqa: F401
+from sqlfluff.core.rules.analysis.select_crawler import (
+    SelectCrawler,
+)  # flake8: noqa: F401
