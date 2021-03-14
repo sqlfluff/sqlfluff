ce9c89d57f9f907537a6958b756aa06a889ebbb4
Barry Hart <barry.hart@mailchimp.com>
2021-03-14 17:14:34 -0400

    Flake8, Black

--- test/core/rules/base_test.py
+++ test/core/rules/base_test.py
@@ -1,5 +1,7 @@
+"""Test sqlfluff.core.rules.base module."""
 import sqlfluff.core.rules.base as rules_base
 
 
 def test_whitespace_segment_is_whitespace():
-    assert rules_base.BaseCrawler.make_whitespace('', '').is_whitespace
+    """Tests that WhitespaceSegment.is_whitespace is True"""
+    assert rules_base.BaseCrawler.make_whitespace("", "").is_whitespace
