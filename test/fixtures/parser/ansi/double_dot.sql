e43df7d8b02527d16ad3ea12b05ae08d5d1fc62d
David Quinlan <david.jf.quinlan@gmail.com>
2021-03-26 19:24:38 +0000

    double_dot_notation: Removing unnecessary DotSegment sequence

--- test/fixtures/parser/ansi/double_dot.sql
+++ test/fixtures/parser/ansi/double_dot.sql
@@ -1,4 +1,4 @@
 -- Snowflake Double-Dot Notation
 -- https://docs.snowflake.com/en/sql-reference/name-resolution.html#resolution-when-schema-omitted-double-dot-notation
 SELECT *
-FROM database..my_table
+FROM my_database..my_table
