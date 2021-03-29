a176cc81780c3f68fd032ca41c5b106a0c342162
nevado <18424398+nevado@users.noreply.github.com>
2021-03-29 21:36:54 +0100

    encoding - make sure file is closed after read

--- src/sqlfluff/core/file_helpers.py
+++ src/sqlfluff/core/file_helpers.py
@@ -10,6 +10,8 @@ def get_encoding(fname: str, config: FluffConfig) -> str:
     encoding_config = config.get("encoding", default="autodetect")
 
     if encoding_config == "autodetect":
-        return chardet.detect(open(fname, "rb").read())["encoding"]
+        with open(fname, "rb") as f:
+            data = f.read()
+        return chardet.detect(data)["encoding"]
 
     return encoding_config
