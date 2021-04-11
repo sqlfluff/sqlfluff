4680c336d173b2c89b51636711f85159199fd08c
GClunies <greg.clunies@gmail.com>
2021-04-11 09:55:08 -0700

    Remove unneeded #noqa after merging in linting fixes from upstream

--- src/sqlfluff/core/linter.py
+++ src/sqlfluff/core/linter.py
@@ -565,17 +565,17 @@ class LintedPath:
 
     @overload
     def check_tuples(self, by_path: Literal[False]) -> List[CheckTuple]:
-        """Return a List of CheckTuples when by_path is False."""  # noqa: D418
+        """Return a List of CheckTuples when by_path is False."""
         ...
 
     @overload
     def check_tuples(self, by_path: Literal[True]) -> Dict[str, List[CheckTuple]]:
-        """Return a Dict of paths and CheckTuples when by_path is True."""  # noqa: D418
+        """Return a Dict of paths and CheckTuples when by_path is True."""
         ...
 
     @overload
     def check_tuples(self, by_path: bool = False):
-        """Default overload method."""  # noqa: D418
+        """Default overload method."""
         ...
 
     def check_tuples(self, by_path=False):
@@ -678,19 +678,19 @@ class LintingResult:
 
     @overload
     def check_tuples(self, by_path: Literal[False]) -> List[CheckTuple]:
-        """Return a List of CheckTuples when by_path is False."""  # noqa: D418
+        """Return a List of CheckTuples when by_path is False."""
         ...
 
     @overload
     def check_tuples(
         self, by_path: Literal[True]
     ) -> Dict[LintedPath, List[CheckTuple]]:
-        """Return a Dict of LintedPath and CheckTuples when by_path is True."""  # noqa: D418
+        """Return a Dict of LintedPath and CheckTuples when by_path is True."""
         ...
 
     @overload
     def check_tuples(self, by_path: bool = False):
-        """Default overload method."""  # noqa: D418
+        """Default overload method."""
         ...
 
     def check_tuples(self, by_path=False):
