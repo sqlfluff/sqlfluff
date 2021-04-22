00ec29ebf08b4481bd32631ecf23054a7ef64328
Barry Hart <barry.hart@mailchimp.com>
2021-04-22 11:58:56 -0400

    Run Black

--- test/generate_parse_fixture_yml.py
+++ test/generate_parse_fixture_yml.py
@@ -27,7 +27,7 @@ for example in parse_success_examples:
     # Remove the .sql file extension
     root = sqlfile[:-4]
     path = os.path.join("test", "fixtures", "parser", dialect, root + ".yml")
-    with open(path, "w", newline='\n') as f:
+    with open(path, "w", newline="\n") as f:
         if r:
             yaml.dump(r, f, default_flow_style=False)
         else:
