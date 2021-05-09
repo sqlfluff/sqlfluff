35bb3f2b904b36557b957a8c8482ae3ba306c02b
Patrick von Glehn <p.vonglehn@carepay.com>
2021-05-09 14:04:22 +0200

    Add project_dir to config fixture and remove in_dbt_project fixture.

--- test/fixtures/dbt/templater.py
+++ test/fixtures/dbt/templater.py
@@ -12,7 +12,8 @@ DBT_FLUFF_CONFIG = {
     },
     "templater": {
         "dbt": {
-            "profiles_dir": "../dbt",
+            "profiles_dir": "test/fixtures/dbt",
+            "project_dir":"test/fixtures/dbt_project"
         },
     },
 }
@@ -23,13 +24,3 @@ def dbt_templater():
     """Returns an instance of the DbtTemplater."""
     return DbtTemplater()
 
-
-@pytest.fixture()
-def in_dbt_project_dir():
-    """A wrapper to chdir into the dbt_project fixture and back to cwd at the end of the test."""
-    try:
-        pre_test_dir = os.getcwd()
-        os.chdir("test/fixtures/dbt_project")
-        yield  # test runs here
-    finally:
-        os.chdir(pre_test_dir)
