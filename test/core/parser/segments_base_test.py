359c9a10949985ed366a7d648d25bae18a405a6d
Alan <alanmcruickshank@gmail.com>
2021-05-10 09:09:30 +0100

    Black

--- test/core/parser/segments_base_test.py
+++ test/core/parser/segments_base_test.py
@@ -53,11 +53,7 @@ def test__parser__base_segments_raw(raw_seg):
     # Check Formatting and Stringification
     # NOTE: The preceding underscore shouldn't be there
     # but it's added by .make(). Revisit later.
-    assert (
-        str(raw_seg)
-        == repr(raw_seg)
-        == "<_RawSegment: ([L:  1, P:  1]) 'foobar'>"
-    )
+    assert str(raw_seg) == repr(raw_seg) == "<_RawSegment: ([L:  1, P:  1]) 'foobar'>"
     assert (
         raw_seg.stringify(ident=1, tabsize=2)
         == "[L:  1, P:  1]      |  raw:                                                        'foobar'\n"
