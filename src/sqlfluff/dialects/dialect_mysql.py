bbada4e98df438abf949276f096450ab3230b5ba
Adam Hafner <ahafner@payrailz.com>
2021-07-09 09:38:57 -0500

    Add statements related to PREPARE for mysql dialect

--- src/sqlfluff/dialects/dialect_mysql.py
+++ src/sqlfluff/dialects/dialect_mysql.py
@@ -48,6 +48,20 @@ mysql_dialect.sets("unreserved_keywords").difference_update(
         "DUMPFILE",
         "SKIP",
         "LOCKED",
+        "CLASS_ORIGIN",
+        "SUBCLASS_ORIGIN",
+        "RETURNED_SQLSTATE",
+        "MESSAGE_TEXT",
+        "MYSQL_ERRNO",
+        "CONSTRAINT_CATALOG",
+        "CONSTRAINT_SCHEMA",
+        "CONSTRAINT_NAME",
+        "CATALOG_NAME",
+        "SCHEMA_NAME",
+        "TABLE_NAME",
+        "COLUMN_NAME",
+        "CURSOR_NAME",
+        "STACKED",
     ]
 )
 mysql_dialect.sets("reserved_keywords").update(
@@ -61,6 +75,20 @@ mysql_dialect.sets("reserved_keywords").update(
         "DUMPFILE",
         "SKIP",
         "LOCKED",
+        "CLASS_ORIGIN",
+        "SUBCLASS_ORIGIN",
+        "RETURNED_SQLSTATE",
+        "MESSAGE_TEXT",
+        "MYSQL_ERRNO",
+        "CONSTRAINT_CATALOG",
+        "CONSTRAINT_SCHEMA",
+        "CONSTRAINT_NAME",
+        "CATALOG_NAME",
+        "SCHEMA_NAME",
+        "TABLE_NAME",
+        "COLUMN_NAME",
+        "CURSOR_NAME",
+        "STACKED",
     ]
 )
 
@@ -290,6 +318,8 @@ class StatementSegment(ansi_dialect.get_segment("StatementSegment")):  # type: i
             Ref("PrepareSegment"),
             Ref("ExecuteSegment"),
             Ref("DeallocateSegment"),
+            Ref("GetDiagnosticsSegment"),
+            Ref("ResignalSegment"),
             Ref("CursorOpenCloseSegment"),
             Ref("CursorFetchSegment"),
         ],
@@ -795,6 +825,63 @@ class PrepareSegment(BaseSegment):
     )
 
 
+@mysql_dialect.segment()
+class GetDiagnosticsSegment(BaseSegment):
+    """This is the body of a `GET DIAGNOSTICS` statement.
+
+    https://dev.mysql.com/doc/refman/8.0/en/get-diagnostics.html
+    """
+
+    type = "get_diagnostics_segment"
+
+    match_grammar = Sequence(
+        "GET",
+        Sequence("CURRENT", "STACKED", optional=True),
+        "DIAGNOSTICS",
+        Delimited(
+            Sequence(
+                OneOf(
+                    Ref("SessionVariableNameSegment"), Ref("LocalVariableNameSegment")
+                ),
+                Ref("EqualsSegment"),
+                OneOf("NUMBER", "ROW_COUNT"),
+            ),
+            optional=True,
+        ),
+        "CONDITION",
+        OneOf(
+            Ref("SessionVariableNameSegment"),
+            Ref("LocalVariableNameSegment"),
+            Ref("NumericLiteralSegment"),
+        ),
+        Delimited(
+            Sequence(
+                OneOf(
+                    Ref("SessionVariableNameSegment"), Ref("LocalVariableNameSegment")
+                ),
+                Ref("EqualsSegment"),
+                OneOf(
+                    "CLASS_ORIGIN",
+                    "SUBCLASS_ORIGIN",
+                    "RETURNED_SQLSTATE",
+                    "MESSAGE_TEXT",
+                    "MYSQL_ERRNO",
+                    "CONSTRAINT_CATALOG",
+                    "CONSTRAINT_SCHEMA",
+                    "CONSTRAINT_NAME",
+                    "CATALOG_NAME",
+                    "SCHEMA_NAME",
+                    "TABLE_NAME",
+                    "COLUMN_NAME",
+                    "CURSOR_NAME",
+                ),
+            ),
+            optional=True,
+        ),
+    )
+
+
+@mysql_dialect.segment()
 class CursorOpenCloseSegment(BaseSegment):
     """This is a CLOSE or Open statement.
 
@@ -841,6 +928,58 @@ class DeallocateSegment(BaseSegment):
     match_grammar = Sequence(
         Sequence(OneOf("DEALLOCATE", "DROP"), "PREPARE"),
         Ref("NakedIdentifierSegment"),
+    )
+
+
+@mysql_dialect.segment()
+class ResignalSegment(BaseSegment):
+    """This is the body of a `RESIGNAL` statement.
+
+    https://dev.mysql.com/doc/refman/8.0/en/resignal.html
+    """
+
+    type = "resignal_segment"
+
+    match_grammar = Sequence(
+        OneOf("SIGNAL", "RESIGNAL"),
+        OneOf(
+            Sequence(
+                "SQLSTATE",
+                Ref.keyword("VALUE", optional=True),
+                Ref("QuotedLiteralSegment"),
+            ),
+            Ref("NakedIdentifierSegment"),
+            optional=True,
+        ),
+        Sequence(
+            "SET",
+            Delimited(
+                Sequence(
+                    OneOf(
+                        "CLASS_ORIGIN",
+                        "SUBCLASS_ORIGIN",
+                        "RETURNED_SQLSTATE",
+                        "MESSAGE_TEXT",
+                        "MYSQL_ERRNO",
+                        "CONSTRAINT_CATALOG",
+                        "CONSTRAINT_SCHEMA",
+                        "CONSTRAINT_NAME",
+                        "CATALOG_NAME",
+                        "SCHEMA_NAME",
+                        "TABLE_NAME",
+                        "COLUMN_NAME",
+                        "CURSOR_NAME",
+                    ),
+                    Ref("EqualsSegment"),
+                    OneOf(
+                        Ref("SessionVariableNameSegment"),
+                        Ref("LocalVariableNameSegment"),
+                        Ref("QuotedLiteralSegment"),
+                    ),
+                ),
+            ),
+            optional=True,
+        ),
     )
 
 
