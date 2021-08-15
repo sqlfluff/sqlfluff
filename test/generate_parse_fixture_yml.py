083aa607d18f56934a6cb14a7fadff53af0929b3
Barry Hart <bhart@rsglab.com>
2021-08-15 12:16:35 -0400

    Use process pool to speed up test/generate_parse_fixture_yml.py

--- test/generate_parse_fixture_yml.py
+++ test/generate_parse_fixture_yml.py
@@ -1,4 +1,5 @@
 """Utility to generate yml files for all the parsing examples."""
+import multiprocessing
 import os
 
 import oyaml as yaml
@@ -7,8 +8,9 @@ from conftest import compute_parse_tree_hash, parse_example_file
 from dialects.parse_fixtures import get_parse_fixtures
 
 
-def generate_parse_fixture(dialect, sqlfile):
+def generate_parse_fixture(example):
     """Parse example SQL file, write parse tree to YAML file."""
+    dialect, sqlfile = example
     tree = parse_example_file(dialect, sqlfile)
     _hash = compute_parse_tree_hash(tree)
     # Remove the .sql file extension
@@ -29,9 +31,9 @@ def generate_parse_fixture(dialect, sqlfile):
 def main():
     """Find all example SQL files, parse and create YAML files."""
     parse_success_examples, _ = get_parse_fixtures()
-    for example in parse_success_examples:
-        dialect, sqlfile = example
-        generate_parse_fixture(dialect, sqlfile)
+    with multiprocessing.Pool(multiprocessing.cpu_count()) as pool:
+        for _ in pool.imap_unordered(generate_parse_fixture, parse_success_examples):
+            pass
 
 
 if __name__ == "__main__":
