e2c608de510cd869f6e5900fb6c5fb2d08eb58f3
Eric Muijs <Muijs.Eric@kpmg.nl>
2021-08-19 14:33:24 +0200

    working without parsing

--- src/sqlfluff/dialects/dialect_tsql.py
+++ src/sqlfluff/dialects/dialect_tsql.py
@@ -4,44 +4,99 @@ https://docs.microsoft.com/en-us/sql/t-sql/language-elements/language-elements-t
 
 
 """
+from enum import Enum
+from typing import Generator, List, Tuple, NamedTuple, Optional, Union
 
 from sqlfluff.core.parser import (
-    AnyNumberOf,
+    Matchable,
     BaseSegment,
-    Bracketed,
-    OptionallyBracketed,
-    Dedent,
-    Delimited,
-    GreedyUntil,
-    Indent,
     KeywordSegment,
-    NamedSegment,
-    Nothing,
-    OneOf,
-    Ref,
-    ReSegment,
+    SymbolSegment,
     Sequence,
+    GreedyUntil,
     StartsWith,
-    RegexParser,
+    OneOf,
+    Delimited,
+    Bracketed,
+    AnyNumberOf,
+    Ref,
+    SegmentGenerator,
+    Anything,
+    Indent,
+    Dedent,
+    Nothing,
+    OptionallyBracketed,
+    StringLexer,
+    RegexLexer,
     CodeSegment,
+    CommentSegment,
+    WhitespaceSegment,
+    NewlineSegment,
+    StringParser,
+    NamedParser,
+    RegexParser,
+    Conditional,
 )
-from sqlfluff.core.dialects import load_raw_dialect
+
+from sqlfluff.core.dialects.base import Dialect
+from sqlfluff.core.dialects.common import AliasInfo
+from sqlfluff.core.parser.segments.base import BracketedSegment
+
+from sqlfluff.dialects.ansi_keywords import (
+    ansi_reserved_keywords,
+    ansi_unreserved_keywords,
+)
 from sqlfluff.dialects.tsql_keywords import (
     # BARE_FUNCTIONS,
     RESERVED_KEYWORDS,
     # UNRESERVED_KEYWORDS,
 )
 
+from sqlfluff.core.dialects import load_raw_dialect
+
+
 ansi_dialect = load_raw_dialect("ansi")
 tsql_dialect = ansi_dialect.copy_as("tsql")
 
 # Update only RESERVED Keywords
-tsql_dialect.sets("reserved_keywords").clear()
-tsql_dialect.sets("reserved_keywords").update(RESERVED_KEYWORDS)
+# tsql_dialect.sets("reserved_keywords").clear()
+# tsql_dialect.sets("reserved_keywords").update(RESERVED_KEYWORDS)
 
+tsql_dialect.add(
+    AtSignLiteralSegment=NamedParser(
+        "atsign",
+        CodeSegment,
+        name="atsign_literal",
+        type="literal",
+        trim_chars=("@",),
+    ),
+)
 
-ansi_dialect.add(
-    # Real segments
+tsql_dialect.add(    
+    AtSignSignSegment=StringParser(
+        "@", SymbolSegment, name="atsign", type="user_designator"
+    ),   
+    SessionVariableNameSegment=RegexParser(
+        r"[@][a-zA-Z0-9_]*",
+        CodeSegment,
+        name="declared_variable",
+        type="variable",
+    ),
+)
+
+tsql_dialect.replace(
     ParameterNameSegment=RegexParser(
-        r"[A-Z][A-Z0-9_]*", CodeSegment, name="parameter", type="parameter"
-    ))
+        r"`?[A-Za-z0-9_]*`?", CodeSegment, name="parameter", type="parameter"
+    )
+)
+
+tsql_dialect.insert_lexer_matchers(
+    [
+        RegexLexer(
+            "atsign",
+            r"[@][a-zA-Z0-9_]*",
+            CodeSegment,
+        ),
+    ],
+    before="code",
+)
