ff9731f02304cc6cf7699426ed7770449613ebe7
Alan <alanmcruickshank@gmail.com>
2021-08-20 20:41:17 +0100

    Update docstrings

--- src/sqlfluff/core/config.py
+++ src/sqlfluff/core/config.py
@@ -459,10 +459,15 @@ class FluffConfig:
         # Restore instance attributes
         self.__dict__.update(state)
         # NB: We don't reinstate the plugin manager, but this should only
-        # be happening between threads where the plugin manager should
+        # be happening between processes where the plugin manager should
         # probably be fresh in any case.
-        # NOTE: This means that registering user plugins directly is not
-        # thread safe.
+        # NOTE: This means that registering user plugins directly will only
+        # work if those plugins are used in the main process (i.e. templaters).
+        # User registered linting rules either must be "installed" and therefore
+        # available to all processes - or their use is limited to only single
+        # process invocations of sqlfluff. In the event that user registered
+        # rules are used in a multi-process invocation, they will not be applied
+        # in the child processes.
 
     @classmethod
     def from_root(cls, overrides: Optional[dict] = None) -> "FluffConfig":
