1fe748e14bc91d1cfdd63d6dc2fad0386984b86e
Danny Jones <danny.jones1994@live.com>
2021-08-31 10:46:56 +0100

    Added $$ syntax to postgres create function test

--- test/fixtures/parser/postgres/postgres_create_function.sql
+++ test/fixtures/parser/postgres/postgres_create_function.sql
@@ -21,8 +21,8 @@ CREATE OR REPLACE FUNCTION increment(i integer) RETURNS integer AS 'C:\\my_file.
 LANGUAGE plpgsql WINDOW IMMUTABLE STABLE NOT LEAKPROOF CALLED ON NULL INPUT EXTERNAL SECURITY DEFINER COST 123
 ROWS 5 SET test_param = 3 WITH (isStrict);
 
-CREATE OR REPLACE FUNCTION increment(i integer) RETURNS integer PARALLEL UNSAFE AS '
+CREATE OR REPLACE FUNCTION increment(i integer) RETURNS integer PARALLEL UNSAFE AS $$
     BEGIN
         RETURN i + 1;
     END;
-' LANGUAGE plpgsql SUPPORT my_function;
+$$ LANGUAGE plpgsql SUPPORT my_function;
