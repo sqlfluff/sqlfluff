686fd968da397dd63aebb95ca2b1a0b0ee0bd99c
Eric Muijs <Muijs.Eric@kpmg.nl>
2021-09-02 09:48:04 +0200

    revert two files

--- src/sqlfluff/dialects/dialect_mysql.py
+++ src/sqlfluff/dialects/dialect_mysql.py
@@ -798,6 +798,7 @@ class CallStoredProcedureSegment(BaseSegment):
         ),
     )
 
+
 @mysql_dialect.segment()
 class SelectPartitionClauseSegment(BaseSegment):
     """This is the body of a partition clause."""
