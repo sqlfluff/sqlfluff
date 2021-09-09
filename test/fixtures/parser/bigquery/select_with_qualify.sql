bd2380a08d99d8cc287e015bcc0f46ce133b5f88
Barry <barry@tunetheweb.com>
2021-09-09 07:49:29 +0100

    Add additional test

--- test/fixtures/parser/bigquery/select_with_qualify.sql
+++ test/fixtures/parser/bigquery/select_with_qualify.sql
@@ -36,6 +36,14 @@ SELECT
 FROM Produce
 WHERE Produce.category = 'vegetable'
 QUALIFY rank <= 3
+LIMIT 5;
+
+SELECT
+  item,
+  RANK() OVER (PARTITION BY category ORDER BY purchases DESC) AS rank
+FROM Produce
+WHERE Produce.category = 'vegetable'
+QUALIFY rank <= 3
 WINDOW item_window AS (
   PARTITION BY category
   ORDER BY purchases
