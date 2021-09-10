c3a1f703e6d34fd2b293ef17387fe603cf910b82
ericmuijs <eric.muijs@gmail.com>
2021-09-10 14:42:22 +0200

    Initial support for Transaction-SQL (T-SQL) dialect (#1313)
    * first step
    
    * working without parsing
    
    * commit intial function fix tsql
    
    * Update src/sqlfluff/dialects/dialect_tsql.py
    
    Co-authored-by: Barry Pollard <barry_pollard@hotmail.com>
    
    * Update test/dialects/tsql_test.py
    
    Co-authored-by: Barry Pollard <barry_pollard@hotmail.com>
    
    * updated version with added test fixtures
    
    * added official reserved keywords
    
    * delete old function
    
    * basic statements support
    
    * cleanup unused segments
    
    * revert two files
    
    * reformat with black
    
    * basic create table support
    
    * reformat black
    
    * update dialect tsql
    
    * question to barry create table statement
    
    * Support bracketed table references
    
    * column definition added to create table.
    
    * close to create table, issue with table name space
    
    * fixed create table with brackets and spaces
    
    * support for basic Tsql
    
    * Minor reformats
    
    * Import keywords
    
    * Clean up
    
    * removed column option and added comments
    
    * removed unused class import
    
    * Fix linting
    
    * Add T-SQL to docs
    
    * Review feedback
    
    Co-authored-by: Eric Muijs <Muijs.Eric@kpmg.nl>
    Co-authored-by: Barry Pollard <barry_pollard@hotmail.com>
    Co-authored-by: Barry <barry@tunetheweb.com>

---
+++ src/sqlfluff/dialects/dialect_tsql.py
@@ -0,0 +1,253 @@
+"""The MSSQL T-SQL dialect.
+
+https://docs.microsoft.com/en-us/sql/t-sql/language-elements/language-elements-transact-sql
+"""
+
+from sqlfluff.core.parser import (
+    BaseSegment,
+    Sequence,
+    OneOf,
+    Bracketed,
+    Ref,
+    Anything,
+    RegexLexer,
+    CodeSegment,
+    RegexParser,
+    Delimited,
+    Matchable,
+    NamedParser,
+)
+
+from sqlfluff.core.dialects import load_raw_dialect
+from sqlfluff.dialects.tsql_keywords import RESERVED_KEYWORDS
+
+ansi_dialect = load_raw_dialect("ansi")
+tsql_dialect = ansi_dialect.copy_as("tsql")
+
+# Update only RESERVED Keywords
+# Should really clear down the old keywords but some are needed by certain segments
+# tsql_dialect.sets("reserved_keywords").clear()
+tsql_dialect.sets("reserved_keywords").update(RESERVED_KEYWORDS)
+
+
+tsql_dialect.insert_lexer_matchers(
+    [
+        RegexLexer(
+            "atsign",
+            r"[@][a-zA-Z0-9_]+",
+            CodeSegment,
+        ),
+        RegexLexer(
+            "square_quote",
+            r"\[([a-zA-Z][^\[\]]*)*\]",
+            CodeSegment,
+        ),
+    ],
+    before="back_quote",
+)
+
+tsql_dialect.add(
+    BracketedIdentifierSegment=NamedParser(
+        "square_quote", CodeSegment, name="quoted_identifier", type="identifier"
+    ),
+)
+
+tsql_dialect.replace(
+    # Below delimiterstatement might need to be removed in the future as delimiting
+    # is optional with semicolon and GO is a end of statement indicator.
+    DelimiterSegment=OneOf(
+        Sequence(Ref("SemicolonSegment"), Ref("GoStatementSegment")),
+        Ref("SemicolonSegment"),
+        Ref("GoStatementSegment"),
+    ),
+    SingleIdentifierGrammar=OneOf(
+        Ref("NakedIdentifierSegment"),
+        Ref("QuotedIdentifierSegment"),
+        Ref("BracketedIdentifierSegment"),
+    ),
+    ParameterNameSegment=RegexParser(
+        r"[@][A-Za-z0-9_]+", CodeSegment, name="parameter", type="parameter"
+    ),
+    FunctionNameIdentifierSegment=RegexParser(
+        r"[A-Z][A-Z0-9_]*|\[[A-Z][A-Z0-9_]*\]",
+        CodeSegment,
+        name="function_name_identifier",
+        type="function_name_identifier",
+    ),
+    DatatypeIdentifierSegment=Ref("SingleIdentifierGrammar"),
+)
+
+
+@tsql_dialect.segment(replace=True)
+class StatementSegment(ansi_dialect.get_segment("StatementSegment")):  # type: ignore
+    """Overriding StatementSegment to allow for additional segment parsing."""
+
+    parse_grammar = ansi_dialect.get_segment("StatementSegment").parse_grammar.copy(
+        insert=[
+            Ref("CreateProcedureStatementSegment"),
+        ],
+    )
+
+
+@tsql_dialect.segment(replace=True)
+class ObjectReferenceSegment(BaseSegment):
+    """A reference to an object.
+
+    Update ObjectReferenceSegment to only allow dot separated SingleIdentifierGrammar
+    So Square Bracketed identifers can be matched.
+    """
+
+    type = "object_reference"
+    # match grammar (don't allow whitespace)
+    match_grammar: Matchable = Delimited(
+        Ref("SingleIdentifierGrammar"),
+        delimiter=OneOf(
+            Ref("DotSegment"), Sequence(Ref("DotSegment"), Ref("DotSegment"))
+        ),
+        allow_gaps=False,
+    )
+
+
+@tsql_dialect.segment()
+class GoStatementSegment(BaseSegment):
+    """GO signals the end of a batch of Transact-SQL statements to the SQL Server utilities.
+
+    GO statements are not part of the TSQL language. They are used to signal batch statements
+    so that clients know in how batches of statements can be executed.
+    """
+
+    type = "go_statement"
+    match_grammar = Sequence("GO")
+
+
+@tsql_dialect.segment(replace=True)
+class DatatypeSegment(BaseSegment):
+    """A data type segment.
+
+    Updated for Transact-SQL to allow bracketed data types with bracketed schemas.
+    """
+
+    type = "data_type"
+    match_grammar = Sequence(
+        # Some dialects allow optional qualification of data types with schemas
+        Sequence(
+            Ref("SingleIdentifierGrammar"),
+            Ref("DotSegment"),
+            allow_gaps=False,
+            optional=True,
+        ),
+        OneOf(
+            Ref("DatatypeIdentifierSegment"),
+            Bracketed(Ref("DatatypeIdentifierSegment"), bracket_type="square"),
+        ),
+        Bracketed(
+            OneOf(
+                Delimited(Ref("ExpressionSegment")),
+                # The brackets might be empty for some cases...
+                optional=True,
+            ),
+            # There may be no brackets for some data types
+            optional=True,
+        ),
+        Ref("CharCharacterSetSegment", optional=True),
+    )
+
+
+@tsql_dialect.segment(replace=True)
+class CreateFunctionStatementSegment(BaseSegment):
+    """A `CREATE FUNCTION` statement.
+
+    This version in the TSQL dialect should be a "common subset" of the
+    structure of the code for those dialects.
+
+    Updated to include AS after declaration of RETURNS. Might be integrated in ANSI though.
+
+    postgres: https://www.postgresql.org/docs/9.1/sql-createfunction.html
+    snowflake: https://docs.snowflake.com/en/sql-reference/sql/create-function.html
+    bigquery: https://cloud.google.com/bigquery/docs/reference/standard-sql/user-defined-functions
+    tsql/mssql : https://docs.microsoft.com/en-us/sql/t-sql/statements/create-function-transact-sql?view=sql-server-ver15
+    """
+
+    type = "create_function_statement"
+
+    match_grammar = Sequence(
+        "CREATE",
+        Sequence("OR", "ALTER", optional=True),
+        "FUNCTION",
+        Anything(),
+    )
+    parse_grammar = Sequence(
+        "CREATE",
+        Sequence("OR", "ALTER", optional=True),
+        "FUNCTION",
+        Ref("ObjectReferenceSegment"),
+        Ref("FunctionParameterListGrammar"),
+        Sequence(  # Optional function return type
+            "RETURNS",
+            Ref("DatatypeSegment"),
+            optional=True,
+        ),
+        Ref("FunctionDefinitionGrammar"),
+    )
+
+
+@tsql_dialect.segment(replace=True)
+class FunctionDefinitionGrammar(BaseSegment):
+    """This is the body of a `CREATE FUNCTION AS` statement.
+
+    Adjusted from ansi as Transact SQL does not seem to have the QuotedLiteralSegmentand Language.
+    Futhermore the body can contain almost anything like a function with table output.
+    """
+
+    type = "function_statement"
+    name = "function_statement"
+
+    match_grammar = Sequence("AS", Sequence(Anything()))
+
+
+@tsql_dialect.segment()
+class CreateProcedureStatementSegment(BaseSegment):
+    """A `CREATE OR ALTER PROCEDURE` statement.
+
+    https://docs.microsoft.com/en-us/sql/t-sql/statements/create-procedure-transact-sql?view=sql-server-ver15
+    """
+
+    type = "create_procedure_statement"
+
+    match_grammar = Sequence(
+        "CREATE",
+        Sequence("OR", "ALTER", optional=True),
+        OneOf("PROCEDURE", "PROC"),
+        Ref("ObjectReferenceSegment"),
+        Ref("FunctionParameterListGrammar", optional=True),
+        Ref("ProcedureDefinitionGrammar"),
+    )
+
+
+@tsql_dialect.segment()
+class ProcedureDefinitionGrammar(BaseSegment):
+    """This is the body of a `CREATE OR ALTER PROCEDURE AS` statement."""
+
+    type = "procedure_statement"
+    name = "procedure_statement"
+
+    match_grammar = Sequence("AS", Sequence(Anything()))
+
+
+@tsql_dialect.segment(replace=True)
+class CreateViewStatementSegment(BaseSegment):
+    """A `CREATE VIEW` statement.
+
+    Adjusted to allow CREATE OR ALTER instead of CREATE OR REPLACE.
+    # https://docs.microsoft.com/en-us/sql/t-sql/statements/create-view-transact-sql?view=sql-server-ver15#examples
+    """
+
+    type = "create_view_statement"
+    match_grammar = Sequence(
+        "CREATE",
+        Sequence("OR", "ALTER", optional=True),
+        "VIEW",
+        Ref("ObjectReferenceSegment"),
+        "AS",
+        Ref("SelectableGrammar"),
+    )
