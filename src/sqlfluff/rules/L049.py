f8a92be0ab1cf07c3bed136b775a3c719689ca68
Barry Hart <barry.hart@mailchimp.com>
2021-10-06 15:15:25 -0400

    Add a rule to warn on "= NULL" or "<> NULL" comparisons

---
+++ src/sqlfluff/rules/L049.py
@@ -0,0 +1,73 @@
+"""Implementation of Rule L006."""
+
+
+from sqlfluff.core.rules.base import BaseRule, LintResult
+from sqlfluff.core.rules.doc_decorators import document_fix_compatible
+
+
+@document_fix_compatible
+class Rule_L049(BaseRule):
+    """Comparisons with NULL should use "IS" or "IS NOT".
+
+    | **Anti-pattern**
+    | In this example, the <> is used to check for NULL values'.
+
+    .. code-block:: sql
+
+        SELECT
+            a
+        FROM foo
+        WHERE a <> NULL
+
+
+    | **Best practice**
+    | Use "IS" or "IS NOT" to check for NULL values.
+
+    .. code-block:: sql
+
+        SELECT
+            a
+        FROM foo
+        WHERE a IS NOT NULL
+    """
+
+    def _eval(self, segment, **kwargs):
+        """Relational operators should not be used to check for NULL values."""
+        # Iterate through children of this segment looking for any of the
+        # target types. We also check for whether any of the children start
+        # or end with the targets.
+
+        # We ignore any targets which start or finish this segment. They'll
+        # be dealt with by the parent segment. That also means that we need
+        # to have at least three children.
+
+        if len(segment.segments) <= 2:
+            return LintResult()
+
+        operator = None
+        for idx, sub_seg in enumerate(segment.segments):
+            # Skip anything which is whitespace
+            if sub_seg.is_whitespace:
+                continue
+            # Skip any non-code elements
+            if not sub_seg.is_code:
+                continue
+
+            # Is it a target in itself?
+            if not operator and sub_seg.name in ("equals", "not_equal_to"):
+                self.logger.debug(
+                    "Found Target [main] @%s: %r", sub_seg.pos_marker, sub_seg.raw
+                )
+                operator = sub_seg
+            elif operator:
+                # Skip anything which is whitespace
+                if sub_seg.is_whitespace:
+                    continue
+                # Skip any non-code elements
+                if not sub_seg.is_code:
+                    continue
+                if sub_seg.name == "null_literal":
+                    return LintResult(anchor=operator)
+
+                else:
+                    operator = None
