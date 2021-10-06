e19f7ca8579dec83a5ccebe2f94d428b303cd38c
Barry Hart <barry.hart@mailchimp.com>
2021-10-06 17:24:24 -0400

    Add space before/after "IS" if needed

--- src/sqlfluff/rules/L049.py
+++ src/sqlfluff/rules/L049.py
@@ -32,6 +32,41 @@ class Rule_L049(BaseRule):
         WHERE a IS NULL
     """
 
+    @staticmethod
+    def _missing_whitespace(seg, before=True):
+        """Check whether we're missing whitespace given an adjoining segment."""
+        # There is a segment
+        if not seg:
+            return False
+        # And it's not whitespace
+        if seg.is_whitespace:
+            return False
+        # And it's not an opening/closing bracket
+        if seg.name.endswith("_bracket"):
+            if seg.name.startswith("start_" if before else "end_"):
+                return False
+        if seg.is_meta:  # pragma: no cover
+            if before:
+                if seg.source_str.endswith(" ") or seg.source_str.endswith("\n"):
+                    return False
+            else:
+                if seg.source_str.startswith(" ") or seg.source_str.startswith("\n"):
+                    return False
+        return True
+
+    @staticmethod
+    def _find_segment(idx, segments, before=True):
+        """Go back or forward to find the next relevant segment."""
+        step = -1 if before else 1
+        j = idx + step
+        while (j >= 0) if before else (j < len(segments)):
+            # Don't trigger on indents, but placeholders are allowed.
+            if segments[j].is_type("indent"):
+                j += step
+            else:
+                return segments[j]
+        return None
+
     def _eval(self, segment, **kwargs):
         """Relational operators should not be used to check for NULL values."""
         # Context/motivation for this rule:
@@ -42,6 +77,7 @@ class Rule_L049(BaseRule):
 
         # Iterate through children of this segment looking for equals or "not
         # equals". Once found, check if the next code segment is a NULL literal.
+        idx_operator = None
         operator = None
         for idx, sub_seg in enumerate(segment.segments):
             # Skip anything which is whitespace or non-code.
@@ -53,6 +89,7 @@ class Rule_L049(BaseRule):
                 self.logger.debug(
                     "Found equals/not equals @%s: %r", sub_seg.pos_marker, sub_seg.raw
                 )
+                idx_operator = idx
                 operator = sub_seg
             elif operator:
                 # Look for a "NULL" literal.
@@ -68,22 +105,34 @@ class Rule_L049(BaseRule):
                     else:
                         is_seg = KeywordSegment("is")
                         not_seg = KeywordSegment("not")
-                    return LintResult(
+
+                    edit = (
+                        [is_seg]
+                        if operator.name == "equals"
+                        else [
+                            is_seg,
+                            WhitespaceSegment(),
+                            not_seg,
+                        ]
+                    )
+                    prev_seg = self._find_segment(
+                        idx_operator, segment.segments, before=True
+                    )
+                    next_seg = self._find_segment(
+                        idx_operator, segment.segments, before=False
+                    )
+                    if self._missing_whitespace(prev_seg, before=True):
+                        edit = [WhitespaceSegment()] + edit
+                    if self._missing_whitespace(next_seg, before=False):
+                        edit = edit + [WhitespaceSegment()]
+                    violation = LintResult(
                         anchor=operator,
                         fixes=[
                             LintFix(
                                 "edit",
                                 operator,
-                                [is_seg]
-                                if operator.name == "equals"
-                                else [
-                                    is_seg,
-                                    WhitespaceSegment(),
-                                    not_seg,
-                                ],
+                                edit,
                             )
                         ],
                     )
-
-                else:
-                    operator = None
+                    return violation
