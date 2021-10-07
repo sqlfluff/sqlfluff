9c091936272ea61911df653154ccb48600b5ccfe
Barry <barry@tunetheweb.com>
2021-10-07 19:21:17 +0100

    Linting

--- src/sqlfluff/dialects/dialect_tsql.py
+++ src/sqlfluff/dialects/dialect_tsql.py
@@ -110,7 +110,7 @@ tsql_dialect.replace(
         Ref("SetOperatorSegment"),
         Ref("WithNoSchemaBindingClauseSegment"),
     ),
-    JoinKeywords=OneOf("JOIN", "APPLY", Sequence("OUTER","APPLY")),
+    JoinKeywords=OneOf("JOIN", "APPLY", Sequence("OUTER", "APPLY")),
 )
 
 
