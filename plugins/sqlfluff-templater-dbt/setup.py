3bae3f97e8135fea1277f5fccbd56cd758d21884
Alan <alanmcruickshank@gmail.com>
2021-10-10 12:48:51 +0100

    Allow readme from file and better config reading.

--- plugins/sqlfluff-templater-dbt/setup.py
+++ plugins/sqlfluff-templater-dbt/setup.py
@@ -1,22 +1,21 @@
 """Setup file for example plugin."""
 from setuptools import find_packages, setup
+from os.path import dirname, join
 import configparser
 
 
 # Get the global config info as currently stated
 # (we use the config file to avoid actually loading any python here)
 config = configparser.ConfigParser()
-config.read(["src/sqlfluff/config.ini"])
+config.read([join(dirname(__file__), "../../src/sqlfluff/config.ini")])
 version = config.get("sqlfluff", "version")
 
 
-long_description = """
-# dbt plugin for SQLFluff
-
-This plugin works with [SQLFluff](https://pypi.org/project/sqlfluff/), the
-SQL linter for humans, to correctly parse and compile SQL projects using
-[dbt](https://pypi.org/project/dbt/).
-"""
+def read(*names, **kwargs):
+    """Read a file and return the contents as a string."""
+    return open(
+        join(dirname(__file__), *names), encoding=kwargs.get("encoding", "utf8")
+    ).read()
 
 
 setup(
@@ -25,7 +24,7 @@ setup(
     include_package_data=True,
     license="MIT License",
     description="Lint your dbt project SQL.",
-    long_description=long_description,
+    long_description=read("README.md"),
     # Make sure pypi is expecting markdown!
     long_description_content_type="text/markdown",
     package_dir={"": "src"},
