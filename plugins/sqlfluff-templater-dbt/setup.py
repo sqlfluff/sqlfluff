05982f04e4eb8204106d717d5f04303f68dc5f9e
Alan <alanmcruickshank@gmail.com>
2021-10-10 14:08:53 +0100

    Remove dependency on core config.ini file

--- plugins/sqlfluff-templater-dbt/setup.py
+++ plugins/sqlfluff-templater-dbt/setup.py
@@ -4,13 +4,6 @@ from os.path import dirname, join
 import configparser
 
 
-# Get the global config info as currently stated
-# (we use the config file to avoid actually loading any python here)
-config = configparser.ConfigParser()
-config.read([join(dirname(__file__), "../../src/sqlfluff/config.ini")])
-version = config.get("sqlfluff", "version")
-
-
 def read(*names, **kwargs):
     """Read a file and return the contents as a string."""
     return open(
@@ -20,19 +13,19 @@ def read(*names, **kwargs):
 
 setup(
     name="sqlfluff-templater-dbt",
-    version=version,
-    include_package_data=True,
+    version="0.7.0a4",
+    include_package_data=False,
     license="MIT License",
     description="Lint your dbt project SQL.",
     long_description=read("README.md"),
     # Make sure pypi is expecting markdown!
     long_description_content_type="text/markdown",
-    package_dir={"": "src"},
+    package_dir={"": join(dirname(__file__), "src")},
     packages=find_packages(where="src"),
     # Make sure sqlfluff is at least as updated at this plugin.
     # We might break this in the future, but for now while
     # the two are bundled, this makes sense given the release
     # cycles are coupled.
-    install_requires=[f"sqlfluff>={version}", "dbt>=0.17"],
+    install_requires=[f"sqlfluff>=0.7.0a2", "dbt>=0.17"],
     entry_points={"sqlfluff": ["sqlfluff_templater_dbt = sqlfluff_templater_dbt"]},
 )
