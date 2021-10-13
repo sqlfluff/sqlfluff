40f1aba9d62892e3cdda9da1ce99c612db2f1e38
jpersons <jpersons@iuhealth.org>
2021-10-13 18:13:39 -0400

    TSQL: Allow for multiple statements in a procedure

--- test/fixtures/dialects/tsql/stored_procedured_mixed_statements.sql
+++ test/fixtures/dialects/tsql/stored_procedured_mixed_statements.sql
@@ -2,6 +2,16 @@ CREATE PROC [Reporting].[PowerPlan] AS
 
 DECLARE @DATEFUNCTION DATE = GETDATE()
 
+DROP TABLE [Reporting].[PowerPlan_BASE];
+
+
 BEGIN
 	SELECT 1
+
+	CREATE TABLE #TempTest
+	WITH (DISTRIBUTION = ROUND_ROBIN, HEAP) AS
+	(SELECT 2 AS Two,
+			3 AS Three, 
+			4 AS Four
+	)
 END
