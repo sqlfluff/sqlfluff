e437480cd49aaa222567aee7a8c2f3b0b4b7cd38
Barry Hart <barry.hart@mailchimp.com>
2021-10-12 20:07:18 -0400

    Update test to use the exact SQL from the issue

--- test/fixtures/dbt/dbt_project/models/my_new_project/issue_1608.sql
+++ test/fixtures/dbt/dbt_project/models/my_new_project/issue_1608.sql
@@ -1,2 +1,16 @@
-select
-    {{- echo('col_name') -}} as col_name2
+{{ config(materialized='view') }}
+
+with cte_example as (
+     select 1 as col_name
+),
+
+final as
+(
+    select
+        col_name,
+        {{- echo('col_name') -}} as col_name2
+    from
+        cte_example
+)
+
+select * from final
