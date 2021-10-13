cb0ee46a9d5ea9dd32f4832bb5bc7cd696826275
jpers36 <jpers36@gmail.com>
2021-10-13 12:22:00 -0400

    TSQL: Fix CASTing variables (#1627)
    * Fix DECLARE Delimitation
    
    * Fix DECLARE Delimitation
    
    * Fix DECLARE Delimitation
    
    * Fix CASTing a variable
    
    * Fix CASTing a variable
    
    Co-authored-by: jpersons <jpersons@iuhealth.org>
    Co-authored-by: Barry Pollard <barry@tunetheweb.com>

--- src/sqlfluff/dialects/dialect_tsql.py
+++ src/sqlfluff/dialects/dialect_tsql.py
@@ -96,6 +96,7 @@ tsql_dialect.replace(
         Ref("QuotedIdentifierSegment"),
         Ref("BracketedIdentifierSegment"),
         Ref("HashIdentifierSegment"),
+        Ref("ParameterNameSegment"),
     ),
     LiteralGrammar=OneOf(
         Ref("QuotedLiteralSegment"),
@@ -401,7 +402,11 @@ class DeclareStatementSegment(BaseSegment):
     type = "declare_segment"
     match_grammar = Sequence(
         "DECLARE",
-        Delimited(Ref("ParameterNameSegment")),
+        Ref("ParameterNameSegment"),
+        AnyNumberOf(
+            Ref("ParameterNameSegment"),
+            Ref("CommaSegment", optional=True),
+        ),
         Ref("DatatypeSegment"),
         Sequence(
             Ref("EqualsSegment"),
@@ -811,6 +816,18 @@ class ConvertFunctionNameSegment(BaseSegment):
 
 
 @tsql_dialect.segment()
+class CastFunctionNameSegment(BaseSegment):
+    """CAST function name segment.
+
+    Need to be able to specify this as type function_name
+    so that linting rules identify it properly
+    """
+
+    type = "function_name"
+    match_grammar = Sequence("CAST")
+
+
+@tsql_dialect.segment()
 class WithinGroupFunctionNameSegment(BaseSegment):
     """WITHIN GROUP function name segment.
 
@@ -916,6 +933,16 @@ class FunctionSegment(BaseSegment):
         ),
         Sequence(
             Sequence(
+                Ref("CastFunctionNameSegment"),
+                Bracketed(
+                    Ref("ExpressionSegment"),
+                    "AS",
+                    Ref("DatatypeSegment"),
+                ),
+            ),
+        ),
+        Sequence(
+            Sequence(
                 Ref("WithinGroupFunctionNameSegment"),
                 Bracketed(
                     Delimited(
@@ -935,6 +962,8 @@ class FunctionSegment(BaseSegment):
                 OneOf(
                     Ref("FunctionNameSegment"),
                     exclude=OneOf(
+                        # List of special functions handled differently
+                        Ref("CastFunctionNameSegment"),
                         Ref("ConvertFunctionNameSegment"),
                         Ref("DateAddFunctionNameSegment"),
                         Ref("WithinGroupFunctionNameSegment"),
