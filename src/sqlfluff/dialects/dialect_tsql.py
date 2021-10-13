c1a6b75a21b750988d6f90e4978ada0bebe8322a
jpersons <jpersons@iuhealth.org>
2021-10-13 16:54:14 -0400

    TSQL: Allow for multiple statements in a procedure

--- src/sqlfluff/dialects/dialect_tsql.py
+++ src/sqlfluff/dialects/dialect_tsql.py
@@ -723,9 +723,12 @@ class ProcedureDefinitionGrammar(BaseSegment):
     type = "procedure_statement"
     name = "procedure_statement"
 
-    match_grammar = OneOf(
-        Ref("StatementSegment"),
-        Ref("BeginEndSegment"),
+    match_grammar = AnyNumberOf(
+        OneOf(
+            Ref("BeginEndSegment"),
+            Ref("StatementSegment"),
+        ),
+        min_times=1,
     )
 
 
