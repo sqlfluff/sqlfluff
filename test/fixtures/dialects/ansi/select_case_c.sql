1b7c2416fc3d04f8d54c8b54033afd44ae7ceece
Barry <barry@tunetheweb.com>
2021-10-15 19:14:50 +0100

    Add more test cases

--- test/fixtures/dialects/ansi/select_case_c.sql
+++ test/fixtures/dialects/ansi/select_case_c.sql
@@ -9,6 +9,15 @@ from table1;
 
 select
     col0,
+    case
+        when col1
+        then col2
+        else col3
+    end::int::float as mycol
+from table1;
+
+select
+    col0,
     cast(case
         when col1
         then col2
