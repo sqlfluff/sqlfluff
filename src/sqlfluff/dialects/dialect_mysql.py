bbbd0fecce9a6ca71dc6f5496d3fc0b65355ecf7
fatelei <fatelei@gmail.com>
2021-10-24 15:20:03 +0000

    feat: enhance mysql drop index

--- src/sqlfluff/dialects/dialect_mysql.py
+++ src/sqlfluff/dialects/dialect_mysql.py
@@ -90,6 +90,14 @@ mysql_dialect.sets("reserved_keywords").update(
         "COLUMN_NAME",
         "CURSOR_NAME",
         "STACKED",
+        "ALGORITHM",
+        "LOCK",
+        "DEFAULT",
+        "INPLACE",
+        "COPY",
+        "NONE",
+        "SHARED",
+        "EXCLUSIVE",
     ]
 )
 
@@ -1242,14 +1250,31 @@ class CursorFetchSegment(BaseSegment):
 
 @mysql_dialect.segment(replace=True)
 class DropIndexStatementSegment(BaseSegment):
-    """A `DROP INDEX` statement."""
+    """A `DROP INDEX` statement.
+
+    https://dev.mysql.com/doc/refman/8.0/en/drop-index.html
+    """
 
     type = "drop_statement"
-    # DROP INDEX <Index name> [CONCURRENTLY] [IF EXISTS] {RESTRICT | CASCADE}
+    # DROP INDEX <Index name> ON <table_name>
+    # [ALGORITHM [=] {DEFAULT | INPLACE | COPY} | LOCK [=] {DEFAULT | NONE | SHARED | EXCLUSIVE}]
     match_grammar = Sequence(
         "DROP",
         "INDEX",
         Ref("IndexReferenceSegment"),
         "ON",
-        Ref("TableReferenceSegment")
+        Ref("TableReferenceSegment"),
+        OneOf(
+            Sequence(
+                "ALGORITHM",
+                Ref("EqualsSegment", optional=True),
+                OneOf("DEFAULT", "INPLACE", "COPY"),
+            ),
+            Sequence(
+                "LOCK",
+                Ref("EqualsSegment", optional=True),
+                OneOf("DEFAULT", "NONE", "SHARED", "EXCLUSIVE"),
+            ),
+            optional=True,
+        ),
     )
