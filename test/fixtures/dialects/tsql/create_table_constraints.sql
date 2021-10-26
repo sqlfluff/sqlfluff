d4b140cf8716a35ca7292142d99f216ef6c0f4c7
Nevado <18424398+nevado@users.noreply.github.com>
2021-10-26 15:05:32 +0100

    tsql - table constraints - add more tests

--- test/fixtures/dialects/tsql/create_table_constraints.sql
+++ test/fixtures/dialects/tsql/create_table_constraints.sql
@@ -4,6 +4,8 @@ CREATE TABLE [dbo].[example](
     [ColumnC] varchar(100) DEFAULT 'mydefault',
     [ColumnDecimal] DATE DEFAULT GETDATE(),
     [col1] int default ((-1)) not null,
+    [col1] int default (-1) not null,
+    [col1] int default -1 not null,
     [col1] INT DEFAULT (NULL) NULL,
     [col1] INT IDENTITY (1, 1) NOT NULL
     , PRIMARY KEY CLUSTERED ([col1] ASC)
