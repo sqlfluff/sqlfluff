465c55e92e4039f810c8cab85cb0ddbe15deb818
Nevado <18424398+nevado@users.noreply.github.com>
2021-10-26 16:10:17 +0100

    tsql - table constraints - remove one failing section

--- test/fixtures/dialects/tsql/create_table_constraints.sql
+++ test/fixtures/dialects/tsql/create_table_constraints.sql
@@ -6,7 +6,5 @@ CREATE TABLE [dbo].[example](
     [col1] int default ((-1)) not null,
     [col1] int default (-1) not null,
     [col1] int default -1 not null,
-    [col1] INT DEFAULT (NULL) NULL,
-    [col1] INT IDENTITY (1, 1) NOT NULL
-    , PRIMARY KEY CLUSTERED ([col1] ASC)
+    [col1] INT DEFAULT (NULL) NULL
 )
