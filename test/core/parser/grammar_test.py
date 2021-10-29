30f9d50da3df7b3fb86dccac07a601a227ff86b6
Danny Jones <danny.jones1994@live.com>
2021-10-29 18:08:08 +0100

    Added check that testenv is set to testenv-specific test

--- test/core/parser/grammar_test.py
+++ test/core/parser/grammar_test.py
@@ -27,6 +27,7 @@ from sqlfluff.core.parser.grammar import (
     Conditional,
 )
 from sqlfluff.core.errors import SQLParseError
+from os import getenv
 
 # NB: All of these tests depend somewhat on the KeywordSegment working as planned
 
@@ -479,8 +480,9 @@ def test__parser__grammar_sequence(seg_list, caplog):
     bs = StringParser("bar", KeywordSegment)
     fs = StringParser("foo", KeywordSegment)
     g = Sequence(bs, fs)
-    # Check that this is running in the test environment (env var SQLFLUFF_TESTENV=1)
-    assert g.test_env
+    # If running in the test environment, assert that Sequence recognises this
+    if getenv("SQLFLUFF_TESTENV", ""):
+        assert g.test_env
     gc = Sequence(bs, fs, allow_gaps=False)
     with RootParseContext(dialect=None) as ctx:
         with caplog.at_level(logging.DEBUG, logger="sqlfluff.parser"):
