f3f808050fee5b43e0ee0c58cc7b3f540b227ae0
Barry Pollard <barry@tunetheweb.com>
2021-11-01 22:31:28 +0000

    BigQuery: Support PIVOT and UNPIVOT (#1794)

---
+++ test/fixtures/dialects/bigquery/select_unpivot.sql
@@ -0,0 +1,13 @@
+WITH Produce AS (
+  SELECT 'Kale' as product, 51 as Q1, 23 as Q2, 45 as Q3, 3 as Q4 UNION ALL
+  SELECT 'Apple', 77, 0, 25, 2)
+SELECT * FROM Produce;
+
+SELECT * FROM Produce
+UNPIVOT(sales FOR quarter IN (Q1, Q2, Q3, Q4));
+
+SELECT * FROM Produce
+UNPIVOT(
+  (first_half_sales, second_half_sales)
+  FOR semesters
+  IN ((Q1, Q2) AS 'semester_1', (Q3, Q4) AS 'semester_2'));
