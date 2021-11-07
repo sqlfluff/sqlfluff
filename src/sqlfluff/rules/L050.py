64a75089942d36b2cd6959b4ee8dd09bb660b40d
jpy-git <josephyoung.jpy@gmail.com>
2021-11-07 20:49:41 +0000

    removed unintentional memory dict

--- src/sqlfluff/rules/L050.py
+++ src/sqlfluff/rules/L050.py
@@ -78,7 +78,6 @@ class Rule_L050(BaseRule):
             )
             & (not context.segment.is_expandable)
         ):
-            context.memory = {"first": True}
             return LintResult(
                 anchor=context.parent_stack[0],
                 fixes=[LintFix("delete", d) for d in context.raw_stack],
