3c141316e1936cdc6d917e81b190b2984f214d8a
jpy-git <josephyoung.jpy@gmail.com>
2021-11-08 10:28:20 +0000

    skip L050 for unaccounted source slice gaps

--- src/sqlfluff/rules/L050.py
+++ src/sqlfluff/rules/L050.py
@@ -1,6 +1,7 @@
 """Implementation of Rule L050."""
-from typing import Optional
+from typing import Optional, Tuple
 
+from sqlfluff.core.parser.segments.raw import RawSegment
 from sqlfluff.core.rules.base import BaseRule, LintResult, LintFix, RuleContext
 from sqlfluff.core.rules.doc_decorators import document_fix_compatible
 
@@ -59,6 +60,30 @@ class Rule_L050(BaseRule):
             foo
     """
 
+    @staticmethod
+    def _check_unaccounted_slice_gaps(raw_stack: Tuple[RawSegment, ...]) -> bool:
+        """Check for missing segments by comparing neighbouring source slices in the raw stack.
+
+        Returns:
+            :obj:`bool` indicating a missing segment discovered.
+
+        """
+        missing_segment_flag = False
+        for i, segment in enumerate(raw_stack):
+            if (i == 0) and (segment.pos_marker.source_slice.start != 0):
+                # First segment does not start at position 0.
+                missing_segment_flag = True
+            elif i > 0:
+                prior_segment = raw_stack[i - 1]
+                if (
+                    segment.pos_marker.source_slice.start
+                    != prior_segment.pos_marker.source_slice.stop
+                ):
+                    # There is an unaccounted for gap in source slices.
+                    missing_segment_flag = True
+
+        return missing_segment_flag
+
     def _eval(self, context: RuleContext) -> Optional[LintResult]:
         """Files must not begin with newlines or whitespace."""
         # If parent_stack is empty we are currently at FileSegment.
@@ -78,6 +103,13 @@ class Rule_L050(BaseRule):
             )
             & (not context.segment.is_expandable)
         ):
+            if self._check_unaccounted_slice_gaps(context.raw_stack):
+                # It is possible that a template segment (e.g. {{ config(materialized='view') }})
+                # renders to an empty string and as such is omitted from the parsed tree.
+                # We therefore should flag any unaccounted for gaps in the earlier source slices
+                # and skip this rule to avoid risking collisions with template objects.
+                return None
+
             return LintResult(
                 anchor=context.parent_stack[0],
                 fixes=[LintFix("delete", d) for d in context.raw_stack],
