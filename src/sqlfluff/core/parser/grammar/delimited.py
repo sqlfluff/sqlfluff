89648689e05ceec33b266e733d6b6639982c87ee
Danny Jones <danny.jones1994@live.com>
2021-11-13 17:27:56 +0000

    Added support for trailing delimiters, with future delimited sections

--- src/sqlfluff/core/parser/grammar/delimited.py
+++ src/sqlfluff/core/parser/grammar/delimited.py
@@ -167,7 +167,19 @@ class Delimited(OneOf):
                         )
                     # No match - Not allowed
                     if not match:
-                        return MatchResult.from_unmatched(mutated_segments)
+                        if self.allow_trailing:
+                            # If we reach this point, the lookahead match has hit a delimiter
+                            # beyond the scope of this Delimited section. Trailing delimiters are allowed,
+                            # so return matched up to this section.
+                            return MatchResult(
+                                matched_segments.matched_segments,
+                                pre_non_code
+                                + match.unmatched_segments
+                                + post_non_code
+                                + delimiter_match.all_segments(),
+                            )
+                        else:
+                            return MatchResult.from_unmatched(mutated_segments)
 
                     if not match.is_complete():
                         # If we reach this point, the lookahead match has hit a delimiter
