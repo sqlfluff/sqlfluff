6ece0e5ec8649d06156be6152e0e6637fcf3392a
jpy-git <josephyoung.jpy@gmail.com>
2021-11-13 22:22:40 +0000

    Incorporated fixes from #1894

--- test/fixtures/dialects/ansi/with_insert_with_statement.sql
+++ test/fixtures/dialects/ansi/with_insert_with_statement.sql
@@ -1,14 +1,22 @@
-WITH mycte AS (
+WITH mycte1 AS (
     SELECT
         foo,
         bar,
         baz
-    FROM mytable1
+    FROM mytable
 )
 
 INSERT INTO table2 (column1, column2, column3)
+WITH mycte2 AS (
+    SELECT
+        foo,
+        bar,
+        baz
+    FROM mycte1
+)
+
 SELECT
     foo,
     bar,
     baz
-FROM mycte;
+FROM mycte2;
