aedd9f07463553925e52b4583c2205abb09b419c
jpy-git <josephyoung.jpy@gmail.com>
2021-11-24 18:39:08 +0000

    Add ability to include and exclude rules via globs

--- src/sqlfluff/core/rules/base.py
+++ src/sqlfluff/core/rules/base.py
@@ -16,6 +16,7 @@ missing.
 
 import bdb
 import copy
+import fnmatch
 import logging
 import pathlib
 import re
@@ -652,6 +653,19 @@ class RuleSet:
         # Make sure we actually return the original class
         return cls
 
+    def _expand_config_rule_glob_list(self, glob_list: List[str]) -> List[str]:
+        """Expand a list of rule globs into a list of rule codes.
+        
+        Returns:
+            :obj:`list` of :obj:`str` rule codes.
+        
+        """
+        expanded_glob_list = []
+        for r in glob_list:
+            expanded_glob_list.extend(fnmatch.filter(self._register, r))
+
+        return expanded_glob_list
+
     def get_rulelist(self, config) -> List[BaseRule]:
         """Use the config to return the appropriate rules.
 
@@ -669,7 +683,7 @@ class RuleSet:
         blacklist = config.get("rule_blacklist") or []
 
         whitelisted_unknown_rule_codes = [
-            r for r in whitelist if r not in self._register
+            r for r in whitelist if not fnmatch.filter(self._register, r)
         ]
         if any(whitelisted_unknown_rule_codes):
             rules_logger.warning(
@@ -679,7 +693,7 @@ class RuleSet:
             )
 
         blacklisted_unknown_rule_codes = [
-            r for r in blacklist if r not in self._register
+            r for r in blacklist if not fnmatch.filter(self._register, r)
         ]
         if any(blacklisted_unknown_rule_codes):  # pragma: no cover
             rules_logger.warning(
@@ -689,8 +703,12 @@ class RuleSet:
             )
 
         keylist = sorted(self._register.keys())
+        # First we expand the whitelist and blacklist
+        expanded_whitelist = self._expand_config_rule_glob_list(whitelist)
+        expanded_blacklist = self._expand_config_rule_glob_list(blacklist)
+        
         # First we filter the rules
-        keylist = [r for r in keylist if r in whitelist and r not in blacklist]
+        keylist = [r for r in keylist if r in expanded_whitelist and r not in expanded_blacklist]
 
         # Construct the kwargs for instantiation before we actually do it.
         rule_kwargs = {}
