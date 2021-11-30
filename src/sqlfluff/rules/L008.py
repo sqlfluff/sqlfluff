8ce2eaf68e595106d9a5f3097b8a0d23301a4bcb
jpy-git <josephyoung.jpy@gmail.com>
2021-11-30 00:30:35 +0000

    Add memory to log previously fixed commas in L008

--- src/sqlfluff/rules/L008.py
+++ src/sqlfluff/rules/L008.py
@@ -40,29 +40,45 @@ class Rule_L008(BaseRule):
         This is a slightly odd one, because we'll almost always evaluate from a point a few places
         after the problem site. NB: We need at least two segments behind us for this to work.
         """
+        # Initialise the memory, we need to check that we haven't already flagged a comma before.
+        if not context.memory.get("L008_fixed_comma_segments"):
+            context.memory["L008_fixed_comma_segments"] = []
+
         if len(context.raw_stack) < 1:
-            return None
+            return LintResult(memory=context.memory)
 
         # Get the first element of this segment.
         first_elem = context.segment.get_raw_segments()[0]
 
         cm1 = context.raw_stack[-1]
+        if cm1 in context.memory["L008_fixed_comma_segments"]:
+            # We've fixed this comma before, we are just in a subsegment
+            # of the same segment that follows the comma.
+            return LintResult(memory=context.memory)
+
         if cm1.name == "comma":
             # comma followed by something that isn't whitespace?
             if first_elem.name not in ["whitespace", "newline", "Dedent"]:
                 self.logger.debug(
                     "Comma followed by something other than whitespace: %s", first_elem
                 )
+                context.memory["L008_fixed_comma_segments"].append(cm1)
                 ins = WhitespaceSegment(raw=" ")
                 return LintResult(
                     anchor=cm1,
                     fixes=[LintFix("edit", context.segment, [ins, context.segment])],
+                    memory=context.memory,
                 )
 
         if len(context.raw_stack) < 2:
-            return None
+            return LintResult(memory=context.memory)
 
         cm2 = context.raw_stack[-2]
+        if cm2 in context.memory["L008_fixed_comma_segments"]:
+            # We've fixed this comma before, we are just in a subsegment
+            # of the same segment that follows the comma.
+            return LintResult(memory=context.memory)
+
         if cm2.name == "comma":
             # comma followed by too much whitespace?
             if (
@@ -72,7 +88,12 @@ class Rule_L008(BaseRule):
                 and not first_elem.is_comment  # ...and not followed by a comment
             ):
                 self.logger.debug("Comma followed by too much whitespace: %s", cm1)
+                context.memory["L008_fixed_comma_segments"].append(cm2)
                 repl = WhitespaceSegment(raw=" ")
-                return LintResult(anchor=cm1, fixes=[LintFix("edit", cm1, repl)])
+                return LintResult(
+                    anchor=cm1,
+                    fixes=[LintFix("edit", cm1, repl)],
+                    memory=context.memory,
+                )
         # Otherwise we're fine
-        return None
+        return LintResult(memory=context.memory)
