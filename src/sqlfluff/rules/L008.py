4a0453268c59a6dc06fb0dc10f94fe8108e2a8e0
jpy-git <josephyoung.jpy@gmail.com>
2021-12-04 23:28:11 +0000

    Add new L008 logic

--- src/sqlfluff/rules/L008.py
+++ src/sqlfluff/rules/L008.py
@@ -1,10 +1,12 @@
 """Implementation of Rule L008."""
-from typing import Optional
+from typing import Optional, Tuple
 
 from sqlfluff.core.parser import WhitespaceSegment
 
 from sqlfluff.core.rules.base import BaseRule, LintResult, LintFix, RuleContext
 from sqlfluff.core.rules.doc_decorators import document_fix_compatible
+
+from sqlfluff.core.parser.segments.base import BaseSegment
 
 
 @document_fix_compatible
@@ -34,66 +36,85 @@ class Rule_L008(BaseRule):
         WHERE a IN ('plop',•'zoo')
     """
 
-    def _eval(self, context: RuleContext) -> Optional[LintResult]:
-        """Commas should be followed by a single whitespace unless followed by a comment.
+    def _get_subsequent_whitespace(
+        self,
+        context,
+    ) -> Tuple[Optional[BaseSegment], Optional[BaseSegment]]:
+        """Search forwards through the tree for subsequent whitespace.
 
-        This is a slightly odd one, because we'll almost always evaluate from a point a few places
-        after the problem site. NB: We need at least two segments behind us for this to work.
+        We navigate the parse tree to discover any trailing whitespace and
+        return a tuple of both the trailing whitespace segment and the
+        first non-whitespace segment discovered.
         """
-        # Initialise the memory, we need to check that we haven't already flagged a comma before.
-        if not context.memory.get("L008_fixed_comma_segments"):
-            context.memory["L008_fixed_comma_segments"] = []
+        # In this algorithm we traverse backwards up the parent stack looking
+        # at the siblings_post at each level to determine if the next segment
+        # is a whitespace and what the first non-whitespace segment is.
+        subsequent_whitespace = None
+        child = context.segment
+        for parent in context.parent_stack[::-1]:
+            next_child_index = parent.segments.index(child) + 1
+            if next_child_index == len(parent.segments):
+                # We are at the end of the current parents children.
+                # Continue up the parent stack.
+                child = parent
+                continue
+            # Similar to iterating over context.siblings_post
+            # but applied at each level up the parent stack.
+            for s in parent.segments[next_child_index:]:
+                if s.is_meta:
+                    continue
+                elif s.is_type("whitespace"):
+                    # Capture the whitespace segment.
+                    subsequent_whitespace = s
+                else:
+                    # We've found a non-whitespace (and non-meta) segment.
+                    # Therefore return the stored whitespace segment
+                    # and this segment for analysis.
+                    return subsequent_whitespace, s
+            # Current parent becomes the current child
+            # of the next parent stack level.
+            child = parent
 
-        if len(context.raw_stack) < 1:
-            return LintResult(memory=context.memory)
+        # If we find ourselves here it's all
+        # whitespace (or nothing) to the end of the file.
+        # Don't think there's any grammar that allows
+        # a comma as the last character in a file so
+        # this should never get triggered,
+        # but we handle just in case.
+        return subsequent_whitespace, None
 
-        # Get the first element of this segment.
-        first_elem = context.segment.get_raw_segments()[0]
+    def _eval(self, context: RuleContext) -> Optional[LintResult]:
+        """Commas should be followed by a single whitespace unless followed by a comment."""
+        # We only care about commas.
+        if not context.segment.name == "comma":
+            return None
 
-        cm1 = context.raw_stack[-1]
-        if cm1 in context.memory["L008_fixed_comma_segments"]:
-            # We've fixed this comma before, we are just in a subsegment
-            # of the same segment that follows the comma.
-            return LintResult(memory=context.memory)
+        # Get subsequent whitespace segment and the first non-whitespace segment.
+        subsequent_whitespace, first_non_whitespace = self._get_subsequent_whitespace(
+            context
+        )
 
-        if cm1.name == "comma":
-            # comma followed by something that isn't whitespace?
-            if first_elem.name not in ["whitespace", "newline", "Dedent"]:
-                self.logger.debug(
-                    "Comma followed by something other than whitespace: %s", first_elem
-                )
-                context.memory["L008_fixed_comma_segments"].append(cm1)
-                ins = WhitespaceSegment(raw=" ")
-                return LintResult(
-                    anchor=cm1,
-                    fixes=[LintFix("edit", context.segment, [ins, context.segment])],
-                    memory=context.memory,
-                )
+        if (
+            (subsequent_whitespace is None)
+            and (first_non_whitespace is not None)
+            and (not first_non_whitespace.is_type("newline"))
+        ):
+            # No trailing whitespace and not followed by a newline,
+            # therefore create a whitespace after the comma.
+            return LintResult(
+                anchor=first_non_whitespace,
+                fixes=[LintFix("create_after", context.segment, WhitespaceSegment())],
+            )
+        elif (
+            (subsequent_whitespace is not None)
+            and (subsequent_whitespace.raw != " ")
+            and (first_non_whitespace is not None)
+            and (not first_non_whitespace.is_comment)
+        ):
+            # Excess trailing whitespace therefore edit to only be one space long.
+            return LintResult(
+                anchor=subsequent_whitespace,
+                fixes=[LintFix("edit", subsequent_whitespace, WhitespaceSegment())],
+            )
 
-        if len(context.raw_stack) < 2:
-            return LintResult(memory=context.memory)
-
-        cm2 = context.raw_stack[-2]
-        if cm2 in context.memory["L008_fixed_comma_segments"]:
-            # We've fixed this comma before, we are just in a subsegment
-            # of the same segment that follows the comma.
-            return LintResult(memory=context.memory)
-
-        if cm2.name == "comma":
-            # comma followed by too much whitespace?
-            if (
-                cm1.is_whitespace  # Must be whitespace
-                and cm1.raw != " "  # ...and not a single one
-                and cm1.name != "newline"  # ...and not a newline
-                and not first_elem.is_comment  # ...and not followed by a comment
-            ):
-                self.logger.debug("Comma followed by too much whitespace: %s", cm1)
-                context.memory["L008_fixed_comma_segments"].append(cm2)
-                repl = WhitespaceSegment(raw=" ")
-                return LintResult(
-                    anchor=cm1,
-                    fixes=[LintFix("edit", cm1, repl)],
-                    memory=context.memory,
-                )
-        # Otherwise we're fine
-        return LintResult(memory=context.memory)
+        return None
