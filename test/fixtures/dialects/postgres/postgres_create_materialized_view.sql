3bfea271d6d1164450e38a7f4e6702480f82be01
Derick Lung'aho <83162+derickl@users.noreply.github.com>
2021-12-07 23:59:09 +0300

    fixes

--- test/fixtures/dialects/postgres/postgres_create_materialized_view.sql
+++ test/fixtures/dialects/postgres/postgres_create_materialized_view.sql
@@ -8,6 +8,16 @@ CREATE MATERIALIZED VIEW my_mat_view AS
     WHERE y = 'value'
 );
 
+CREATE MATERIALIZED VIEW IF NOT EXISTS my_mat_view AS
+(
+    SELECT
+        a,
+        b
+    FROM
+        my_table
+    WHERE y = 'value'
+);
+
 
 CREATE MATERIALIZED VIEW my_mat_view AS
 SELECT
@@ -17,12 +27,29 @@ FROM
     my_table
 WHERE y = 'value';
 
+CREATE MATERIALIZED VIEW IF NOT EXISTS my_mat_view AS
+SELECT
+    a,
+    b
+FROM
+    my_table
+WHERE y = 'value';
+
+
 CREATE MATERIALIZED VIEW my_mat_view AS
 SELECT
     a,
     b
 FROM
     my_table;
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS my_mat_view AS
+SELECT
+    a,
+    b
+FROM
+    my_table;
+
 -- SQL from issue #2039
 CREATE MATERIALIZED VIEW bar
 AS
@@ -32,8 +59,32 @@ AS
 )
 WITH NO DATA;
 
+CREATE MATERIALIZED VIEW IF NOT EXISTS bar
+AS
+(
+    SELECT col
+    FROM my_table
+)
+WITH NO DATA;
+
 
 CREATE MATERIALIZED VIEW my_mat_view
+USING heap
+TABLESPACE pg_default
+WITH (prop_a = 1, prob_b = 'some_value', prop_c = FALSE, prop_d)
+AS
+(
+    SELECT
+        a,
+        avg(b) AS my_avg,
+        count(*) AS my_count
+    FROM my_table
+    GROUP BY grp
+    HAVING col > 2
+)
+WITH DATA;
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS my_mat_view
 USING heap
 TABLESPACE pg_default
 WITH (prop_a = 1, prob_b = 'some_value', prop_c = FALSE, prop_d)
