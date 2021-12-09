bfa8419250dc1e7cb032038ea9eebeaac7c76c5d
jpy-git <josephyoung.jpy@gmail.com>
2021-12-09 18:26:58 +0000

    Add ability to use default and extra config files in simple API

--- src/sqlfluff/api/simple.py
+++ src/sqlfluff/api/simple.py
@@ -1,9 +1,48 @@
 """The simple public API methods."""
 
 from typing import Any, Dict, List, Optional
-from sqlfluff.core import Linter
-from sqlfluff.core import SQLBaseError
+from sqlfluff.core import (
+    dialect_selector,
+    FluffConfig,
+    Linter,
+    SQLBaseError,
+    SQLFluffUserError,
+)
 from sqlfluff.core.linter import ParsedString
+
+
+def get_simple_config(
+    dialect: str = "ansi",
+    rules: Optional[List[str]] = None,
+    exclude_rules: Optional[List[str]] = None,
+    extra_config_path: Optional[str] = None,
+    ignore_local_config: bool = False,
+) -> FluffConfig:
+    """Get a config object from simple API arguments."""
+    # Check the requested dialect exists and is valid.
+    try:
+        dialect_selector(dialect)
+    except SQLFluffUserError as err:
+        raise SQLFluffUserError(f"Error loading dialect '{dialect}': {str(err)}")
+    except KeyError:
+        raise SQLFluffUserError(f"Error: Unknown dialect '{dialect}'")
+
+    # Create overrides for simple API arguments.
+    overrides = {
+        "dialect": dialect,
+        "rules": ",".join(rules) if rules is not None else None,
+        "exclude_rules": ",".join(exclude_rules) if exclude_rules is not None else None,
+    }
+
+    # Instantiate a config object.
+    try:
+        return FluffConfig.from_root(
+            extra_config_path=extra_config_path,
+            ignore_local_config=ignore_local_config,
+            overrides=overrides,
+        )
+    except SQLFluffUserError as err:  # pragma: no cover
+        raise SQLFluffUserError(f"Error loading config: {str(err)}")
 
 
 class APIParsingError(ValueError):
@@ -22,6 +61,8 @@ def lint(
     dialect: str = "ansi",
     rules: Optional[List[str]] = None,
     exclude_rules: Optional[List[str]] = None,
+    extra_config_path: Optional[str] = None,
+    ignore_local_config: bool = False,
 ) -> List[Dict[str, Any]]:
     """Lint a sql string or file.
 
@@ -38,7 +79,14 @@ def lint(
     Returns:
         :obj:`list` of :obj:`dict` for each violation found.
     """
-    linter = Linter(dialect=dialect, rules=rules, exclude_rules=exclude_rules)
+    cfg = get_simple_config(
+        dialect=dialect,
+        rules=rules,
+        exclude_rules=exclude_rules,
+        extra_config_path=extra_config_path,
+        ignore_local_config=ignore_local_config,
+    )
+    linter = Linter(config=cfg)
 
     result = linter.lint_string_wrapped(sql)
     result_records = result.as_records()
@@ -51,6 +99,8 @@ def fix(
     dialect: str = "ansi",
     rules: Optional[List[str]] = None,
     exclude_rules: Optional[List[str]] = None,
+    extra_config_path: Optional[str] = None,
+    ignore_local_config: bool = False,
 ) -> str:
     """Fix a sql string or file.
 
@@ -67,14 +117,26 @@ def fix(
     Returns:
         :obj:`str` for the fixed sql if possible.
     """
-    linter = Linter(dialect=dialect, rules=rules, exclude_rules=exclude_rules)
+    cfg = get_simple_config(
+        dialect=dialect,
+        rules=rules,
+        exclude_rules=exclude_rules,
+        extra_config_path=extra_config_path,
+        ignore_local_config=ignore_local_config,
+    )
+    linter = Linter(config=cfg)
 
     result = linter.lint_string_wrapped(sql, fix=True)
     fixed_string = result.paths[0].files[0].fix_string()[0]
     return fixed_string
 
 
-def parse(sql: str, dialect: str = "ansi") -> ParsedString:
+def parse(
+    sql: str,
+    dialect: str = "ansi",
+    extra_config_path: Optional[str] = None,
+    ignore_local_config: bool = False,
+) -> ParsedString:
     """Parse a sql string or file.
 
     Args:
@@ -86,7 +148,13 @@ def parse(sql: str, dialect: str = "ansi") -> ParsedString:
     Returns:
         :obj:`ParsedString` containing the parsed structure.
     """
-    linter = Linter(dialect=dialect)
+    cfg = get_simple_config(
+        dialect=dialect,
+        extra_config_path=extra_config_path,
+        ignore_local_config=ignore_local_config,
+    )
+    linter = Linter(config=cfg)
+
     parsed = linter.parse_string(sql)
     # If we encounter any parsing errors, raise them in a combined issue.
     if parsed.violations:
