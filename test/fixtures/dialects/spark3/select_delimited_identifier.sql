e69e6d8ced037baefee6a37f6c4c3cc6ead6b229
mcannamela <michael.cannamela@hasbro.com>
2021-12-10 13:31:14 -0500

    Add tests for parsing identifiers in spark3
    In Spark SQL, backticks are used for delimited identifiers:
    https://spark.apache.org/docs/latest/sql-ref-identifier.html#delimited-identifier
    
    The test with a plain identifier passess but using backticks fails to
    parse.

---
+++ test/fixtures/dialects/spark3/select_delimited_identifier.sql
@@ -0,0 +1,3 @@
+select 1 as `delimited_identifer`;
+select 2 as `% unruly-identifier`;
+select `delimited_identifier` from `delimited_table`;
---
+++ test/fixtures/dialects/spark3/select_plain_identifier.sql
@@ -0,0 +1,2 @@
+select 1 as regular_identifier;
+select plain_identifier from plain_table;
