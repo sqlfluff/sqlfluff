2194b43cbf2afece72a512867dc49d24dc970eb2
jpy-git <josephyoung.jpy@gmail.com>
2021-12-11 19:11:10 +0000

    Add unit tests

--- test/fixtures/dialects/tsql/comment_blocks.sql
+++ test/fixtures/dialects/tsql/comment_blocks.sql
@@ -12,3 +12,15 @@ nest nest /* */*/*/*/nest*/*/*/ FROM bar
 
 -- A recursive block comment (fun patternwise - no actual recursion going on ofc)
 /* A block comment looks like /* A block comment looks like /* A block comment looks like /* A block comment looks like /* A block comment looks like /* ... */ */ */ */ */ */
+
+-- Test cases from #2086.
+/**
+**/
+
+/**
+(
+**/
+
+/**
+'
+**/
