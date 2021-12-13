42224b2a09dbea7a823f064d3c85b9b51020cbc1
mcannamela <michael.cannamela@hasbro.com>
2021-12-13 13:25:55 -0500

    Don't address CTE for now

--- test/fixtures/dialects/spark3/select_delimited_identifier_from_cte.sql
+++
@@ -1,8 +0,0 @@
-with `a``b!` as (
-    select 1 as _identifier_1,
-    random() as `100% questionable ``identifier`
-),
-a0b as (
-    select * from `a``b!`
-)
-select _identifier_1, `100% questionable ``identifier` from a0b;
