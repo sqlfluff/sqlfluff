2aa85f3d53dd4b8f15e922a245de51bb53c1eacc
jpy-git <josephyoung.jpy@gmail.com>
2021-12-14 00:35:36 +0000

    Add negative lookahead to stop numeric literal from splitting valid naked identifiers

---
+++ test/fixtures/dialects/ansi/naked_identifiers.sql
@@ -0,0 +1,4 @@
+-- A valid identifier is alphanumeric and contains at least one letter.
+select "a" as 0_is_a_legal_identifier;
+select "a" as _is_a_legal_identifier;
+select "a" as is_a_legal_identifier0;
