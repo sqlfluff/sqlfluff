f41c5d16320595f37df1a3940a9849539b76a48f
jpers36 <jpers36@gmail.com>
2021-12-19 10:26:38 -0500

    Lint spaces in qualified names (#2130)
    * L039: Add logic to flag and remove spaces within references
    
    * L039: Add logic to flag and remove spaces within references
    
    * L039: Add logic to flag and remove spaces within references
    
    * L039: Add logic to flag and remove spaces within references
    
    * Update src/sqlfluff/rules/L039.py
    
    Co-authored-by: Joseph Young <80432516+jpy-git@users.noreply.github.com>
    
    * Update src/sqlfluff/rules/L039.py
    
    Co-authored-by: Joseph Young <80432516+jpy-git@users.noreply.github.com>
    
    * Update src/sqlfluff/rules/L039.py
    
    Co-authored-by: Joseph Young <80432516+jpy-git@users.noreply.github.com>
    
    * L039: Add logic to flag and remove spaces within references
    
    * L039: Add logic to flag and remove spaces within references
    
    Co-authored-by: jpersons <jpersons@iuhealth.org>
    Co-authored-by: Joseph Young <80432516+jpy-git@users.noreply.github.com>

--- src/sqlfluff/rules/L039.py
+++ src/sqlfluff/rules/L039.py
@@ -66,4 +66,22 @@ class Rule_L039(BaseRule):
                         )
                 prev_newline = False
                 prev_whitespace = None
+
+            if seg.is_type("object_reference"):
+                # This variable is a workaround to avoid removing new indents added at the beginning of a segment by L003.
+                # See Github issue #1304: https://github.com/sqlfluff/sqlfluff/issues/1304
+                # It represents the question: are we parsing through leading whitespace in this loop?
+                leading_whitespace = True
+                for child_seg in seg.get_raw_segments():
+                    if child_seg.is_whitespace:
+                        if not leading_whitespace:
+                            violations.append(
+                                LintResult(
+                                    anchor=child_seg,
+                                    fixes=[LintFix.delete(child_seg)],
+                                )
+                            )
+                    else:
+                        leading_whitespace = False
+
         return violations or None
