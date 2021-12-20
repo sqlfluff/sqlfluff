c73c32f99b2b0eb0fca53e92975630d29da3aa2f
jpy-git <josephyoung.jpy@gmail.com>
2021-12-20 19:57:03 +0000

    Fix L009 predicate implementation

--- src/sqlfluff/rules/L009.py
+++ src/sqlfluff/rules/L009.py
@@ -95,7 +95,7 @@ class Rule_L009(BaseRule):
         # Find the trailing newline segments.
         trailing_newlines = reversed_complete_stack.select(
             select_if=[sp.is_type("newline")],
-            loop_while=[sp.or_(sp.is_whitespace, sp.is_meta)],
+            loop_while=[sp.or_(sp.is_whitespace, sp.is_type("dedent"))],
         )
 
         if len(trailing_newlines) == 1:
