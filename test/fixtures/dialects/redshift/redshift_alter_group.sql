608fea6ee15189d13a87f8ad1922f1644346a935
Joseph Young <80432516+jpy-git@users.noreply.github.com>
2021-12-21 13:04:44 +0000

    Add ALTER USER and ALTER GROUP to redshift dialect (#2131)
    * Add alter user/group to redshift dialect
    
    * Update src/sqlfluff/dialects/dialect_redshift.py
    
    Co-authored-by: Barry Pollard <barry_pollard@hotmail.com>
    
    Co-authored-by: Barry Pollard <barry@tunetheweb.com>
    Co-authored-by: Barry Pollard <barry_pollard@hotmail.com>

---
+++ test/fixtures/dialects/redshift/redshift_alter_group.sql
@@ -0,0 +1,12 @@
+alter group admin_group
+add user dwuser;
+alter group admin_group
+add user dwuser1, dwuser2;
+
+alter group admin_group
+drop user dwuser;
+alter group admin_group
+drop user dwuser1, dwuser2;
+
+alter group admin_group
+rename to administrators;
