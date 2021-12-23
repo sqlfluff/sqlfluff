a171eeff3ee484b3ab346947192a61bde471c0d2
jpersons <jpersons@iuhealth.org>
2021-12-23 16:03:53 -0500

    Lexer: add non-breaking spaces to whitespace

--- src/sqlfluff/dialects/dialect_ansi.py
+++ src/sqlfluff/dialects/dialect_ansi.py
@@ -58,7 +58,7 @@ ansi_dialect = Dialect("ansi", root_segment_name="FileSegment")
 
 ansi_dialect.set_lexer_matchers(
     [
-        RegexLexer("whitespace", r"[\t\xc2\xa0 ]+", WhitespaceSegment),
+        RegexLexer("whitespace", r"[^\S\r\n]+", WhitespaceSegment),
         RegexLexer(
             "inline_comment",
             r"(--|#)[^\n]*",
