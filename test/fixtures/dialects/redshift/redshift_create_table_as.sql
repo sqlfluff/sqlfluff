a22bd99870a21e9068e290a0e0a4ddd2e0924e13
tdstark <tomdstark@gmail.com>
2021-12-27 13:13:27 -0500

    Adding CREATE TABLE AS tests

---
+++ test/fixtures/dialects/redshift/redshift_create_table_as.sql
@@ -0,0 +1,74 @@
+CREATE TEMP TABLE t1 AS (
+    SELECT something
+    FROM t2
+);
+
+CREATE TEMP TABLE t1 AS
+    SELECT something
+    FROM t2
+;
+
+CREATE TEMPORARY TABLE t1 AS
+    SELECT something
+    FROM t2
+;
+
+CREATE TABLE t1 AS (
+    SELECT something
+    FROM t2
+);
+
+CREATE TABLE t1 AS
+    SELECT something
+    FROM t2
+;
+
+CREATE LOCAL TEMP TABLE t1 AS
+    SELECT something
+    FROM t2
+;
+
+CREATE TEMP TABLE t1
+SORTKEY(col1) AS
+SELECT
+    col1
+FROM t2
+;
+
+CREATE TABLE t1
+SORTKEY(col1) AS
+SELECT
+    col1
+FROM t2
+;
+
+CREATE TABLE t1
+DISTKEY(col1) AS
+SELECT
+    col1
+FROM t2
+;
+
+CREATE TABLE t1
+DISTKEY(col1)
+SORTKEY(col1) AS
+SELECT
+    col1
+FROM t2
+;
+
+CREATE TABLE t1
+DISTSTYLE EVEN AS
+SELECT
+    col1
+FROM t2
+;
+
+CREATE TABLE t1
+DISTSTYLE ALL
+DISTKEY(col1)
+SORTKEY(col1) AS
+SELECT
+    col1
+FROM t2
+;
