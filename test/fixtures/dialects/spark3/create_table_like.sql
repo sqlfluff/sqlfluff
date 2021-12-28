89024cc223b63256273ace0fd5c89972e183ce1f
R7L208 <22798188+R7L208@users.noreply.github.com>
2021-12-28 12:44:52 -0500

    create table like synax

--- test/fixtures/dialects/spark3/create_table_like.sql
+++ test/fixtures/dialects/spark3/create_table_like.sql
@@ -1,22 +1,22 @@
 -- Create Table Like with all optional syntax
 CREATE TABLE IF NOT EXISTS table_identifier LIKE source_table_identifier
-    USING PARQUET
-    ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
-    STORED AS PARQUET
-    TBLPROPERTIES ( "key1" = "val1", "key2" = "val2")
-    LOCATION "path/to/files"
+USING PARQUET
+ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
+STORED AS PARQUET
+TBLPROPERTIES ( "key1" = "val1", "key2" = "val2")
+LOCATION "path/to/files";
 
 -- Create table using an existing table
-CREATE TABLE Student_Dupli like Student;
+CREATE TABLE student_dupli LIKE student;
 
 -- Create table like using a data source
-CREATE TABLE Student_Dupli like Student USING CSV;
+CREATE TABLE student_dupli LIKE student USING CSV;
 
 -- Table is created as external table at the location specified
-CREATE TABLE Student_Dupli like Student location  '/root1/home';
+CREATE TABLE student_dupli LIKE student LOCATION '/root1/home';
 
 -- Create table like using a rowformat
-CREATE TABLE Student_Dupli like Student
-    ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
-    STORED AS TEXTFILE
-    TBLPROPERTIES ('owner'='xxxx');
+CREATE TABLE student_dupli LIKE student
+ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
+STORED AS TEXTFILE
+TBLPROPERTIES ('owner' = 'xxxx');
