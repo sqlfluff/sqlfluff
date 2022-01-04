a7f2dd280750c86add683e02f402aef65e586371
Albert Yue <albert.yue@gotinder.com>
2022-01-03 16:52:08 -0800

    redshift create external tests

---
+++ test/fixtures/dialects/redshift/redshift_create_external_table_as.sql
@@ -0,0 +1,51 @@
+CREATE EXTERNAL TABLE
+external_schema.table_name
+STORED AS PARQUET
+LOCATION 's3://bucket/folder/'
+AS
+SELECT col1, col2 FROM external_schema.source_table
+;
+
+CREATE EXTERNAL TABLE
+external_schema.table_name
+STORED AS PARQUET
+LOCATION 's3://bucket/folder/'
+AS
+SELECT * FROM external_schema.source_table
+;
+
+CREATE EXTERNAL TABLE
+external_schema.table_name
+PARTITIONED BY (col1 integer)
+STORED AS PARQUET
+LOCATION 's3://bucket/folder/'
+AS
+SELECT col1, col2 FROM external_schema.source_table
+;
+
+CREATE EXTERNAL TABLE
+external_schema.table_name
+PARTITIONED BY (col1 integer)
+STORED AS PARQUET
+LOCATION 's3://bucket/folder/'
+AS
+SELECT * FROM external_schema.source_table
+;
+
+CREATE EXTERNAL TABLE
+external_schema.table_name
+PARTITIONED BY (col1 integer, col2 integer)
+STORED AS PARQUET
+LOCATION 's3://bucket/folder/'
+AS
+SELECT col1, col2 FROM external_schema.source_table
+;
+
+CREATE EXTERNAL TABLE
+external_schema.table_name
+PARTITIONED BY (col1 integer, col2 integer)
+STORED AS PARQUET
+LOCATION 's3://bucket/folder/'
+AS
+SELECT * FROM external_schema.source_table
+;
