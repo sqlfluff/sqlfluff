554b35a3f629913bf6c32eef7e8009bef47480ff
mcannamela <michael.cannamela@hasbro.com>
2022-01-04 13:46:40 -0500

    also one column cases can fail

---
+++ test/fixtures/dialects/spark3/select_from_values_one_column.sql
@@ -0,0 +1,2 @@
+select * from values 1,2,3;
+select * from (values 1,2,3);
--- test/fixtures/dialects/spark3/select_from_values_one_record.sql
+++ test/fixtures/dialects/spark3/select_from_values_one_record.sql
@@ -1,1 +1,4 @@
+select * from values 1;
+select * from (values 1);
 select * from values (1, 2);
+select * from (values (1, 2));
