7652ce9f8db392e04dd3e78f8e1bb703f72920fb
Albert Yue <albert.yue@gotinder.com>
2022-01-05 19:02:57 -0800

    update tests

--- test/fixtures/dialects/redshift/redshift_create_external_table_as.sql
+++ test/fixtures/dialects/redshift/redshift_create_external_table_as.sql
@@ -49,3 +49,13 @@ LOCATION 's3://bucket/folder/'
 AS
 SELECT * FROM external_schema.source_table
 ;
+
+CREATE EXTERNAL TABLE
+external_schema.table_name
+PARTITIONED BY (col1 integer, col2 integer)
+STORED AS PARQUET
+LOCATION 's3://bucket/folder/'
+AS (
+    SELECT col1, col2 FROM external_schema.source_table
+)
+;
