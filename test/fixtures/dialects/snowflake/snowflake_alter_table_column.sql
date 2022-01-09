f6a924c08e1bb77ef7d68ee5b6580c7190fd98c7
Kenny Wong <wong@coda.io>
2022-01-09 16:00:50 -0500

    Snowflake: Added `AlterTableStatement` specific for Snowflake
    * First commit to get AlterTableStatement fully supported with Snowflake's
      grammar.
    * Cleaned up `snowflake_alter_table_column` test cases.
    * Added test cases for all the ALTER TABLE supported at the moment.
    * Use `ColumnRefSegment` when appropriate.

--- test/fixtures/dialects/snowflake/snowflake_alter_table_column.sql
+++ test/fixtures/dialects/snowflake/snowflake_alter_table_column.sql
@@ -1,32 +1,35 @@
+-- SET/DROP column properties
+-- Single column
 alter table t1 alter column c1 drop not null;
-
-alter table t1 modify c2 drop default, c3 set default seq5.nextval ;
-
-alter table t1 alter c4 set data type varchar(50), column c4 drop default;
-
 alter table t1 alter c5 comment '50 character column';
 
-ALTER TABLE xxxx.example_table MODIFY COLUMN employeeCode SET MASKING POLICY example_MASKING_POLICY;
+-- Multiple columns/properties
+alter table t1 modify c2 drop default, c3 set default seq5.nextval ;
+alter table t1 alter c4 set data type varchar(50), column c4 drop default;
 
+
+-- SET Masking Policy
+-- Single column
+ALTER TABLE xxxx.example_table MODIFY COLUMN employeeCode SET MASKING POLICY example_MASKING_POLICY;
 alter table empl_info modify column empl_id set masking policy mask_empl_id;
 
--- multiple columns
-
+-- Multiple columns
 alter table empl_info modify
     column empl_id set masking policy mask_empl_id
    , column empl_dob set masking policy mask_empl_dob
 ;
 
--- single column
 
+-- UNSET masking policy
+-- Single column
 alter table empl_info modify column empl_id unset masking policy;
 
--- multiple columns
-
+-- Multiple columns
 alter table empl_info modify
     column empl_id unset masking policy
   , column empl_dob unset masking policy
 ;
 
-alter table test_table column test_column;
-alter table test_schema.test_table column test_column;
+-- DROP COLUMN
+ALTER TABLE empl_info DROP COLUMN my_column;
+ALTER TABLE some_schema.empl_info DROP COLUMN my_column;
