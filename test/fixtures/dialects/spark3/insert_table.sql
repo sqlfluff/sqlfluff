977257de7ffec470591773c4e6f6415835e88450
R7L208 <22798188+R7L208@users.noreply.github.com>
2022-01-11 18:16:55 -0500

    update test files
    add overwrite test cases
    
    rename file to better reflect usage

--- test/fixtures/dialects/spark3/insert_table.sql
+++ test/fixtures/dialects/spark3/insert_table.sql
@@ -1,9 +1,19 @@
 -- Single Row Insert Using a VALUES Clause
+INSERT INTO TABLE students VALUES
+('Amy Smith', '123 Park Ave, San Jose', 111111);
+
 INSERT INTO students VALUES
+('Amy Smith', '123 Park Ave, San Jose', 111111);
+
+INSERT OVERWRITE students VALUES
 ('Amy Smith', '123 Park Ave, San Jose', 111111);
 
 -- Multi-Row Insert Using a VALUES Clause
 INSERT INTO students VALUES
+('Bob Brown', '456 Taylor St, Cupertino', 222222),
+('Cathy Johnson', '789 Race Ave, Palo Alto', 333333);
+
+INSERT OVERWRITE students VALUES
 ('Bob Brown', '456 Taylor St, Cupertino', 222222),
 ('Cathy Johnson', '789 Race Ave, Palo Alto', 333333);
 
@@ -16,12 +26,28 @@ SELECT
 -- FROM persons WHERE name = "Dora Williams";
 FROM persons WHERE name = 'Dora Williams';
 
+INSERT OVERWRITE students PARTITION (student_id = 444444)
+SELECT
+    name,
+    address
+-- TODO double quotes creates an unparsable section
+-- FROM persons WHERE name = "Dora Williams";
+FROM persons WHERE name = 'Dora Williams';
+
 -- Insert Using a TABLE Statement
 INSERT INTO students TABLE visiting_students;
 
--- TODO
+INSERT OVERWRITE students TABLE visiting_students;
+
 -- Insert Using a FROM Statement
 INSERT INTO students
+FROM applicants
+SELECT
+--name, address, id
+applicants
+WHERE qualified = TRUE;
+
+INSERT OVERWRITE students
 FROM applicants
 SELECT
 --name, address, id
@@ -32,6 +58,13 @@ WHERE qualified = TRUE;
 INSERT INTO students PARTITION (birthday = DATE'2019-01-02')
 VALUES ('Amy Smith', '123 Park Ave, San Jose');
 
+INSERT OVERWRITE students PARTITION (birthday = DATE'2019-01-02')
+VALUES ('Amy Smith', '123 Park Ave, San Jose');
+
 -- Insert with both a partition spec and a column list
 INSERT INTO students PARTITION (student_id = 11215017) (address, name) VALUES
 ('Hangzhou, China', 'Kent Yao Jr.');
+
+INSERT OVERWRITE students
+PARTITION (student_id = 11215017) (address, name)
+VALUES ('Hangzhou, China', 'Kent Yao Jr.');
