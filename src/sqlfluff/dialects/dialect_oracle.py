4c2cad833c0aea187f5b46409ea0b02eac9da9c9
Raphael Deem <rdeem@linkedin.biz>
2022-01-12 16:50:46 -0800

    fix flake8

--- src/sqlfluff/dialects/dialect_oracle.py
+++ src/sqlfluff/dialects/dialect_oracle.py
@@ -3,17 +3,11 @@
 This inherits from the ansi dialect.
 """
 from sqlfluff.core.dialects import load_raw_dialect
-from sqlfluff.core.parser.segments import NewlineSegment
-from sqlfluff.core.parser.segments.raw import KeywordSegment
 from sqlfluff.core.parser import (
-    Anything,
     BaseSegment,
-    CommentSegment,
     Ref,
     Sequence,
     StartsWith,
-    StringParser,
-    OptionallyBracketed,
     OneOf,
 )
 
@@ -22,6 +16,7 @@ oracle_dialect = ansi_dialect.copy_as("oracle")
 
 oracle_dialect.sets("unreserved_keywords").difference_update(["COMMENT"])
 oracle_dialect.sets("reserved_keywords").update(["COMMENT", "ON"])
+
 
 # Adding Oracle specific statements.
 @oracle_dialect.segment(replace=True)
@@ -38,17 +33,19 @@ class StatementSegment(BaseSegment):
 
     match_grammar = ansi_dialect.get_segment("StatementSegment").match_grammar.copy()
 
+
 @oracle_dialect.segment()
 class CommentStatementSegment(BaseSegment):
     """A `Comment` statement.
+
     COMMENT [text]
     https://docs.oracle.com/cd/B19306_01/server.102/b14200/statements_4009.htm
     """
-    
+
     type = "comment_statement"
-    
+
     match_grammar = StartsWith(Sequence("COMMENT", "ON"))
-    
+
     parse_grammar = Sequence(
         "COMMENT",
         "ON",
@@ -69,10 +66,10 @@ class CommentStatementSegment(BaseSegment):
                     "OPERATOR",
                     Ref("OperatorReferenceSegment"),
                 ),
-                #Sequence(
-                #    "INDEXTYPE",
-                #    Ref("IndexTypeReferenceSegment"),
-                #),
+                # Sequence(
+                #     "INDEXTYPE",
+                #     Ref("IndexTypeReferenceSegment"),
+                # ),
                 Sequence(
                     "MATERIALIZED",
                     "VIEW",
