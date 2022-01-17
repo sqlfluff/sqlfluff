7dbebff07cc32b9cd325daab83074856b3bcd541
jpy-git <josephyoung.jpy@gmail.com>
2022-01-17 10:35:26 +0000

    Snowflake ALTER TABLE: Add searchOptimizationAction

--- test/fixtures/dialects/snowflake/snowflake_alter_table.sql
+++ test/fixtures/dialects/snowflake/snowflake_alter_table.sql
@@ -2,6 +2,10 @@ ALTER TABLE my_old_table RENAME TO my_new_table;
 
 ALTER TABLE my_existing_table SWAP WITH my_another_table;
 
+ALTER TABLE my_existing_table ADD SEARCH OPTIMIZATION;
+
+ALTER TABLE my_existing_table DROP SEARCH OPTIMIZATION;
+
 ALTER TABLE my_table SET DATA_RETENTION_TIME_IN_DAYS = 30;
 
 ALTER TABLE my_table SET DEFAULT_DDL_COLLATION = 'en-ci';
