d974cdaa1c49090fa1460c10d2b9d9ae69cd5fb8
rpr-ableton <57257087+rpr-ableton@users.noreply.github.com>
2022-01-20 14:35:53 +0100

    Complete Redshift CREATE EXTERNAL TABLE support (#2354)
    * Complete Redshift create external table support
    
    * leverage RowFormatDelimitedSegment
    
    Co-authored-by: Barry Pollard <barry@tunetheweb.com>

--- test/fixtures/dialects/redshift/redshift_create_external_table_as.sql
+++ test/fixtures/dialects/redshift/redshift_create_external_table_as.sql
@@ -90,3 +90,12 @@ TABLE PROPERTIES ('some_property1'='some_value1', 'some_property2'='some_value2'
 AS
 SELECT col1, col2 FROM external_schema.source_table
 ;
+
+CREATE EXTERNAL TABLE spectrum.partitioned_lineitem
+PARTITIONED BY (l_shipdate date, l_shipmode varchar(24))
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n\l'
+STORED AS textfile
+LOCATION 'S3://mybucket/cetas/partitioned_lineitem/'
+AS SELECT l_orderkey, l_shipmode, l_shipdate, l_partkey FROM local_table;
