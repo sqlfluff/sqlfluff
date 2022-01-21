6dbb5cd302df4b4cf02e38077c42b67fef5f5198
jpy-git <josephyoung.jpy@gmail.com>
2022-01-21 17:40:36 +0000

    Fix unit test

--- test/fixtures/dialects/mysql/values_statement.sql
+++ test/fixtures/dialects/mysql/values_statement.sql
@@ -2,4 +2,4 @@ VALUES ROW ('a', 1), ROW ('b', 2);
 
 VALUES ROW ('a', 1), ROW (upper('b'), 2+1);
 
-VALUES ROW (CURRENT_DATE, 1), ROW (INTERVAL 10 MINUTE, 2+1);
+VALUES ROW (CURRENT_DATE, '2020-06-04' + interval -5 day);
