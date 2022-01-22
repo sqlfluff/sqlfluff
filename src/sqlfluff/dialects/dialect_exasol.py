d2197f0bc880366274213ba9c7f26f328618e399
Timo <sht.online@gmail.com>
2022-01-22 22:32:29 +0100

    Fix `LOCAL.ALIAS` Syntax

--- src/sqlfluff/dialects/dialect_exasol.py
+++ src/sqlfluff/dialects/dialect_exasol.py
@@ -104,11 +104,7 @@ exasol_dialect.patch_lexer_matchers(
     ]
 )
 
-# Access column aliases by using the LOCAL keyword
 exasol_dialect.add(
-    LocalIdentifierSegment=StringParser(
-        "LOCAL", KeywordSegment, name="local_identifier", type="identifier"
-    ),
     UDFParameterDotSyntaxSegment=NamedParser(
         "udf_param_dot_syntax", SymbolSegment, type="identifier"
     ),
@@ -211,7 +207,6 @@ exasol_dialect.add(
 
 exasol_dialect.replace(
     SingleIdentifierGrammar=OneOf(
-        Ref("LocalIdentifierSegment"),
         Ref("NakedIdentifierSegment"),
         Ref("QuotedIdentifierSegment"),
         Ref("EscapedIdentifierSegment"),
@@ -552,7 +547,7 @@ class GroupByClauseSegment(BaseSegment):
                 Ref("ExpressionSegment"),
                 Ref("CubeRollupClauseSegment"),
                 Ref("GroupingSetsClauseSegment"),
-                Bracketed(),  # Allows empty parentheses
+                Bracketed(),  # Allows empty parentheses,
             ),
             terminator=OneOf(
                 Sequence("ORDER", "BY"),
@@ -666,6 +661,23 @@ class LimitClauseSegment(BaseSegment):
             ),
         ),
     )
+
+
+@exasol_dialect.segment(replace=True)
+class LocalAliasSegment(BaseSegment):
+    """The `LOCAL.ALIAS` syntax allows to use a alias name of a column within clauses.
+
+    E.g.
+    `SELECT ABS(x) AS x FROM t WHERE local.x>10`
+
+    This is supported by: `SELECT`, `WHERE`, `GROUP BY`, `ORDER BY`, `HAVING`, `QUALIFY`
+
+    Note: it's not necessary to use `LOCAL` within `ÒRDER BY` and `QUALIFY` because the
+    alias could be accessed directly (...but we can).
+    """
+
+    type = "local_alias_segment"
+    match_grammar = Sequence("LOCAL", Ref("DotSegment"), Ref("SingleIdentifierGrammar"))
 
 
 ############################
