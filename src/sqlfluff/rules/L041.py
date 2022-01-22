f1dba0e1dd764ae72d67c3d5e1471cf14d3db030
Barry Pollard <barry@tunetheweb.com>
2022-01-22 12:35:28 +0000

    Fix code formatting in Rule docs (#2418)

--- src/sqlfluff/rules/L041.py
+++ src/sqlfluff/rules/L041.py
@@ -10,7 +10,7 @@ from sqlfluff.core.rules.functional import sp
 
 @document_fix_compatible
 class Rule_L041(BaseRule):
-    """``SELECT`` modifiers (e.g.``DISTINCT``) must be on the same line as ``SELECT``.
+    """``SELECT`` modifiers (e.g. ``DISTINCT``) must be on the same line as ``SELECT``.
 
     | **Anti-pattern**
 
