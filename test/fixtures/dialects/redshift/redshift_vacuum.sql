c13dad408f8b1976edf88dab5afcfee5c5e0ccf7
rpr <remy.prioul@ableton.com>
2022-01-23 14:12:55 +0100

    expand test suite

--- test/fixtures/dialects/redshift/redshift_vacuum.sql
+++ test/fixtures/dialects/redshift/redshift_vacuum.sql
@@ -4,6 +4,8 @@ vacuum sales;
 
 vacuum sales to 100 percent;
 
+vacuum recluster sales;
+
 vacuum sort only sales to 75 percent;
 
 vacuum delete only sales to 75 percent;
@@ -11,3 +13,5 @@ vacuum delete only sales to 75 percent;
 vacuum reindex listing;
 
 vacuum reindex listing to 75 percent;
+
+vacuum listing to 75 percent BOOST;
