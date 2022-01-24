c00bd67b74fd8c774d3ff33f5cb6791ed6a08b2a
rpr <remy.prioul@ableton.com>
2022-01-24 11:58:20 +0100

    add a test statement with two types of commas

--- test/fixtures/dialects/postgres/postgres_drop_procedure.sql
+++ test/fixtures/dialects/postgres/postgres_drop_procedure.sql
@@ -11,3 +11,5 @@ drop procedure delete_actor, update_actor;
 drop procedure delete_actor, update_actor CASCADE;
 
 drop procedure delete_actor(in id varchar);
+
+drop procedure insert_actor(varchar, varchar), insert_actor2(varchar, varchar);
