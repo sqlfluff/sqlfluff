a42ee4337028b6bd6d5359e1cb18d47a201a119c
Barry Pollard <barry@tunetheweb.com>
2022-01-25 14:51:20 +0000

    Cleanup date logic by removing DatePartClause and using DatetimeUnitSegment instead (#2464)
    * Fix various rule exceptions
    
    * Tidy up dateparts
    
    * Fix rule L044
    
    * Tidy up
    
    * Revert non-date changes
    
    * Revert change that wasn't supposed to be in this PR
    
    Co-authored-by: Barry Hart <barrywhart@yahoo.com>

--- src/sqlfluff/dialects/dialect_tsql.py
+++ src/sqlfluff/dialects/dialect_tsql.py
@@ -42,6 +42,48 @@ tsql_dialect.sets("unreserved_keywords").clear()
 tsql_dialect.sets("reserved_keywords").update(RESERVED_KEYWORDS)
 tsql_dialect.sets("unreserved_keywords").update(UNRESERVED_KEYWORDS)
 
+# Set the datetime units
+tsql_dialect.sets("datetime_units").clear()
+tsql_dialect.sets("datetime_units").update(
+    [
+        "D",
+        "DAY",
+        "DAYOFYEAR",
+        "DD",
+        "DW",
+        "DY",
+        "HH",
+        "HOUR",
+        "M",
+        "MCS",
+        "MI",
+        "MICROSECOND",
+        "MILLISECOND",
+        "MINUTE",
+        "MM",
+        "MONTH",
+        "MS",
+        "N",
+        "NANOSECOND",
+        "NS",
+        "Q",
+        "QQ",
+        "QUARTER",
+        "S",
+        "SECOND",
+        "SS",
+        "W",
+        "WEEK",
+        "WEEKDAY",
+        "WK",
+        "WW",
+        "YEAR",
+        "Y",
+        "YY",
+        "YYYY",
+    ]
+)
+
 tsql_dialect.insert_lexer_matchers(
     [
         RegexLexer(
@@ -1842,7 +1884,7 @@ class FunctionSegment(BaseSegment):
             Ref("DatePartFunctionNameSegment"),
             Bracketed(
                 Delimited(
-                    Ref("DatePartClause"),
+                    Ref("DatetimeUnitSegment"),
                     Ref(
                         "FunctionContentsGrammar",
                         # The brackets might be empty for some functions...
@@ -2329,51 +2371,6 @@ class CreateTableAsSelectStatementSegment(BaseSegment):
         OptionallyBracketed(Ref("SelectableGrammar")),
         Ref("OptionClauseSegment", optional=True),
         Ref("DelimiterSegment", optional=True),
-    )
-
-
-@tsql_dialect.segment(replace=True)
-class DatePartClause(BaseSegment):
-    """DatePart clause for use within DATEADD() or related functions."""
-
-    type = "date_part"
-
-    match_grammar = OneOf(
-        "D",
-        "DAY",
-        "DAYOFYEAR",
-        "DD",
-        "DW",
-        "DY",
-        "HH",
-        "HOUR",
-        "M",
-        "MCS",
-        "MI",
-        "MICROSECOND",
-        "MILLISECOND",
-        "MINUTE",
-        "MM",
-        "MONTH",
-        "MS",
-        "N",
-        "NANOSECOND",
-        "NS",
-        "Q",
-        "QQ",
-        "QUARTER",
-        "S",
-        "SECOND",
-        "SS",
-        "W",
-        "WEEK",
-        "WEEKDAY",
-        "WK",
-        "WW",
-        "YEAR",
-        "Y",
-        "YY",
-        "YYYY",
     )
 
 
