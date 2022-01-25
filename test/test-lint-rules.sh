d898a1182e37585ac9a385932c1a39f14410bfb0
Barry <barry@tunetheweb.com>
2022-01-25 13:23:43 +0000

    Refactor to avoid having to rerun

--- test/test-lint-rules.sh
+++ test/test-lint-rules.sh
@@ -3,14 +3,13 @@
 DIALECTS=($( ls -d test/fixtures/dialects/*/ ))
 for DIALECT in "${DIALECTS[@]}"
 do
-    echo "Testing $DIALECT SQL files lint without critical errors"
-    sqlfluff lint ${DIALECT} | grep -i -q criticaly
+    echo "Testing $DIALECT SQL files lint without critical errors..."
+    OUTPUT=$(sqlfluff lint ${DIALECT})
+    echo "${OUTPUT}" | grep -i -q critical
     # exit error, if match
     if  [ $? -eq 0 ]; then
-        echo
-        # Rerun to get critical error and filename
-        # Annoying that you have to rerun, but can't think of a better way
-        sqlfluff lint ${DIALECT} --disable_progress_bar | grep -i "critical\|test/fixtures" | grep -i critical -A 1
+        echo "Critical errors found:"
+        echo "${OUTPUT}" | grep -i "critical\|test/fixtures" | grep -i critical -A 1
         exit 1
     fi
 done
