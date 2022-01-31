42ecf5d1efd8660497c1b9764541c7f8a14aa7fd
Barry <barry@tunetheweb.com>
2022-01-31 22:35:29 +0000

    Test cases

---
+++ test/fixtures/dialects/tsql/set-statements.sql
@@ -0,0 +1,17 @@
+-- Single params
+SET @param1 = 1,
+
+-- Multiple params
+SET @param1 = 1,
+    @param2 = 2
+;
+
+-- Comma separated params with comment with comma
+SET @param1 = "test, test",
+    @param2 = 2
+;
+
+-- Params with expression
+SET @param1 = ("test", "test"),
+    @param2 = 2
+;
