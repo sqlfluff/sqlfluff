c395eb0a16a682f606745218f9519fde35151cd5
Barry Hart <barry.hart@mailchimp.com>
2022-02-04 08:56:51 -0500

    PR review

--- src/sqlfluff/api/simple.py
+++ src/sqlfluff/api/simple.py
@@ -131,7 +131,6 @@ def fix(
         # If fix_even_unparsable wasn't set, check for templating or parse
         # errors and suppress fixing if there were any.
         _, num_filtered_errors = result.count_tmp_prs_errors()
-        result.discard_fixes_for_lint_errors_in_files_with_tmp_or_prs_errors()
         if num_filtered_errors > 0:
             should_fix = False
     if should_fix:
