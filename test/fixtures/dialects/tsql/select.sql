04b8ca94723c330043955efc5cad558a801c7a1c
jpers36 <jpers36@gmail.com>
2022-02-05 11:50:55 -0500

    TSQL: Add Frame clause unreserved keywords (#2562)
    Co-authored-by: jpersons <jpersons@iuhealth.org>
    Co-authored-by: Barry Pollard <barry@tunetheweb.com>

--- test/fixtures/dialects/tsql/select.sql
+++ test/fixtures/dialects/tsql/select.sql
@@ -88,7 +88,12 @@ SELECT
 
 	ROW_NUMBER() OVER (PARTITION BY (SELECT mediaty FROM dbo.MediaTypes ms WHERE ms.MediaTypeID = f.mediatypeid) ORDER BY AdjustedPriorityScore DESC) AS Subselect_Partition,
 
-	ROW_NUMBER() OVER (PARTITION BY COALESCE(NPI1, NPI2) ORDER BY COALESCE(SystemEffectiveDTS1, SystemEffectiveDTS2) DESC) AS Coalesce_Partition
+	ROW_NUMBER() OVER (PARTITION BY COALESCE(NPI1, NPI2) ORDER BY COALESCE(SystemEffectiveDTS1, SystemEffectiveDTS2) DESC) AS Coalesce_Partition,
+
+
+	[preceding]	= count(*) over(order by object_id ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW ),
+	[central]	= count(*) over(order by object_id ROWS BETWEEN 2 PRECEDING AND 2 FOLLOWING ),
+	[following]	= count(*) over(order by object_id ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING)
 
 
 FROM dbo . all_pop	
