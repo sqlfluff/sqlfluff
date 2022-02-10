d021c9db1faf2da46c07c62f2faf9a0ee0fa8a63
Barry <barry@tunetheweb.com>
2022-02-10 11:14:51 +0000

    Support indenting WINDOWS clauses and (optionally) CTEs

--- src/sqlfluff/dialects/dialect_ansi.py
+++ src/sqlfluff/dialects/dialect_ansi.py
@@ -2012,9 +2012,11 @@ class NamedWindowSegment(BaseSegment):
     type = "named_window"
     match_grammar = Sequence(
         "WINDOW",
+        Indent,
         Delimited(
             Ref("NamedWindowExpressionSegment"),
         ),
+        Dedent,
     )
 
 
@@ -2208,10 +2210,12 @@ class WithCompoundStatementSegment(BaseSegment):
     parse_grammar = Sequence(
         "WITH",
         Ref.keyword("RECURSIVE", optional=True),
+        Conditional(Indent, indented_ctes=True),
         Delimited(
             Ref("CTEDefinitionSegment"),
             terminator=Ref.keyword("SELECT"),
         ),
+        Conditional(Dedent, indented_ctes=True),
         OneOf(
             Ref("NonWithSelectableGrammar"),
             Ref("NonWithNonSelectableGrammar"),
