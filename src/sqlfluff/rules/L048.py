d9f64445b60a4746619c3cb577cf157e632a4862
Barry Pollard <barry@tunetheweb.com>
2022-02-15 01:28:59 +0000

    L048 - handle more statements and exclude casting operators (#2642)
    * L048 - handle casting operators
    
    * Add test cases
    
    * Fix rule L048 to work properly
    
    * Review feedback

--- src/sqlfluff/rules/L048.py
+++ src/sqlfluff/rules/L048.py
@@ -35,6 +35,8 @@ class Rule_L048(Rule_L006):
         FROM foo
     """
 
+    _require_three_children: bool = False
+
     _target_elems: List[Tuple[str, str]] = [
         ("name", "quoted_literal"),
     ]
@@ -47,6 +49,15 @@ class Rule_L048(Rule_L006):
         https://github.com/sqlfluff/sqlfluff/issues/943
         """
         simple_res = Rule_L006._missing_whitespace(seg, before=before)
-        if not before and seg and seg.is_type("comma", "statement_terminator"):
+        if (
+            not before
+            and seg
+            and (
+                seg.is_type("comma", "statement_terminator")
+                or (
+                    seg.is_type("cast_expression") and seg.get_child("casting_operator")
+                )
+            )
+        ):
             return False
         return simple_res
