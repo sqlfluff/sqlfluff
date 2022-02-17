ed31c312408ec081be625d8579b4adb0ddd04afa
Barry Hart <barry.hart@mailchimp.com>
2022-02-17 17:49:49 -0500

    Remove obsolete code (workaround for the bug we're fixing in this branch)

--- src/sqlfluff/rules/L039.py
+++ src/sqlfluff/rules/L039.py
@@ -75,18 +75,14 @@ class Rule_L039(BaseRule):
                 # https://github.com/sqlfluff/sqlfluff/issues/1304
                 # It represents the question: are we parsing through leading whitespace
                 # in this loop?
-                leading_whitespace = True
                 for child_seg in seg.get_raw_segments():
                     if child_seg.is_whitespace:
-                        if not leading_whitespace:
-                            violations.append(
-                                LintResult(
-                                    anchor=child_seg,
-                                    fixes=[LintFix.delete(child_seg)],
-                                )
+                        violations.append(
+                            LintResult(
+                                anchor=child_seg,
+                                fixes=[LintFix.delete(child_seg)],
                             )
-                    else:
-                        leading_whitespace = False
+                        )
 
             if seg.is_type("comparison_operator"):
                 delete_fixes = [
