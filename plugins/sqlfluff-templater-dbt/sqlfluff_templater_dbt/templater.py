11aa652da6b6763514449fa6ede1e82a3b7d8157
Barry Hart <barry.hart@mailchimp.com>
2022-02-28 21:21:00 -0500

    Make it more efficient

--- plugins/sqlfluff-templater-dbt/sqlfluff_templater_dbt/templater.py
+++ plugins/sqlfluff-templater-dbt/sqlfluff_templater_dbt/templater.py
@@ -442,7 +442,12 @@ class DbtTemplater(JinjaTemplater):
 
         node = self._find_node(fname, config)
 
-        save_nodes = dict(self.dbt_manifest.nodes)
+        save_ephemeral_nodes = dict(
+            (k, v)
+            for k, v in self.dbt_manifest.nodes.items()
+            if v.config.materialized == "ephemeral"
+            and not getattr(v, "compiled", False)
+        )
         with self.connection():
             node = self.dbt_compiler.compile_node(
                 node=node,
@@ -502,17 +507,15 @@ class DbtTemplater(JinjaTemplater):
                 config=config,
                 make_template=make_template,
             )
-        # :HACK: Restore compiled ephemeral nodes back to parsed. This prevents
-        # a runtime error inside dbt with 2nd-level ephemeral model dependencies
-        # after they have been compiled/linted earlier in the same run. Perhaps
-        # there is a cleaner way to do this, but this is reasonably efficient,
-        # and it works.
-        for k, v in save_nodes.items():
-            if v.config.materialized == "ephemeral":
-                if not getattr(v, "compiled", False) and getattr(
-                    self.dbt_manifest.nodes[k], "compiled", False
-                ):
-                    self.dbt_manifest.nodes[k] = v
+        # :HACK: If calling compile_node() compiled any ephemeral nodes,
+        # restore them to their earlier state. This prevents a runtime error
+        # in the dbt "_inject_ctes_into_sql()" function that occurs with
+        # 2nd-level ephemeral model dependencies (e.g. A -> B -> C, where
+        # both B and C are ephemeral). Perhaps there is a better way to do
+        # this, but this seems good enough for now.
+        for k, v in save_ephemeral_nodes.items():
+            if getattr(self.dbt_manifest.nodes[k], "compiled", False):
+                self.dbt_manifest.nodes[k] = v
 
         if make_template and n_trailing_newlines:
             # Update templated_sql as we updated the other strings above. Update
