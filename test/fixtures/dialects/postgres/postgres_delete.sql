83ba1a7ac7a2cce46275e0b992e4a08f25e20f7e
jpy-git <josephyoung.jpy@gmail.com>
2022-03-05 17:41:43 +0000

    Postgres: Complete DELETE FROM grammar

--- test/fixtures/dialects/postgres/postgres_delete.sql
+++ test/fixtures/dialects/postgres/postgres_delete.sql
@@ -1,12 +1,31 @@
-DELETE FROM films WHERE kind <> 'Musical';
-
 DELETE FROM films;
 
-DELETE FROM tasks WHERE status = 'DONE' RETURNING *;
+DELETE FROM ONLY films;
+
+DELETE FROM films *;
+
+DELETE FROM films AS f;
+
+DELETE FROM films USING producers
+  WHERE producer_id = producers.id AND producers.name = 'foo';
+
+DELETE FROM films AS f USING producers AS p
+  WHERE f.producer_id = p.id AND p.name = 'foo';
+
+DELETE FROM films AS f USING producers AS p, actors AS a
+  WHERE f.producer_id = p.id AND p.name = 'foo'
+    AND f.actor_id = a.id AND a.name = 'joe cool';
 
 DELETE FROM tasks WHERE CURRENT OF c_tasks;
 
-DELETE FROM some_table
-USING other_table
-WHERE other_table.col = some_table.col
+DELETE FROM films WHERE kind <> 'Musical';
 
+DELETE FROM tasks WHERE status = 'DONE' RETURNING *;
+
+DELETE FROM tasks WHERE status = 'DONE' RETURNING actor_id;
+
+DELETE FROM tasks WHERE status = 'DONE' RETURNING actor_id as a_id;
+
+DELETE FROM tasks WHERE status = 'DONE' RETURNING actor_id, producer_id;
+
+DELETE FROM tasks WHERE status = 'DONE' RETURNING actor_id as a_id, producer_id as p_id;
