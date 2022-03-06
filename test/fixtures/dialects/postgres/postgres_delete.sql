08d0d98c21d77a115da47ed9bfc7b202f2753e0e
Joseph Young <80432516+jpy-git@users.noreply.github.com>
2022-03-06 12:08:26 +0000

    Postgres: Make AS optional in Postgres DELETE (#2794)
    * Postgres: Make AS optional in Postgres DELETE
    
    * Re-add test
    
    Co-authored-by: Barry Pollard <barry@tunetheweb.com>

--- test/fixtures/dialects/postgres/postgres_delete.sql
+++ test/fixtures/dialects/postgres/postgres_delete.sql
@@ -6,6 +6,8 @@ DELETE FROM films *;
 
 DELETE FROM films AS f;
 
+DELETE FROM films f;
+
 DELETE FROM films USING producers
   WHERE producer_id = producers.id AND producers.name = 'foo';
 
@@ -13,6 +15,13 @@ DELETE FROM films AS f USING producers AS p
   WHERE f.producer_id = p.id AND p.name = 'foo';
 
 DELETE FROM films AS f USING producers AS p, actors AS a
+  WHERE f.producer_id = p.id AND p.name = 'foo'
+    AND f.actor_id = a.id AND a.name = 'joe cool';
+
+DELETE FROM films f USING producers p
+  WHERE f.producer_id = p.id AND p.name = 'foo';
+
+DELETE FROM films f USING producers p, actors a
   WHERE f.producer_id = p.id AND p.name = 'foo'
     AND f.actor_id = a.id AND a.name = 'joe cool';
 
@@ -26,9 +35,13 @@ DELETE FROM tasks WHERE status = 'DONE' RETURNING actor_id;
 
 DELETE FROM tasks WHERE status = 'DONE' RETURNING actor_id as a_id;
 
+DELETE FROM tasks WHERE status = 'DONE' RETURNING actor_id a_id;
+
 DELETE FROM tasks WHERE status = 'DONE' RETURNING actor_id, producer_id;
 
 DELETE FROM tasks WHERE status = 'DONE' RETURNING actor_id as a_id, producer_id as p_id;
+
+DELETE FROM tasks WHERE status = 'DONE' RETURNING actor_id a_id, producer_id p_id;
 
 WITH test as (select foo from bar)
 DELETE FROM films;
