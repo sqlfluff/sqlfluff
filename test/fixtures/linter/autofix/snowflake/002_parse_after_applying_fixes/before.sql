e456f60136b2a34bf9fcb9dbd90a735773438035
Barry Hart <barrywhart@yahoo.com>
2022-03-08 18:00:08 -0500

    Fixes for the new post-fix parse check (#2813)
    * Post-fix parse check: Prefer parse_grammar over match_grammar
    
    * Bug fix for _choose_anchor_segment()
    
    * Add comments
    
    * Apply suggestions from code review
    
    Co-authored-by: Barry Pollard <barry_pollard@hotmail.com>
    
    * Simplify how we check (parse) the fixes
    
    * Add test case that runs multiple rules
    
    * Type hints, tidying
    
    Co-authored-by: Barry Hart <barry.hart@mailchimp.com>
    Co-authored-by: Barry Pollard <barry_pollard@hotmail.com>

---
+++ test/fixtures/linter/autofix/snowflake/002_parse_after_applying_fixes/before.sql
@@ -0,0 +1,16 @@
+merge into foo.bar as tgt
+using (
+select
+  foo::date as bar
+from foo.bar
+where
+split(foo, '|')[2] REGEXP '^\\d+\\-\\d+\\-\\d+ \\d+\\:\\d+$'
+OR
+foo IN ('BAR','FOO')
+) as src
+on
+  src.foo = tgt.foo
+when matched then
+update set
+  tgt.foo = src.foo
+;
