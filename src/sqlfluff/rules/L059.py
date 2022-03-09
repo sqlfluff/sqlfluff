add224e891d1e6b4e36ec015bb4709a664136823
Barry <barry@tunetheweb.com>
2022-03-09 10:14:40 +0000

    Fix bug with L059 for IF statements

--- src/sqlfluff/rules/L059.py
+++ src/sqlfluff/rules/L059.py
@@ -108,9 +108,10 @@ class Rule_L059(BaseRule):
         # Ignore the segments that are not of the same type as the defined policy above.
         # Also TSQL has a keyword called QUOTED_IDENTIFIER which maps to the name so
         # need to explicity check for that.
-        if (
-            context.segment.name not in context_policy
-            or context.segment.raw.lower() in ("quoted_identifier", "naked_identifier")
+        # print("BARRY:%s:%s:" % (context.segment, context.segment.name))
+        if context.segment.name != context_policy or context.segment.raw.lower() in (
+            "quoted_identifier",
+            "naked_identifier",
         ):
             return None
 
