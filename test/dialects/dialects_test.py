0fa217dcab2d248feb0fae88d7723877b5b6cbc0
Michael O Toole <otoole.timmy@gmail.com>
2022-03-11 11:19:41 +0100

    Remove test refs & add cache

--- test/dialects/dialects_test.py
+++ test/dialects/dialects_test.py
@@ -3,8 +3,8 @@
 Any files in the test/fixtures/dialects/ directory will be picked up
 and automatically tested against the appropriate dialect.
 """
-
-from functools import cache
+import json
+from functools import lru_cache
 from typing import Any, Dict, Optional
 import pytest
 
@@ -24,18 +24,15 @@ parse_success_examples, parse_structure_examples = get_parse_fixtures(
     fail_on_missing_yml=True
 )
 
-@cache
-def lex_and_parse(config_overrides: Dict[str, Any], raw:str) -> Optional[BaseSegment]:
-    """Performs a Lex and Parse, and caches result for further steps."""
-    print("LINTING!")
+def _lex_and_parse(config_overrides: str, raw:str) -> Optional[BaseSegment]:
+    """Performs a Lex and Parse, best cached within fixture."""
     # Load the right dialect
-    config = FluffConfig(overrides=config_overrides)
+    config = FluffConfig(overrides=json.loads(config_overrides))
     tokens, lex_vs = Lexer(config=config).lex(raw)
     # From just the initial parse, check we're all there
     assert "".join(token.raw for token in tokens) == raw
     # Check we don't have lexing issues
     assert not lex_vs
-
     # Do the parse WITHOUT lots of logging
     # The logs get too long here to be useful. We should use
     # specific segment tests if we want to debug logs.
@@ -45,13 +42,31 @@ def lex_and_parse(config_overrides: Dict[str, Any], raw:str) -> Optional[BaseSeg
     return Parser(config=config).parse(tokens)
 
 
+@pytest.fixture(scope="session")
+def cached_parser():
+    """Creates a cached version of the Lex and Parse.
+    
+    This halfs execution time while allowing nice test naming.
+    """
+    # Manually apply decorator to allow the use of _lex_and_parse
+    # ouside this fixture.
+    cached_parse_fn = lru_cache(maxsize=None)(_lex_and_parse)
+
+    def lex_and_parse(config_overrides: Dict[str, Any], raw:str) -> Optional[BaseSegment]:
+        """Sets up cache parsable inputs from normal inpurts."""
+        cachable_str = json.dumps(config_overrides, sort_keys=True)
+        return cached_parse_fn(cachable_str, raw)
+
+    return lex_and_parse
+
+
 @pytest.mark.parametrize("dialect,file", parse_success_examples)
-def test__dialect__base_file_parse(dialect, file):
+def test__dialect__base_file_parse(dialect, file, cached_parser):
     """For given test examples, check successful parsing."""
+    raw = load_file(dialect, file)
     config_overides = dict(dialect=dialect)
-    raw = load_file(config_overides, file)
     # Use the helper function to avoid parsing twice
-    parsed = lex_and_parse(dialect, raw)
+    parsed = cached_parser(config_overides, raw)
     if not parsed:
         return
 
@@ -65,19 +80,32 @@ def test__dialect__base_file_parse(dialect, file):
 
 
 @pytest.mark.parametrize("dialect,file", parse_success_examples)
-def test__dialect__base_broad_fix(dialect, file, raise_critical_errors_after_fix):
+def test__dialect__base_broad_fix(dialect, file, raise_critical_errors_after_fix, cached_parser):
     """For given test examples, check successful parsing."""
     raw = load_file(dialect, file)
     config_overides = dict(dialect=dialect)
     # Lean on the cached result of the above test if possible
-    parsed = lex_and_parse(config_overides, raw)
+    parsed = cached_parser(config_overides, raw)
     if not parsed:
         return
 
+    config = FluffConfig(overrides=dict(config_overides))
+    # Linter is setup manually to take advantage of parser caching
+    linter = Linter(config=config)
+    templated_file, _ = linter.templater.process(
+        in_str=raw, fname="<string>", config=config, formatter=linter.formatter,
+    )
+
     # Due to "raise_critical_errors_after_fix" fixure "fix",
     # will now throw.
-    config = FluffConfig(overrides=config_overides)
-    Linter(config=config).fix(parsed)
+    linter.lint_fix_parsed(
+        parsed,
+        fix=True,
+        config=config,
+        rule_set=linter.get_ruleset(),
+        templated_file=templated_file,
+    )
+    raise ValueError()
 
 
 @pytest.mark.parametrize("dialect,sqlfile,code_only,yamlfile", parse_structure_examples)
