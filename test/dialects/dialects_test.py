018feb177ca71ecb16bdf61ee4d323b341ee5972
Michael O Toole <otoole.timmy@gmail.com>
2022-03-11 14:57:19 +0100

    add more cores

--- test/dialects/dialects_test.py
+++ test/dialects/dialects_test.py
@@ -4,7 +4,6 @@ Any files in the test/fixtures/dialects/ directory will be picked up
 and automatically tested against the appropriate dialect.
 """
 import json
-from functools import lru_cache
 from typing import Any, Dict, Optional
 import pytest
 
@@ -26,7 +25,7 @@ parse_success_examples, parse_structure_examples = get_parse_fixtures(
 
 
 def _lex_and_parse(config_overrides: str, raw: str) -> Optional[BaseSegment]:
-    """Performs a Lex and Parse, best cached within fixture."""
+    """Performs a Lex and Parse, with cachable inputs within fixture."""
     # Load the right dialect
     config = FluffConfig(overrides=json.loads(config_overrides))
     tokens, lex_vs = Lexer(config=config).lex(raw)
@@ -43,33 +42,19 @@ def _lex_and_parse(config_overrides: str, raw: str) -> Optional[BaseSegment]:
     return Parser(config=config).parse(tokens)
 
 
-@pytest.fixture(scope="session")
-def cached_parser():
-    """Creates a cached version of the Lex and Parse.
-
-    This drastically reduces execution time while allowing nicbe test naming.
-    """
-    # Manually apply decorator to allow the use of _lex_and_parse
-    # ouside this fixture.
-    cached_parse_fn = lru_cache(maxsize=None)(_lex_and_parse)
-
-    def lex_and_parse(
-        config_overrides: Dict[str, Any], raw: str
-    ) -> Optional[BaseSegment]:
-        """Sets up cache parsable inputs from normal inpurts."""
-        cachable_str = json.dumps(config_overrides, sort_keys=True)
-        return cached_parse_fn(cachable_str, raw)
-
-    return lex_and_parse
+def lex_and_parse(config_overrides: Dict[str, Any], raw: str) -> Optional[BaseSegment]:
+    """Sets up cache parsable inputs from normal inpurts."""
+    cachable_str = json.dumps(config_overrides, sort_keys=True)
+    return _lex_and_parse(cachable_str, raw)
 
 
 @pytest.mark.parametrize("dialect,file", parse_success_examples)
-def test__dialect__base_file_parse(dialect, file, cached_parser):
+def test__dialect__base_file_parse(dialect, file):
     """For given test examples, check successful parsing."""
     raw = load_file(dialect, file)
     config_overides = dict(dialect=dialect)
     # Use the helper function to avoid parsing twice
-    parsed: Optional[BaseSegment] = cached_parser(config_overides, raw)
+    parsed: Optional[BaseSegment] = lex_and_parse(config_overides, raw)
     if not parsed:
         return
 
@@ -84,14 +69,12 @@ def test__dialect__base_file_parse(dialect, file, cached_parser):
 
 @pytest.mark.integration_test
 @pytest.mark.parametrize("dialect,file", parse_success_examples)
-def test__dialect__base_broad_fix(
-    dialect, file, raise_critical_errors_after_fix, cached_parser
-):
+def test__dialect__base_broad_fix(dialect, file, raise_critical_errors_after_fix):
     """Run a full fix with all rules, in search of critical errors."""
     raw = load_file(dialect, file)
     config_overides = dict(dialect=dialect)
     # Lean on the cached result of the above test if possible
-    parsed: Optional[BaseSegment] = cached_parser(config_overides, raw)
+    parsed: Optional[BaseSegment] = lex_and_parse(config_overides, raw)
     if not parsed:
         return
 
@@ -118,7 +101,11 @@ def test__dialect__base_broad_fix(
 
 @pytest.mark.parametrize("dialect,sqlfile,code_only,yamlfile", parse_structure_examples)
 def test__dialect__base_parse_struct(
-    dialect, sqlfile, code_only, yamlfile, yaml_loader
+    dialect,
+    sqlfile,
+    code_only,
+    yamlfile,
+    yaml_loader,
 ):
     """For given test examples, check parsed structure against yaml."""
     parsed: Optional[BaseSegment] = parse_example_file(dialect, sqlfile)
