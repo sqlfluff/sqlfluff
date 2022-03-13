ac0ec19ba229fc8a5224c49f5d7a14c0ee0e3219
Barry Hart <barry.hart@mailchimp.com>
2022-03-13 11:17:24 -0400

    Tweak comment and whitespace

--- src/sqlfluff/core/templaters/slicers/tracer.py
+++ src/sqlfluff/core/templaters/slicers/tracer.py
@@ -96,7 +96,6 @@ class JinjaTracer:
             target_slice_idx = self.find_slice_index(alt_id)
             slice_length = content_info if literal else len(str(content_info))
             self.move_to_slice(target_slice_idx, slice_length)
-
         return JinjaTrace(
             self.make_template(self.raw_str).render(), self.raw_sliced, self.sliced_file
         )
