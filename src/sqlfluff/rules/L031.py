04f2009a9d116832314a4a9acb869e67092542e3
Barry Hart <barry.hart@mailchimp.com>
2022-03-18 14:48:07 -0400

    Add doc decorator

--- src/sqlfluff/rules/L031.py
+++ src/sqlfluff/rules/L031.py
@@ -5,7 +5,11 @@ from typing import Generator, NamedTuple, Optional
 
 from sqlfluff.core.parser import BaseSegment
 from sqlfluff.core.rules.base import BaseRule, LintFix, LintResult, RuleContext
-from sqlfluff.core.rules.doc_decorators import document_fix_compatible
+from sqlfluff.core.rules.doc_decorators import (
+    document_configuration,
+    document_fix_compatible,
+)
+
 import sqlfluff.core.rules.functional.segment_predicates as sp
 
 
@@ -19,6 +23,7 @@ class TableAliasInfo(NamedTuple):
 
 
 @document_fix_compatible
+@document_configuration
 class Rule_L031(BaseRule):
     """Avoid table aliases in from clauses and join conditions.
 
