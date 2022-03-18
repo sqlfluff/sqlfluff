cb3ba161ff854064dfe3e9578b1e67c9c76b87fb
Barry Hart <barry.hart@mailchimp.com>
2022-03-18 17:03:49 -0400

    Add automated test to verify dbt macros are skipped

--- plugins/sqlfluff-templater-dbt/test/linter_test.py
+++ plugins/sqlfluff-templater-dbt/test/linter_test.py
@@ -24,6 +24,21 @@ def test__linter__skip_dbt_model_disabled(project_dir):  # noqa
     assert not linted_file.tree
 
 
+def test__linter__skip_dbt_macro(project_dir):  # noqa
+    """Test that the linter skips macros."""
+    conf = FluffConfig(configs=DBT_FLUFF_CONFIG)
+    lntr = Linter(config=conf)
+    model_file_path = os.path.join(project_dir, "macros/echo.sql")
+    linted_path = lntr.lint_path(path=model_file_path)
+    # Check that the file is still there
+    assert len(linted_path.files) == 1
+    linted_file = linted_path.files[0]
+    # Normalise paths to control for OS variance
+    assert os.path.normpath(linted_file.path) == os.path.normpath(model_file_path)
+    assert not linted_file.templated_file
+    assert not linted_file.tree
+
+
 def test__linter__lint_ephemeral_3_level(project_dir):  # noqa
     """Test linter can lint a project with 3-level ephemeral dependencies."""
     # This was previously crashing inside dbt, in a function named
