ae2eef7764ce830149afa874a1b273547a15f22e
Barry Hart <barry.hart@mailchimp.com>
2022-03-18 17:16:03 -0400

    Parametrize existing tests for disabled files to also test macro files

--- plugins/sqlfluff-templater-dbt/test/linter_test.py
+++ plugins/sqlfluff-templater-dbt/test/linter_test.py
@@ -3,32 +3,20 @@
 import os
 import os.path
 
+import pytest
+
 from sqlfluff.core import Linter, FluffConfig
 from test.fixtures.dbt.templater import DBT_FLUFF_CONFIG, project_dir  # noqa: F401
 
 
-def test__linter__skip_dbt_model_disabled(project_dir):  # noqa
-    """Test that the linter skips disabled dbt models."""
+@pytest.mark.parametrize(
+    "path", ["models/my_new_project/disabled_model.sql", "macros/echo.sql"]
+)
+def test__linter__skip_file(path, project_dir):  # noqa
+    """Test that the linter skips disabled dbt models and macros."""
     conf = FluffConfig(configs=DBT_FLUFF_CONFIG)
     lntr = Linter(config=conf)
-    model_file_path = os.path.join(
-        project_dir, "models/my_new_project/disabled_model.sql"
-    )
-    linted_path = lntr.lint_path(path=model_file_path)
-    # Check that the file is still there
-    assert len(linted_path.files) == 1
-    linted_file = linted_path.files[0]
-    # Normalise paths to control for OS variance
-    assert os.path.normpath(linted_file.path) == os.path.normpath(model_file_path)
-    assert not linted_file.templated_file
-    assert not linted_file.tree
-
-
-def test__linter__skip_dbt_macro(project_dir):  # noqa
-    """Test that the linter skips macros."""
-    conf = FluffConfig(configs=DBT_FLUFF_CONFIG)
-    lntr = Linter(config=conf)
-    model_file_path = os.path.join(project_dir, "macros/echo.sql")
+    model_file_path = os.path.join(project_dir, path)
     linted_path = lntr.lint_path(path=model_file_path)
     # Check that the file is still there
     assert len(linted_path.files) == 1
