b14f3b8ab663e77c16d88a08865976aca2e974f2
Barry <barry@tunetheweb.com>
2022-03-18 09:08:54 +0000

    BigQuery: Better `STRUCT` Array Support

--- test/fixtures/dialects/bigquery/select_function_object_fields.sql
+++ test/fixtures/dialects/bigquery/select_function_object_fields.sql
@@ -4,5 +4,7 @@ SELECT
   testFunction(a).b.c AS field_with_field,
   testFunction(a).b.* AS field_with_wildcard,
   testFunction(a)[OFFSET(0)].* AS field_with_offset_wildcard,
-  testFunction(a)[ORDINAL(1)].* AS field_with_ordinal_wildcard
+  testFunction(a)[SAFE_OFFSET(0)].* AS field_with_safe_offset_wildcard,
+  testFunction(a)[ORDINAL(1)].* AS field_with_ordinal_wildcard,
+  testFunction(a)[ORDINAL(1)].a AS field_with_ordinal_field
 FROM table1
