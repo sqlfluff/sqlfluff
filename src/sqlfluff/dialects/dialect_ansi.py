c2d9201a641c50b03a21f0a46d6b1d5addba32f3
Barry Hart <barry.hart@mailchimp.com>
2022-03-22 15:13:04 -0400

    L042 fix fails with AttributeError on non-ANSI dialects

--- src/sqlfluff/dialects/dialect_ansi.py
+++ src/sqlfluff/dialects/dialect_ansi.py
@@ -2225,15 +2225,6 @@ class CTEDefinitionSegment(BaseSegment):
         ),
     )
 
-    def get_identifier(self) -> BaseSegment:
-        """Gets the identifier of this CTE.
-
-        Note: it blindly get the first identifier it finds
-        which given the structure of a CTE definition is
-        usually the right one.
-        """
-        return self.get_child("identifier")
-
 
 @ansi_dialect.segment()
 class WithCompoundStatementSegment(BaseSegment):
