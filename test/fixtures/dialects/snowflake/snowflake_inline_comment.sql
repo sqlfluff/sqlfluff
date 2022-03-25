39d5b31b5bd180c1b7699d83b2eb02b5652aa727
WittierDinosaur <daniel.jones@deliveroo.co.uk>
2022-03-25 11:30:33 +0000

    More in-liney commments

--- test/fixtures/dialects/snowflake/snowflake_inline_comment.sql
+++ test/fixtures/dialects/snowflake/snowflake_inline_comment.sql
@@ -1,6 +1,7 @@
-//Snowflake Inline Comment
--- Classic Inline Comment
-SELECT 1;
 # Classic Inline Comment
-SELECT 1//Snowflake Inline
-;
+SELECT 1; -- Classic Inline Comment
+SELECT 1; # Classic Inline Comment
+SELECT 1; //Snowflake Inline Comment
+SELECT 1;-- Classic Inline Comment No Space
+SELECT 1;# Classic Inline Comment No Space
+SELECT 1//Snowflake Inline Comment No Space
