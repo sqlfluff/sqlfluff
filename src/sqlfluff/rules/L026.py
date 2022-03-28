4d4a50c77d797db8e14d664d20504441e137e3e9
R7L208 <22798188+R7L208@users.noreply.github.com>
2022-03-28 18:03:49 -0400

    fix spark3 to sparksql in l026 comment

--- src/sqlfluff/rules/L026.py
+++ src/sqlfluff/rules/L026.py
@@ -31,7 +31,7 @@ class Rule_L026(BaseRule):
     """References cannot reference objects not present in ``FROM`` clause.
 
     .. note::
-       This rule is disabled by default for BigQuery, Hive, Redshift, and Spark3
+       This rule is disabled by default for BigQuery, Hive, Redshift, and SparkSQL
        due to the use of structs and lateral views which trigger false positives.
        It can be enabled with the ``force_enable = True`` flag.
 
