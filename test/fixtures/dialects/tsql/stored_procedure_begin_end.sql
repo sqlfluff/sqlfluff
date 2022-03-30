dae30f24abe0952a00935addb77414b83edb623c
Barry <barry@tunetheweb.com>
2022-03-30 08:52:43 +0100

    Add another test case

--- test/fixtures/dialects/tsql/stored_procedure_begin_end.sql
+++ test/fixtures/dialects/tsql/stored_procedure_begin_end.sql
@@ -32,3 +32,23 @@ BEGIN
 SELECT '8'
 END;
 GO
+
+CREATE PROCEDURE [dbo].[usp_process_tran_log]
+	  @out_vchCode uddt_output_code OUTPUT
+	, @out_vchMsg uddt_output_msg OUTPUT
+	, @in_debug INT = 1
+AS
+SET NOCOUNT ON;
+BEGIN
+	BEGIN TRY
+		SELECT '8';
+	END TRY
+	BEGIN CATCH
+		SET @v_nSysErrorNum = ERROR_NUMBER();
+		SET @v_vchCode = ERROR_LINE();
+		SET @v_vchMsg = N'Missing control type.';
+		SET @v_vchMsg = @v_vchMsg + N' SQL Error = ' + ERROR_MESSAGE();
+		GOTO ERROR_HANDLER;
+	END CATCH
+END;
+GO
