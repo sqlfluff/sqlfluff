f17b3c00c6bb3f97d6832be21c1bd956911bf414
Barry <barry@tunetheweb.com>
2022-03-30 08:48:32 +0100

    TSQL: Support OUTPUT Params and GOTO Statements

---
+++ test/fixtures/dialects/tsql/goto_statement.sql
@@ -0,0 +1,1 @@
+GOTO Branch_Three;
--- test/fixtures/dialects/tsql/stored_procedure_begin_end.sql
+++ test/fixtures/dialects/tsql/stored_procedure_begin_end.sql
@@ -6,4 +6,29 @@ BEGIN
 	select a from tbl1;
 	
 	select b from tbl2;
-END
+END;
+GO
+
+CREATE PROCEDURE [dbo].[usp_process_tran_log]
+	  @out_vchCode uddt_output_code OUTPUT
+	, @out_vchMsg uddt_output_msg OUTPUT
+	, @in_debug INT = 1
+AS
+--*******************************************************************************************
+SET NOCOUNT ON;
+BEGIN 
+SELECT '8'
+END;
+GO
+
+CREATE PROCEDURE [dbo].[usp_process_tran_log]
+	  @out_vchCode uddt_output_code OUTPUT
+	, @out_vchMsg uddt_output_msg OUT
+	, @in_debug INT = 1 READONLY
+AS
+--*******************************************************************************************
+SET NOCOUNT ON;
+BEGIN 
+SELECT '8'
+END;
+GO
