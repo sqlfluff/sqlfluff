b7510f798b58a2ffae9d65be9492a85196429157
Barry <barry@tunetheweb.com>
2022-03-31 16:17:22 +0100

    Black (again) - wish I'd never started this!

--- plugins/sqlfluff-templater-dbt/test/templater_test.py
+++ plugins/sqlfluff-templater-dbt/test/templater_test.py
@@ -442,13 +442,13 @@ def test__project_dir_does_not_exist_error(dbt_templater, caplog):  # noqa: F811
     ],
 )
 def test__context_in_config_is_loaded(
-        project_dir, dbt_templater, model_path, var_value
-):  # noqa: F811
+    project_dir, dbt_templater, model_path, var_value  # noqa: F811
+):
     """Test that variables inside .sqlfluff are passed to dbt."""
     context = {"passed_through_cli": var_value} if var_value else {}
 
     config_dict = DBT_FLUFF_CONFIG
-    config_dict["templater"]['dbt']['context'] = context
+    config_dict["templater"]["dbt"]["context"] = context
     config = FluffConfig(config_dict)
 
     fname = os.path.abspath(os.path.join(project_dir, model_path))
