7b398a8030ab63eeb5635dae270901dcfb89dd70
Alan <alanmcruickshank@gmail.com>
2022-04-01 10:03:13 +0100

    bring the ansi dialect into line

--- src/sqlfluff/dialects/dialect_ansi.py
+++ src/sqlfluff/dialects/dialect_ansi.py
@@ -312,7 +312,7 @@ ansi_dialect.add(
             CodeSegment,
             name="data_type_identifier",
             type="data_type_identifier",
-            anti_template=r"^(NOT)$",
+            anti_template=r"^(" + r"|".join(dialect.sets("reserved_keywords")) + r")$",
             # TODO - this is a stopgap until we implement explicit data types
         ),
     ),
