817aa0294ca2313f95050019043fedb04ddd364d
Lorin Dawson <22798188+R7L208@users.noreply.github.com>
2022-04-02 11:47:38 -0400

    SparkSQL: Support for querying snapshots when reading data with Delta Lake (#2972)
    * support for reading snapshots in delta
    
    * fixed a missed Spark3 reference the in README
    
    * change atsign_literal to at_sign_literal
    
    * remove AsOfGrammar
    
    Co-authored-by: Barry Pollard <barry_pollard@hotmail.com>

--- src/sqlfluff/dialects/dialect_exasol.py
+++ src/sqlfluff/dialects/dialect_exasol.py
@@ -97,7 +97,7 @@ exasol_dialect.insert_lexer_matchers(
                 NewlineSegment,
             ),
         ),
-        RegexLexer("atsign_literal", r"@[a-zA-Z_][\w]*", CodeSegment),
+        RegexLexer("at_sign_literal", r"@[a-zA-Z_][\w]*", CodeSegment),
         RegexLexer("dollar_literal", r"[$][a-zA-Z0-9_.]*", CodeSegment),
     ],
     before="like_operator",
