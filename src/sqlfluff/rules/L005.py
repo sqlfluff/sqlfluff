d93f4f2b3044fa22c095e351b509101005c6d198
Barry Hart <barry.hart@mailchimp.com>
2022-04-09 14:05:24 -0400

    PR review

--- src/sqlfluff/rules/L005.py
+++ src/sqlfluff/rules/L005.py
@@ -39,19 +39,15 @@ class Rule_L005(BaseRule):
     """
 
     def _eval(self, context: RuleContext) -> Optional[LintResult]:
-        """Commas should not have whitespace directly before them.
-
-        We need at least one segment behind us for this to work.
-
-        """
-        if context.raw_segment_pre is not None:
-            cm1: RawSegment = context.raw_segment_pre
-            if (
-                context.segment.is_type("comma")
-                and cm1.is_type("whitespace")
-                and cm1.pos_marker.line_pos > 1
-            ):
-                anchor = cm1
-                return LintResult(anchor=anchor, fixes=[LintFix.delete(cm1)])
-        # Otherwise fine
+        """Commas should not have whitespace directly before them."""
+        anchor: Optional[RawSegment] = context.raw_segment_pre
+        if (
+            # We need at least one segment previous segment for this to work.
+            anchor is not None
+            and context.segment.is_type("comma")
+            and anchor.is_type("whitespace")
+            and anchor.pos_marker.line_pos > 1
+        ):
+            return LintResult(anchor=anchor, fixes=[LintFix.delete(anchor)])
+        # Otherwise fine.
         return None
