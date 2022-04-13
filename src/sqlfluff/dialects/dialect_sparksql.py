778271bcd548dd4f62cf03db4bf9f4086bee8e95
Barry Pollard <barry_pollard@hotmail.com>
2022-04-13 13:17:29 +0100

    Postgres: Support `DROP EXTENSION` (#3083)

--- src/sqlfluff/dialects/dialect_sparksql.py
+++ src/sqlfluff/dialects/dialect_sparksql.py
@@ -2239,7 +2239,6 @@ class StatementSegment(ansi.StatementSegment):
             Ref("TransactionStatementSegment"),
             Ref("CreateSchemaStatementSegment"),
             Ref("SetSchemaStatementSegment"),
-            Ref("CreateExtensionStatementSegment"),
             Ref("CreateModelStatementSegment"),
             Ref("DropModelStatementSegment"),
         ],
