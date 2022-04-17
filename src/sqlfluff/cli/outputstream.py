b80e858400bf76ab477301f983d59f2495d94840
Barry Hart <barry.hart@mailchimp.com>
2022-04-17 16:51:05 -0400

    Undo previous change, introduced a bug

--- src/sqlfluff/cli/outputstream.py
+++ src/sqlfluff/cli/outputstream.py
@@ -67,5 +67,9 @@ def make_output_stream(
         if not output_path:
             # Human-format output to stdout.
             return TqdmOutput(config)
-    # Human-format output to a file (discard if output_path is None)..
-    return FileOutput(config, output_path or os.devnull)
+        else:
+            # Human-format output to a file.
+            return FileOutput(config, output_path)
+    else:
+        # Discard output
+        return FileOutput(config, os.devnull)
