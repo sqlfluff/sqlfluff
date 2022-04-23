47c2236c68585302e75b81be4c329007834a41fe
Barry <barry@tunetheweb.com>
2022-04-23 17:33:38 +0100

    Coverage

--- src/sqlfluff/core/parser/grammar/greedy.py
+++ src/sqlfluff/core/parser/grammar/greedy.py
@@ -62,7 +62,9 @@ class GreedyUntil(BaseGrammar):
         seg_buff = segments
         seg_bank = ()  # Empty tuple
         # If no terminators then just return the whole thing.
-        if matchers == [None]:
+        # Shouldn't really happen as GreedyMatch is everything
+        # and StartsWith has mandatory terminator
+        if matchers == [None]:  # pragma: no cover
             return MatchResult.from_matched(segments)
 
         while True:
