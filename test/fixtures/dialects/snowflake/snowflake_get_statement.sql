9171ade2adb68e85579469ca9822ebd1b834a2b1
James McNeilis <james.mcneilis@deliveroo.co.uk>
2022-05-10 16:21:10 +0100

    Update tests

--- test/fixtures/dialects/snowflake/snowflake_get_statement.sql
+++ test/fixtures/dialects/snowflake/snowflake_get_statement.sql
@@ -1,4 +1,4 @@
 get @%mytable file://C:\temp\load;
 get @~/myfiles file:///tmp/data/;
 get @~/myfiles file:///tmp/data/ PATTERN = '.*foo.*';
-get @~/myfiles file:///tmp/data/ PATTERN = '.*foo.*';
+get @~/myfiles file:///tmp/data/ PARALLEL = 1;
