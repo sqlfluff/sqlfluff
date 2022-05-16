7843fabbec31430721c5453aa6bc396065e20466
Greg Finley <gregory.finley@gmail.com>
2022-05-15 23:47:04 -0700

    SOQL: Support date_n_literal

--- src/sqlfluff/dialects/dialect_soql.py
+++ src/sqlfluff/dialects/dialect_soql.py
@@ -3,18 +3,16 @@
 https://developer.salesforce.com/docs/atlas.en-us.soql_sosl.meta/soql_sosl/sforce_api_calls_soql.htm
 """
 
+from sqlfluff.core.parser.grammar.anyof import OneOf
+from sqlfluff.core.parser.grammar.sequence import Sequence
 from sqlfluff.dialects import dialect_ansi as ansi
-from sqlfluff.core.parser import Ref
+from sqlfluff.core.parser import BaseSegment, Ref
 from sqlfluff.core.dialects import load_raw_dialect
 
 ansi_dialect = load_raw_dialect("ansi")
 
 soql_dialect = ansi_dialect.copy_as("soql")
 
-"""A Date literal keyword that does not take the :n integer suffix.
-
-https://developer.salesforce.com/docs/atlas.en-us.soql_sosl.meta/soql_sosl/sforce_api_calls_soql_select_dateformats.htm
-"""
 date_literals = [
     "YESTERDAY",
     "TODAY",
@@ -41,9 +39,51 @@ date_literals = [
     "NEXT_FISCAL_YEAR",
 ]
 
-soql_dialect.sets("reserved_keywords").update(date_literals)
+date_n_literals = [
+    "LAST_N_DAYS",
+    "NEXT_N_DAYS",
+    "LAST_N_WEEKS",
+    "NEXT_N_WEEKS",
+    "LAST_N_MONTHS",
+    "NEXT_N_MONTHS",
+    "LAST_N_QUARTERS",
+    "NEXT_N_QUARTERS",
+    "LAST_N_YEARS",
+    "NEXT_N_YEARS",
+    "LAST_N_FISCAL_QUARTERS",
+    "NEXT_N_FISCAL_QUARTERS",
+    "LAST_N_FISCAL_YEARS",
+    "NEXT_N_FISCAL_YEARS",
+]
+
+soql_dialect.sets("reserved_keywords").update(date_literals + date_n_literals)
 
 soql_dialect.sets("bare_functions").update(date_literals)
+
+
+class DateLiteralNSegment(BaseSegment):
+    """A Date literal keyword that takes the :n integer suffix.
+
+    https://developer.salesforce.com/docs/atlas.en-us.soql_sosl.meta/soql_sosl/sforce_api_calls_soql_select_dateformats.htm
+    """
+
+    type = "date_n_literal"
+
+    match_grammar = Sequence(
+        OneOf(*date_n_literals),
+        Ref("ColonSegment"),
+        Ref("NumericLiteralSegment"),
+        allow_gaps=False,
+    )
+
+
+soql_dialect.replace(
+    Expression_C_Grammar=ansi_dialect.get_grammar("Expression_C_Grammar").copy(
+        insert=[
+            Ref("DateLiteralNSegment"),
+        ]
+    )
+)
 
 
 class StatementSegment(ansi.StatementSegment):
