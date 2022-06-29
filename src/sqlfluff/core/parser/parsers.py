e666055391b67cd7ea74163e77c6c6acea699d57
Judah Rand <17158624+judahrand@users.noreply.github.com>
2022-06-29 12:20:15 +0100

    Compile regexes at init time to avoid overhead
    This means that the regexes are only compiled once rather than everytime
    a `RegexParser` is evaluated.

--- src/sqlfluff/core/parser/parsers.py
+++ src/sqlfluff/core/parser/parsers.py
@@ -142,6 +142,9 @@ class RegexParser(StringParser):
     ):
         # Store the optional anti-template
         self.anti_template = anti_template
+        # Compile regexes upfront to avoid repeated overhead
+        self._anti_template = regex.compile(anti_template or r"", regex.IGNORECASE)
+        self._template = regex.compile(template, regex.IGNORECASE)
         super().__init__(
             template=template,
             raw_class=raw_class,
@@ -170,15 +173,13 @@ class RegexParser(StringParser):
             # In any case, it won't match here.
             return False
         # Try the regex. Case sensitivity is not supported.
-        result = regex.match(self.template, segment.raw_upper, regex.IGNORECASE)
+        result = self._template.match(segment.raw_upper)
         if result:
             result_string = result.group(0)
             # Check that we've fully matched
             if result_string == segment.raw_upper:
                 # Check that the anti_template (if set) hasn't also matched
-                if self.anti_template and regex.match(
-                    self.anti_template, segment.raw_upper, regex.IGNORECASE
-                ):
+                if self.anti_template and self._anti_template.match(segment.raw_upper):
                     return False
                 else:
                     return True
