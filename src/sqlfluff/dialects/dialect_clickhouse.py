e42f60cda1a685e948b8ee3d454e711ae0119854
Thomas <thomas.aubry94@gmail.com>
2022-07-02 00:12:07 +0200

    Clickhouse: allow `FINAL` modifier (#3534)
    * allow final modifier in clickhouse dialect
    
    * Update src/sqlfluff/dialects/dialect_clickhouse.py
    
    Co-authored-by: Barry Pollard <barrypollard@google.com>
    
    * Update test/fixtures/dialects/clickhouse/final.sql
    
    Co-authored-by: Barry Pollard <barrypollard@google.com>
    
    Co-authored-by: Barry Pollard <barrypollard@google.com>
    Co-authored-by: Barry Pollard <barry_pollard@hotmail.com>

--- src/sqlfluff/dialects/dialect_clickhouse.py
+++ src/sqlfluff/dialects/dialect_clickhouse.py
@@ -3,15 +3,15 @@
 https://clickhouse.com/
 """
 
+from sqlfluff.core.dialects import load_raw_dialect
 from sqlfluff.core.parser import (
     Bracketed,
     Matchable,
     OneOf,
+    OptionallyBracketed,
     Ref,
     Sequence,
 )
-
-from sqlfluff.core.dialects import load_raw_dialect
 from sqlfluff.dialects import dialect_ansi as ansi
 
 ansi_dialect = load_raw_dialect("ansi")
@@ -45,4 +45,32 @@ class CTEDefinitionSegment(ansi.CTEDefinitionSegment):
             "AS",
             Ref("SingleIdentifierGrammar"),
         ),
+    )
+
+
+class FromExpressionElementSegment(ansi.FromExpressionElementSegment):
+    """A table expression.
+
+    Overridden from ANSI to allow FINAL modifier.
+    https://clickhouse.com/docs/en/sql-reference/statements/select/from#final-modifier
+    """
+
+    type = "from_expression_element"
+    match_grammar: Matchable = Sequence(
+        Ref("PreTableFunctionKeywordsGrammar", optional=True),
+        OptionallyBracketed(Ref("TableExpressionSegment")),
+        Ref(
+            "AliasExpressionSegment",
+            exclude=OneOf(
+                Ref("SamplingExpressionSegment"),
+                Ref("JoinLikeClauseGrammar"),
+                Ref.keyword("Final"),
+            ),
+            optional=True,
+        ),
+        Ref.keyword("FINAL", optional=True),
+        # https://cloud.google.com/bigquery/docs/reference/standard-sql/arrays#flattening_arrays
+        Sequence("WITH", "OFFSET", Ref("AliasExpressionSegment"), optional=True),
+        Ref("SamplingExpressionSegment", optional=True),
+        Ref("PostTableExpressionGrammar", optional=True),
     )
