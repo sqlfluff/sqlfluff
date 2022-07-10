010cb8b298781d9158bb7d125ecdf1f88956d8ac
Greg Finley <gregory.finley@gmail.com>
2022-07-10 10:14:07 -0700

    Smush into a single test using UNIQUE instead of PK

--- test/fixtures/dialects/postgres/postgres_create_table.sql
+++ test/fixtures/dialects/postgres/postgres_create_table.sql
@@ -228,11 +228,6 @@ CREATE TABLE users (
 CREATE TABLE orders
 (
 id bigint NOT NULL DEFAULT NEXTVAL('orders_id_seq'::regclass),
-order_number text PRIMARY KEY COLLATE numeric NOT NULL
-);
-
-CREATE TABLE orders
-(
-id bigint NOT NULL DEFAULT NEXTVAL('orders_id_seq'::regclass),
-order_number text NOT NULL PRIMARY KEY COLLATE numeric
+order_number text UNIQUE COLLATE numeric NOT NULL,
+order_number2 text NOT NULL UNIQUE COLLATE numeric
 );
