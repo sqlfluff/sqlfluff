1f15c4dbec8a634f13c4b3f8042b0405f2df96d2
Gordon Ball <gordon.ball@northvolt.com>
2022-08-02 14:22:25 +0200

    tests: redshift: test expressions in array accessors

--- test/fixtures/dialects/redshift/redshift_array_unnest.sql
+++ test/fixtures/dialects/redshift/redshift_array_unnest.sql
@@ -21,3 +21,10 @@ FROM example_data ed, ed.inventory AS value AT index;
 SELECT c_name, orders.o_orderkey AS orderkey, index AS orderkey_index
 FROM customer_orders_lineitem c, c.c_orders AS orders AT index
 ORDER BY orderkey_index;
+
+-- can extract the correlated values from multiple arrays using the index variable
+SELECT
+    value_a::BIGINT,
+    array_b[idx]::VARCHAR AS value_b,
+    array_c[MOD(idx, 3) + 1]::FLOAT8 AS value_c
+FROM mytable t, t.array_a AS value_a AT idx;
