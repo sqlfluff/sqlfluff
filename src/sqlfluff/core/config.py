4928dc13211261e3e6bd6994ad34cadde6d747bf
Barry Hart <barrywhart@yahoo.com>
2022-08-21 10:35:30 -0400

    Add "# pragma: no cover" to work around sporadic, spurious coverage failure (#3767)
    * Add .gitpod.yml
    
    * Add "# pragma: no cover" to work around sporadic, spurious coverage failure
    
    * Remove added file
    
    Co-authored-by: Barry Hart <barry.hart@zoro.com>
    Co-authored-by: Alan Cruickshank <alanmcruickshank@gmail.com>

--- src/sqlfluff/core/config.py
+++ src/sqlfluff/core/config.py
@@ -543,7 +543,7 @@ class FluffConfig:
         del state["_plugin_manager"]
         return state
 
-    def __setstate__(self, state):
+    def __setstate__(self, state):  # pragma: no cover
         # Restore instance attributes
         self.__dict__.update(state)
         # NB: We don't reinstate the plugin manager, but this should only
