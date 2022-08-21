8f01fa636870155357b2fe125719ff71cc3577de
Alan Cruickshank <alan@designingoverload.com>
2022-08-21 13:34:11 +0100

    better logging

--- src/sqlfluff/utils/testing/rules.py
+++ src/sqlfluff/utils/testing/rules.py
@@ -68,8 +68,9 @@ def assert_rule_fail_in_sql(code, sql, configs=None, line_numbers=None):
     # Lint it using the current config (while in fix mode)
     linted = Linter(config=cfg).lint_string(sql, fix=True)
     lerrs = linted.get_violations()
-    print(f"Errors Found: {lerrs}")
+    print(f"Errors Found:")
     for e in lerrs:
+        print(repr(e))
         if e.desc().startswith("Unexpected exception"):
             pytest.fail(f"Linter failed with {e.desc()}")  # pragma: no cover
     parse_errors = list(
