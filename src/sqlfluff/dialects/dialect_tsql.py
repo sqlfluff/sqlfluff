c60d75a49c2079ed0af85a2d814810aa5b6b0cc3
Felix Möller <mail@felixmoeller.de>
2022-08-28 06:42:32 +0200

    T-SQL: fix parsing partion by NULL (#3790)
    Co-authored-by: Felix Moeller <felix.moeller@noventum.de>
    Co-authored-by: Greg Finley <gregory.finley@gmail.com>

--- src/sqlfluff/dialects/dialect_tsql.py
+++ src/sqlfluff/dialects/dialect_tsql.py
@@ -2025,6 +2025,7 @@ class PartitionClauseSegment(ansi.PartitionClauseSegment):
                     ),
                     Ref("FunctionSegment"),
                     Ref("VariableIdentifierSegment"),
+                    "NULL",
                 ),
             ),
         ),
