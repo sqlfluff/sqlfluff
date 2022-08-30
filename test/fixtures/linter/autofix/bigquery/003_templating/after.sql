6687f9b685a27baefaa44a9536ef61924af8a6a8
Alan Cruickshank <alanmcruickshank@gmail.com>
2022-08-30 08:00:40 +0100

    Indentation fixes and rule logging improvements (#3808)
    * Indentation fixes and rule logging improvements
    
    * Update src/sqlfluff/core/rules/base.py
    
    * coverage
    
    Co-authored-by: Barry Hart <barrywhart@yahoo.com>

--- test/fixtures/linter/autofix/bigquery/003_templating/after.sql
+++ test/fixtures/linter/autofix/bigquery/003_templating/after.sql
@@ -16,13 +16,12 @@ SELECT
 FROM
 {% for action in considered_actions %}
     {% if loop.first %}
-        -- This next section gets very strange with forced template_blocks_indent
         {{action}}_raw_effect_sizes
     {% else %}
     JOIN
-            {{action}}_raw_effect_sizes
+        {{action}}_raw_effect_sizes
         USING
-                    ({{corr_states}})
+            ({{corr_states}})
     {% endif %}
 {% endfor %}
 CROSS JOIN action_states
