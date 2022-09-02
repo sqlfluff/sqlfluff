88ff21afe483a0b368a8f03c77c1dd9d5fad96a5
Alan Cruickshank <alan@designingoverload.com>
2022-09-02 10:24:39 +0100

    linting

--- src/sqlfluff/core/linter/linting_result.py
+++ src/sqlfluff/core/linter/linting_result.py
@@ -113,9 +113,7 @@ class LintingResult:
 
     def violation_dict(self, **kwargs):
         """Return a dict of paths and violations."""
-        return self.combine_dicts(
-            path.violation_dict(**kwargs) for path in self.paths
-        )
+        return self.combine_dicts(path.violation_dict(**kwargs) for path in self.paths)
 
     def stats(self) -> Dict[str, Any]:
         """Return a stats dictionary of this result."""
