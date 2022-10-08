ab86dd12f17942e55da66b8d023f28cc20746333
CommonCrisis <samuel@Brudi.localdomain>
2022-10-08 14:02:48 +0200

    create yaml file

--- test/fixtures/dialects/athena/create_external_table_orc_file_type.sql
+++ test/fixtures/dialects/athena/create_external_table_orc_file_type.sql
@@ -1,8 +1,13 @@
-create external table my_database.my_table(
-    field_1 string,
-    field_2 int,
-    field_3 float
-)  PARTITIONED BY (field_partition string)
-    STORED AS ORC
-    tblproperties ("orc.compress"="ZLIB")
-    LOCATION 's3://athena-examples-myregion/flight/csv/';
+CREATE EXTERNAL TABLE my_table(
+  col_1 string,
+  col_2 boolean,
+  col_3 bigint,
+  col_4 string,
+  col_5 string
+  )
+  PARTITIONED BY (field_partition string)
+  ROW FORMAT SERDE 'org.apache.hadoop.hive.ql.io.orc.OrcSerde'
+  STORED AS INPUTFORMAT 'org.apache.hadoop.hive.ql.io.SymlinkTextInputFormat'
+  OUTPUTFORMAT  'org.apache.hadoop.hive.ql.io.IgnoreKeyTextOutputFormat'
+  LOCATION 's3://athena-examples-myregion/flight/orc_data/'
+  TBLPROPERTIES ('has_encrypted_data'='true');
