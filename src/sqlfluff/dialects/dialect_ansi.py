a6b0720b2096a8cd773cb511b03eb64708264a42
Charles He <charleshe@Charless-MacBook-Pro.local>
2022-10-26 13:15:46 -0700

    format with black

--- src/sqlfluff/dialects/dialect_ansi.py
+++ src/sqlfluff/dialects/dialect_ansi.py
@@ -1148,7 +1148,9 @@ class WindowSpecificationSegment(BaseSegment):
 
     type = "window_specification"
     match_grammar: Matchable = Sequence(
-        Ref("SingleIdentifierGrammar", optional=True, exclude=Ref.keyword("PARTITION")),  # "Base" window name
+        Ref(
+            "SingleIdentifierGrammar", optional=True, exclude=Ref.keyword("PARTITION")
+        ),  # "Base" window name
         Ref("PartitionClauseSegment", optional=True),
         Ref("OrderByClauseSegment", optional=True),
         Ref("FrameClauseSegment", optional=True),
@@ -2707,8 +2709,7 @@ class StructTypeSegment(BaseSegment):
 class CreateTableStatementSegment(BaseSegment):
     """A `CREATE TABLE` statement."""
 
-    type = "" \
-           "create_table_statement"
+    type = "" "create_table_statement"
     # https://crate.io/docs/sql-99/en/latest/chapters/18.html
     # https://www.postgresql.org/docs/12/sql-createtable.html
     match_grammar: Matchable = Sequence(
