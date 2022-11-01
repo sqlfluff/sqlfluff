d2f4fabfe23726b7fffecc4199a1d6c41ee61ea8
rpr <remy.prioul@ableton.com>
2022-11-01 16:11:29 +0100

    add extra test cases

--- test/fixtures/dialects/redshift/redshift_create_external_function.sql
+++ test/fixtures/dialects/redshift/redshift_create_external_function.sql
@@ -10,3 +10,9 @@ STABLE
 LAMBDA 'exfunc_sleep_3'
 IAM_ROLE 'arn:aws:iam::123456789012:role/Redshift-Exfunc-Test'
 RETRY_TIMEOUT 0;
+
+CREATE OR REPLACE EXTERNAL FUNCTION exfunc_foo(varchar)
+RETURNS int
+IMMUTABLE
+SAGEMAKER 'some_endpoint_name'
+IAM_ROLE default;
