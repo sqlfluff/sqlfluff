ba93ada1c85156487836fefa918b90cd08322969
erevear <erevear@gmail.com>
2022-11-02 19:36:35 -0700

    fix flake8 issues

--- src/sqlfluff/rules/L070.py
+++ src/sqlfluff/rules/L070.py
@@ -2,23 +2,19 @@
 from typing import Optional
 
 from sqlfluff.utils.analysis.select_crawler import Query, SelectCrawler, WildcardInfo
-from sqlfluff.core.parser import BaseSegment
 from sqlfluff.core.rules import BaseRule, LintResult, RuleContext
 from sqlfluff.core.rules.crawlers import SegmentSeekerCrawler
 from sqlfluff.core.rules.doc_decorators import document_groups
-from sqlfluff.utils.functional import sp, FunctionalContext
-
-from sqlfluff.utils.analysis.select import SelectStatementColumnsAndTables
-from sqlfluff.utils.analysis.select import get_select_statement_info
-from sqlfluff.utils.functional import sp, Segments
-from typing import Iterator
 
 
 @document_groups
 class Rule_L070(BaseRule):
-    """Queries within set query (UNION, INTERSECT, EXCEPT) produce different numbers of columns.
+    """Queries within set query produce different numbers of columns.
 
     **Anti-pattern**
+
+    When writing set expressions, all queries must return the
+    same number of columns
 
     .. code-block:: sql
         WITH cte AS (
@@ -33,7 +29,12 @@ class Rule_L070(BaseRule):
         , d
         , e
          FROM t
+
     **Best practice**
+
+    Always specify columns when writing set queries
+    and ensure that they all seleect same number of columns
+
     .. code-block:: sql
         WITH cte AS (
             SELECT a, b FROM foo
@@ -49,7 +50,7 @@ class Rule_L070(BaseRule):
     crawl_behaviour = SegmentSeekerCrawler({"set_expression"}, provide_raw_stack=True)
 
     def _find_all_ctes_utils(self, query: Query, cte_dict):
-        """Generate a list of all ctes in a query"""
+        """Generate a list of all ctes in a query."""
         cte_dict = cte_dict | query.ctes
         for cte_name, cte in query.ctes.items():
             cte_dict = self._find_all_ctes_utils(cte, cte_dict)
@@ -63,8 +64,6 @@ class Rule_L070(BaseRule):
                 ).query_tree
                 cte_dict = cte_dict | self._find_all_ctes_utils(parent_query, cte_dict)
 
-                crawler = SelectCrawler(context.segment, context.dialect)
-
         return cte_dict
 
     def __resolve_wildcard(
@@ -75,10 +74,11 @@ class Rule_L070(BaseRule):
         resolve_targets,
         all_ctes,
     ):
-        """Given a query that contains a wildcard, attempt to resolve the wildcard to a list of selectables"""
+        """Attempt to resolve the wildcard to a list of selectables."""
         process_queries = query.selectables
-        # if one of the source queries for a query within the set is a set expression, just use the first
-        # query. If that first query isn't reflective of the others, that will be caught when that segment
+        # if one of the source queries for a query within the set is a
+        # set expression, just use the first query. If that first query isn't
+        # reflective of the others, that will be caught when that segment
         # is processed
         if query.selectables[0].parent:
             if query.selectables[0].parent.type == "set_expression":
@@ -120,7 +120,8 @@ class Rule_L070(BaseRule):
                                     else:
                                         resolve_targets.append(wildcard)
 
-                                # if select_info_target is not a string, process as a subquery
+                                # if select_info_target is not a string
+                                # , process as a subquery
                                 else:
                                     self.__resolve_wildcard(
                                         context,
@@ -171,23 +172,22 @@ class Rule_L070(BaseRule):
         return resolve_targets
 
     def _get_select_target_counts(self, context: RuleContext, crawler: SelectCrawler):
-        """Given a set expression, get the number of select targets in each query"""
+        """Given a set expression, get the number of select targets in each query."""
         select_list = None
         select_target_counts = set()
         set_selectables = crawler.query_tree.selectables
         resolved_wildcard = True
         parent_crawler = SelectCrawler(context.parent_stack[0], context.dialect)
-        # for each selectable in the set, add the number of select targets to a set; in the end
+        # for each selectable in the set
+        # , add the number of select targets to a set; in the end
         # length of the set should be one (one size)
         for selectable in set_selectables:
-            # if the set query contains a wildcard, attempt to resolve wildcard to a list of
+            # if the set query contains a wildcard
+            # , attempt to resolve wildcard to a list of
             # select targets that can be counted
             if selectable.get_wildcard_info():
-                parent_query = SelectCrawler(
-                    context.parent_stack[0], context.dialect
-                ).query_tree
-                # to start, get a list of all of the ctes in the parent stack to check whether they resolve
-                # to wildcards
+                # to start, get a list of all of the ctes in the parent
+                # stack to check whether they resolve to wildcards
 
                 # all_cte_queries = self._find_all_ctes(context, {})
                 all_cte_queries = self._find_all_ctes(context, {})
@@ -203,8 +203,10 @@ class Rule_L070(BaseRule):
                     all_cte_queries,
                 )
 
-                # get the number of resolved targets plus the total number of targets minus the number of wildcards
-                # if all wildcards have been resolved this adds up to the total number of select targets in the query
+                # get the number of resolved targets plus the total number of
+                # targets minus the number of wildcards
+                # if all wildcards have been resolved this adds up to the
+                # total number of select targets in the query
                 select_target_counts.add(
                     len(select_list)
                     + (
@@ -223,14 +225,15 @@ class Rule_L070(BaseRule):
         return (select_target_counts, resolved_wildcard)
 
     def _eval(self, context: RuleContext) -> Optional[LintResult]:
-        """All queries in set expression should return the same number of columns"""
+        """All queries in set expression should return the same number of columns."""
         assert context.segment.is_type("set_expression")
         crawler = SelectCrawler(context.segment, context.dialect)
 
         set_segment_select_sizes, resolve_wildcard = self._get_select_target_counts(
             context, crawler
         )
-        # if queries had different select target counts and all wildcards have been resolved; fail
+        # if queries had different select target counts
+        # and all wildcards have been resolved; fail
         if len(set_segment_select_sizes) > 1 and resolve_wildcard:
             return LintResult(anchor=context.segment)
 
