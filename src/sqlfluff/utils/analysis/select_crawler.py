a23f707deb42059e56ea6e9348b2186e5ec2fb75
Greg Finley <gregory.finley@gmail.com>
2022-11-07 15:26:58 -0800

    Appease mypy

--- src/sqlfluff/utils/analysis/select_crawler.py
+++ src/sqlfluff/utils/analysis/select_crawler.py
@@ -145,13 +145,9 @@ class Query:
         if self.query_type != QueryType.Simple:
             result["query_type"] = self.query_type.name
         if self.selectables:
-            result["selectables"] = [
-                s.as_str() for s in self.selectables
-            ]  # type: ignore
+            result["selectables"] = [s.as_str() for s in self.selectables]
         if self.ctes:
-            result["ctes"] = {
-                k: v.as_json() for k, v in self.ctes.items()
-            }  # type: ignore
+            result["ctes"] = {k: v.as_json() for k, v in self.ctes.items()}
         return result
 
     def lookup_cte(self, name: str, pop: bool = True) -> Optional["Query"]:
