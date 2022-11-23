1d3536706f475a4e927b09edbc431524e6a2356e
Barry Hart <barrywhart@yahoo.com>
2022-11-22 21:21:27 -0500

    Try a different way of resetting between tests

--- plugins/sqlfluff-templater-dbt/test/conftest.py
+++ plugins/sqlfluff-templater-dbt/test/conftest.py
@@ -2,15 +2,14 @@
 import pytest
 
 import sqlfluff_templater_dbt
-from sqlfluff_templater_dbt.osmosis import DbtProjectContainer
 
 
 @pytest.fixture(autouse=True)
 def reset_dbt_projects():
-    """For each test, clear/reset the set of loaded dbt projects.
+    """For each test, drop all loaded dbt projects.
 
     This avoids cached projects causing an issue. Some tests make "in-place"
     changes to a project on disk, which would otherwise affect subsequent
     tests.
     """
-    sqlfluff_templater_dbt.dbt_project_container = DbtProjectContainer()
+    sqlfluff_templater_dbt.dbt_project_container.drop_all_projects()
