54ab72081f8e08f09413b2f0cf5b6fa1cb9511a0
Barry Hart <barrywhart@yahoo.com>
2022-12-09 17:32:35 -0500

    Update diff-quality plugin tests

--- test/diff_quality_plugin_test.py
+++ test/diff_quality_plugin_test.py
@@ -28,12 +28,12 @@ def test_diff_quality_plugin(sql_path, expected_violations_lines, monkeypatch):
     """Test the plugin at least finds errors on the expected lines."""
     monkeypatch.chdir("test/fixtures/")
     violation_reporter = diff_quality_plugin.diff_cover_report_quality()
-    violations = violation_reporter.violations(sql_path)
-    assert isinstance(violations, list)
+    violations_dict = violation_reporter.violations_batch([sql_path])
+    assert isinstance(violations_dict, dict)
     if expected_violations_lines:
-        assert len(violations) > 0
-        violations_lines = {v.line for v in violations}
+        assert len(violations_dict[sql_path]) > 0
+        violations_lines = {v.line for v in violations_dict[sql_path]}
         for expected_line in expected_violations_lines:
             assert expected_line in violations_lines
     else:
-        assert len(violations) == 0
+        assert len(violations_dict[sql_path]) == 0
