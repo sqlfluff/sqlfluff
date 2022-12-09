1e4a61de4c389ee118aba7bdba67b71c4a70c63d
Barry Hart <barrywhart@yahoo.com>
2022-12-09 16:55:31 -0500

    Implement diff_quality_plugin using command-line rather than Python

--- src/sqlfluff/diff_quality_plugin.py
+++ src/sqlfluff/diff_quality_plugin.py
@@ -1,40 +1,90 @@
 """This module integrates SQLFluff with diff_cover's "diff-quality" tool."""
-from typing import List
+import copy
+import json
+import logging
+import os
+import sys
 
+from diff_cover.command_runner import execute, run_command_for_code
 from diff_cover.hook import hookimpl as diff_cover_hookimpl
-from diff_cover.violationsreporters.base import BaseViolationReporter, Violation
+from diff_cover.violationsreporters.base import (
+    QualityDriver,
+    QualityReporter,
+    Violation,
+)
 
-from sqlfluff.core import FluffConfig, Linter
+
+logger = logging.getLogger(__name__)
 
 
-class SQLFluffViolationReporter(BaseViolationReporter):
+class SQLFluffDriver(QualityDriver):
+    def __init__(self):
+        super().__init__(
+            "sqlfluff",
+            [".sql"],
+            [
+                s.encode(sys.getfilesystemencoding())
+                for s in ["sqlfluff", "lint", "--format=json"]
+            ],
+            exit_codes=[0, 1],
+        )
+
+    def parse_reports(self, reports):
+        pass
+
+    def installed(self):
+        """
+        Method checks if the provided tool is installed.
+        Returns: boolean True if installed
+        """
+        return run_command_for_code("sqlfluff") == 0
+
+
+class SQLFluffViolationReporter(QualityReporter):
     """Class that implements diff-quality integration."""
 
     supported_extensions = ["sql"]
 
-    def __init__(self):
+    def __init__(self, reports=None, options=None):
         """Calls the base class constructor to set the object's name."""
-        super().__init__("sqlfluff")
+        super().__init__(SQLFluffDriver())
 
-    @staticmethod
-    def violations(src_path: str) -> List[Violation]:
-        """Return list of violations.
-
-        Given the path to a .sql file, analyze it and return a list of
-        violations (i.e. formatting or style issues).
+    def violations_batch(self, src_paths):
         """
-        linter = Linter(config=FluffConfig.from_root())
-        linted_path = linter.lint_path(src_path, ignore_non_existent_files=True)
-        result = []
-        for violation in linted_path.get_violations():
-            try:
-                # Normal SQLFluff warnings
-                message = f"{violation.rule_code()}: {violation.description}"
-            except AttributeError:
-                # Parse errors
-                message = str(violation)
-            result.append(Violation(violation.line_no, message))
-        return result
+        Return a dictionary of Violations recorded in `src_paths`.
+        """
+        # Check if SQLFluff is installed.
+        if self.driver_tool_installed is None:
+            self.driver_tool_installed = self.driver.installed()
+        if not self.driver_tool_installed:
+            raise OSError(f"{self.driver.name} is not installed")
+
+        # Prepare the SQLFluff command to run.
+        command = copy.deepcopy(self.driver.command)
+        if self.options:
+            for arg in self.options.split():
+                command.append(arg)
+        for src_path in src_paths:
+            if src_path.endswith(".sql") and os.path.exists(src_path):
+                command.append(src_path.encode(sys.getfilesystemencoding()))
+
+        # Run SQLFluff.
+        logger.warning(
+            f"{' '.join([c.decode(sys.getfilesystemencoding()) for c in command])}"
+        )
+        output = execute(command, self.driver.exit_codes)
+        if self.driver.output_stderr:
+            output = output[1]
+        else:
+            output = output[0]
+
+        # Parse the output.
+        report = json.loads(output)
+        for file in report:
+            self.violations_dict[file["filepath"]] = [
+                Violation(v["line_no"], v["description"]) for v in file["violations"]
+            ]
+        return self.violations_dict
 
     def measured_lines(self, src_path: str) -> None:  # pragma: no cover
         """Return list of the lines in src_path that were measured."""
