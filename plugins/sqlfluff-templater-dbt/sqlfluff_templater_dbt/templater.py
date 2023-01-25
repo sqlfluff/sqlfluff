ca47bbbcf7c552a65da4e7ceba513586790d2c7e
Greg Finley <gregory.finley@gmail.com>
2023-01-25 10:34:47 -0800

    Handle renamed dbt exceptions

--- plugins/sqlfluff-templater-dbt/sqlfluff_templater_dbt/templater.py
+++ plugins/sqlfluff-templater-dbt/sqlfluff_templater_dbt/templater.py
@@ -14,10 +14,18 @@ from dbt.config import read_user_config
 from dbt.config.runtime import RuntimeConfig as DbtRuntimeConfig
 from dbt.adapters.factory import register_adapter, get_adapter
 from dbt.compilation import Compiler as DbtCompiler
-from dbt.exceptions import (
-    CompilationException as DbtCompilationException,
-    FailedToConnectException as DbtFailedToConnectException,
-)
+
+try:
+    from dbt.exceptions import (
+        CompilationException as DbtCompilationException,
+        FailedToConnectException as DbtFailedToConnectException,
+    )
+except ImportError:
+    from dbt.exceptions import (
+        CompilationError as DbtCompilationException,
+        FailedToConnectError as DbtFailedToConnectException,
+    )
+
 from dbt import flags
 from jinja2 import Environment
 from jinja2_simple_tags import StandaloneTag
