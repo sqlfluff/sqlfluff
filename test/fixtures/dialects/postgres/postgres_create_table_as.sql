1bf33dfbe477c2605047c03c0750ae6ce9bae386
Aidan Harvey-Nelson <firzen2550@gmail.com>
2023-02-05 09:58:44 +1100

    Updated Test Case

--- test/fixtures/dialects/postgres/postgres_create_table_as.sql
+++ test/fixtures/dialects/postgres/postgres_create_table_as.sql
@@ -117,19 +117,10 @@ CREATE TABLE t1 WITH (val=70) AS
     FROM t2
 ;
 
-create temp table a_new_table
-with (appendoptimized = true, aba = 123) as
+create temp table t1
+with (autovacuum_enabled = true, toast_tuple_target = 123, vacuum_index_cleanup = false) as
 select
     column_1
     , column_2
     , column_3
 from tablename;
-
-create temp table a_new_table
-with (appendoptimized = true, compresstype = 'abc') as
-select
-    column_1
-    , column_2
-    , column_3
-from schema.tablename
-group by 1, 2, 3;
