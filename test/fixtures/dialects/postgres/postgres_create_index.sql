8c4d0f40bd8c42f6fb5d0047e1705ad812506d2c
Dan Ilan <15796788+jmpfar@users.noreply.github.com>
2023-02-05 14:02:20 +0000

    Fixing bugs in postgres CREATE INDEX and adding missing paths
    * Changed code to use `optional` matchers rather than `AnyNumberOf`
      that can match invalid syntax.
    * Fix a bug where operator classes parameters were not matched properly
    * Fix a bug where the `NULLS` keyword was matched by the Opclass name.
      Postgres implements `NULLS` `FIRST`/`LAST` matching using one
      token lookahead. Worked around this issue by disallowing `NULLS` in
      Opclass name.
    * Added `NULLS DISTINCT` / `NULLS NOT DISTINCT` syntax handling
    * Added `TABLESPACE` syntax handling
    * Added `WHERE` syntax handling

--- test/fixtures/dialects/postgres/postgres_create_index.sql
+++ test/fixtures/dialects/postgres/postgres_create_index.sql
@@ -10,6 +10,8 @@ CREATE INDEX title_idx_german ON films (title COLLATE "de_DE");
 
 CREATE INDEX title_idx_nulls_low ON films (title NULLS FIRST);
 
+CREATE INDEX title_idx_nulls_high ON films (title NULLS LAST);
+
 CREATE UNIQUE INDEX title_idx ON films (title) WITH (fillfactor = 70);
 
 CREATE INDEX gin_idx ON documents_table USING GIN (locations) WITH (fastupdate = 'off');
@@ -21,4 +23,19 @@ CREATE INDEX pointloc
 
 CREATE INDEX CONCURRENTLY sales_quantity_index ON sales_table (quantity);
 
-CREATE INDEX super_idx ON super_table USING BTREE (super_column DESC);
+CREATE INDEX super_idx ON super_table USING btree(super_column DESC);
+
+CREATE INDEX opclass_index ON schema.opclass_table (col varchar_pattern_ops);
+
+CREATE INDEX opclass_index_with_parameters ON schema.opclass_table (col varchar_pattern_ops(p1='3', p2='4'));
+
+CREATE UNIQUE INDEX tests_success_constraint ON tests (subject, target)
+    WHERE success;
+
+CREATE INDEX nulls_distinct_index ON documents_table USING GIN (locations)
+     NULLS DISTINCT WITH (fastupdate = 'off');
+
+CREATE INDEX nulls_not_distinct_index ON documents_table USING GIN (locations)
+    NULLS NOT DISTINCT WITH (fastupdate = 'off');
+
+CREATE INDEX code_idx ON films (code) TABLESPACE indexspace;
