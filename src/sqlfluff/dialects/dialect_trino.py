40c6c94c53e6539462ba47e42bfa1c7b780566ac
Eric Fung <eric.fung@shopify.com>
2023-02-08 11:52:48 -0500

    Add date-related bare functions

--- src/sqlfluff/dialects/dialect_trino.py
+++ src/sqlfluff/dialects/dialect_trino.py
@@ -1,40 +1,26 @@
-"""The Trino dialect."""
-
-from sqlfluff.core.parser import (
-    AnyNumberOf,
-    Anything,
-    BaseSegment,
-    Bracketed,
-    CodeSegment,
-    CommentSegment,
-    Dedent,
-    Delimited,
-    Indent,
-    Matchable,
-    TypedParser,
-    NewlineSegment,
-    OneOf,
-    OptionallyBracketed,
-    Ref,
-    RegexLexer,
-    RegexParser,
-    SegmentGenerator,
-    Sequence,
-    SymbolSegment,
-    StartsWith,
-    StringParser,
-)
-from sqlfluff.core.parser.segments.base import BracketedSegment
+"""The Trino dialect. https://trino.io/docs/current/language.html """
 
 from sqlfluff.core.dialects import load_raw_dialect
-from sqlfluff.core.parser.grammar.anyof import AnySetOf
-from sqlfluff.core.parser.lexer import StringLexer
-from sqlfluff.dialects.dialect_trino_keywords import (
-    trino_reserved_keywords,
-    trino_nonreserved_keywords,
+from sqlfluff.core.parser import (
+    Delimited,
+    Ref,
+    Sequence,
 )
-
 from sqlfluff.dialects import dialect_ansi as ansi
 
 ansi_dialect = load_raw_dialect("ansi")
 trino_dialect = ansi_dialect.copy_as("trino")
+
+# Set the bare functions: https://trino.io/docs/current/functions/datetime.html
+trino_dialect.sets("bare_functions").update(
+    ["current_date", "current_time", "current_timestamp", "localtime", "localtimestamp"]
+)
+class ValuesClauseSegment(ansi.ValuesClauseSegment):
+    """A `VALUES` clause within in `WITH`, `SELECT`, `INSERT`."""
+    match_grammar = Sequence(
+        "VALUES",
+        Delimited(
+            Ref("ExpressionSegment"),
+            ephemeral_name="ValuesClauseElements",
+        ),
+    )
