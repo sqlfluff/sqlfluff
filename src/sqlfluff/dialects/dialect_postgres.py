d7703c9d8fec28d2e8d54c59b452f716a0127948
Peter Debelak <pdebelak@gmail.com>
2023-02-09 11:38:42 -0600

    Postgres: column constraint default syntax (#4379)

--- src/sqlfluff/dialects/dialect_postgres.py
+++ src/sqlfluff/dialects/dialect_postgres.py
@@ -2608,11 +2608,10 @@ class ColumnConstraintSegment(ansi.ColumnConstraintSegment):
             Sequence(  # DEFAULT <value>
                 "DEFAULT",
                 OneOf(
+                    Ref("ShorthandCastSegment"),
                     Ref("LiteralGrammar"),
                     Ref("FunctionSegment"),
                     Ref("BareFunctionSegment"),
-                    Ref("ExpressionSegment")
-                    # ?? Ref('IntervalExpressionSegment')
                 ),
             ),
             Sequence("GENERATED", "ALWAYS", "AS", Ref("ExpressionSegment"), "STORED"),
