8293c10cbf8fa6f247116813dc125f8e0e4ef663
Danny Jones <danny.jones1994@live.com>
2023-02-19 16:22:21 +0000

    SQLite Refactor to reduce statement and keyweord scope

--- src/sqlfluff/dialects/dialect_sqlite.py
+++ src/sqlfluff/dialects/dialect_sqlite.py
@@ -12,15 +12,23 @@ from sqlfluff.core.parser import (
     OptionallyBracketed,
     Ref,
     Sequence,
+    Delimited,
+    TypedParser,
+    Nothing,
+    AnyNumberOf,
+    Anything,
 )
 from sqlfluff.dialects import dialect_ansi as ansi
+from sqlfluff.dialects.dialect_sqlite_keywords import RESERVED_KEYWORDS, UNRESERVED_KEYWORDS
 
 ansi_dialect = load_raw_dialect("ansi")
 
 sqlite_dialect = ansi_dialect.copy_as("sqlite")
 
-sqlite_dialect.sets("reserved_keywords").update(["AUTOINCREMENT"])
-sqlite_dialect.sets("unreserved_keywords").update(["FAIL"])
+sqlite_dialect.sets("reserved_keywords").clear()
+sqlite_dialect.sets("reserved_keywords").update(RESERVED_KEYWORDS)
+sqlite_dialect.sets("unreserved_keywords").clear()
+sqlite_dialect.sets("unreserved_keywords").update(UNRESERVED_KEYWORDS)
 
 sqlite_dialect.replace(
     BooleanBinaryOperatorGrammar=OneOf(
@@ -29,7 +37,241 @@ sqlite_dialect.replace(
     PrimaryKeyGrammar=Sequence(
         "PRIMARY", "KEY", Sequence("AUTOINCREMENT", optional=True)
     ),
+    TemporaryTransientGrammar=Ref("TemporaryGrammar"),
+    DateTimeLiteralGrammar=Sequence(
+        OneOf("DATE", "DATETIME"),
+        TypedParser("single_quote", ansi.LiteralSegment, type="date_constructor_literal"),
+    ),
+    BaseExpressionElementGrammar=OneOf(
+        Ref("LiteralGrammar"),
+        Ref("BareFunctionSegment"),
+        Ref("FunctionSegment"),
+        Ref("ColumnReferenceSegment"),
+        Ref("ExpressionSegment"),
+        Sequence(
+            Ref("DatatypeSegment"),
+            Ref("LiteralGrammar"),
+        ),
+    ),
+    AutoIncrementGrammar=Nothing(),
+    CommentClauseSegment=Nothing(),
+    IntervalExpressionSegment=Nothing(),
+    TimeZoneGrammar=Nothing(),
+    TrimParametersGrammar=Nothing(),
+    LikeGrammar=Sequence("LIKE"),
+    OverlapsClauseSegment=Nothing(),
+    MLTableExpressionSegment=Nothing(),
+    MergeIntoLiteralGrammar=Nothing(),
+    SamplingExpressionSegment=Nothing(),
+    OrderByClauseTerminators=OneOf(
+        "LIMIT",
+        # For window functions
+        "WINDOW",
+        Ref("FrameClauseUnitGrammar"),
+    ),
+    WhereClauseTerminatorGrammar=OneOf(
+        "LIMIT",
+        Sequence("GROUP", "BY"),
+        Sequence("ORDER", "BY"),
+        "WINDOW",
+    ),
+    FromClauseTerminatorGrammar=OneOf(
+        "WHERE",
+        "LIMIT",
+        Sequence("GROUP", "BY"),
+        Sequence("ORDER", "BY"),
+        "WINDOW",
+        Ref("SetOperatorSegment"),
+        Ref("WithNoSchemaBindingClauseSegment"),
+        Ref("WithDataClauseSegment"),
+    ),
+    SelectClauseElementTerminatorGrammar=OneOf(
+        "FROM",
+        "WHERE",
+        Sequence("ORDER", "BY"),
+        "LIMIT",
+        Ref("SetOperatorSegment"),
+    ),
+    FunctionContentsGrammar=AnyNumberOf(
+        Ref("ExpressionSegment"),
+        # A Cast-like function
+        Sequence(Ref("ExpressionSegment"), "AS", Ref("DatatypeSegment")),
+        # Trim function
+        Sequence(
+            Ref("TrimParametersGrammar"),
+            Ref("ExpressionSegment", optional=True, exclude=Ref.keyword("FROM")),
+            "FROM",
+            Ref("ExpressionSegment"),
+        ),
+        # An extract-like or substring-like function
+        Sequence(
+            OneOf(Ref("DatetimeUnitSegment"), Ref("ExpressionSegment")),
+            "FROM",
+            Ref("ExpressionSegment"),
+        ),
+        Sequence(
+            # Allow an optional distinct keyword here.
+            Ref.keyword("DISTINCT", optional=True),
+            OneOf(
+                # Most functions will be using the delimited route
+                # but for COUNT(*) or similar we allow the star segment
+                # here.
+                Ref("StarSegment"),
+                Delimited(Ref("FunctionContentsExpressionGrammar")),
+            ),
+        ),
+        Ref(
+            "OrderByClauseSegment"
+        ),  # used by string_agg (postgres), group_concat (exasol),listagg (snowflake)..
+        # like a function call: POSITION ( 'QL' IN 'SQL')
+        Sequence(
+            OneOf(
+                Ref("QuotedLiteralSegment"),
+                Ref("SingleIdentifierGrammar"),
+                Ref("ColumnReferenceSegment"),
+            ),
+            "IN",
+            OneOf(
+                Ref("QuotedLiteralSegment"),
+                Ref("SingleIdentifierGrammar"),
+                Ref("ColumnReferenceSegment"),
+            ),
+        ),
+        Ref("IndexColumnDefinitionSegment"),
+    ),
+    Expression_A_Grammar=Sequence(
+        OneOf(
+            Ref("Expression_C_Grammar"),
+            Sequence(
+                OneOf(
+                    Ref("SignedSegmentGrammar"),
+                    # Ref('TildeSegment'),
+                    Ref("NotOperatorGrammar"),
+                    # used in CONNECT BY clauses (EXASOL, Snowflake, Postgres...)
+                ),
+                Ref("Expression_C_Grammar"),
+            ),
+        ),
+        AnyNumberOf(
+            OneOf(
+                Sequence(
+                    OneOf(
+                        Sequence(
+                            Ref.keyword("NOT", optional=True),
+                            Ref("LikeGrammar"),
+                        ),
+                        Sequence(
+                            Ref("BinaryOperatorGrammar"),
+                            Ref.keyword("NOT", optional=True),
+                        ),
+                        # We need to add a lot more here...
+                    ),
+                    Ref("Expression_C_Grammar"),
+                    Sequence(
+                        Ref.keyword("ESCAPE"),
+                        Ref("Expression_C_Grammar"),
+                        optional=True,
+                    ),
+                ),
+                Sequence(
+                    Ref.keyword("NOT", optional=True),
+                    "IN",
+                    Bracketed(
+                        OneOf(
+                            Delimited(
+                                Ref("Expression_A_Grammar"),
+                            ),
+                            Ref("SelectableGrammar"),
+                            ephemeral_name="InExpression",
+                        )
+                    ),
+                ),
+                Sequence(
+                    Ref.keyword("NOT", optional=True),
+                    "IN",
+                    Ref("FunctionSegment"),  # E.g. UNNEST()
+                ),
+                Sequence(
+                    "IS",
+                    Ref.keyword("NOT", optional=True),
+                    Ref("IsClauseGrammar"),
+                ),
+                Ref("IsNullGrammar"),
+                Ref("NotNullGrammar"),
+                Ref("CollateGrammar"),
+                Sequence(
+                    # e.g. NOT EXISTS, but other expressions could be met as
+                    # well by inverting the condition with the NOT operator
+                    "NOT",
+                    Ref("Expression_C_Grammar"),
+                ),
+                Sequence(
+                    Ref.keyword("NOT", optional=True),
+                    "BETWEEN",
+                    Ref("Expression_B_Grammar"),
+                    "AND",
+                    Ref("Expression_A_Grammar"),
+                ),
+            )
+        ),
+    ),
 )
+
+
+class SetOperatorSegment(BaseSegment):
+    """A set operator such as Union, Minus, Except or Intersect."""
+
+    type = "set_operator"
+    match_grammar: Matchable = OneOf(
+        Sequence("UNION", OneOf("DISTINCT", "ALL", optional=True)),
+        Sequence(
+            OneOf(
+                "INTERSECT",
+                "EXCEPT",
+            ),
+            Ref.keyword("ALL", optional=True),
+        ),
+        exclude=Sequence("EXCEPT", Bracketed(Anything())),
+    )
+
+
+class DatatypeSegment(ansi.DatatypeSegment):
+    """A data type segment.
+
+    Supports timestamp with(out) time zone. Doesn't currently support intervals.
+    """
+
+    type = "data_type"
+    match_grammar: Matchable = OneOf(
+        Sequence(
+            "DOUBLE",
+            "PRECISION",
+        ),
+        Sequence(
+            "UNSIGNED",
+            "BIG",
+            "INT"
+        ),
+        Sequence(
+            OneOf(
+                Sequence(
+                    OneOf("VARYING", "NATIVE"),
+                    OneOf("CHARACTER"),
+                ),
+                Ref("DatatypeIdentifierSegment")
+            ),
+            Bracketed(
+                OneOf(
+                    Delimited(Ref("ExpressionSegment")),
+                    # The brackets might be empty for some cases...
+                    optional=True,
+                ),
+                # There may be no brackets for some data types
+                optional=True,
+            ),
+        ),
+    )
+
 
 
 class TableEndClauseSegment(BaseSegment):
@@ -40,6 +282,26 @@ class TableEndClauseSegment(BaseSegment):
 
     type = "table_end_clause_segment"
     match_grammar: Matchable = Sequence("WITHOUT", "ROWID")
+
+
+class ValuesClauseSegment(ansi.ValuesClauseSegment):
+    """A `VALUES` clause like in `INSERT`."""
+
+    type = "values_clause"
+    match_grammar: Matchable = Sequence(
+        "VALUES",
+        Delimited(
+            Sequence(
+                Bracketed(
+                    Delimited(
+                        "DEFAULT",
+                        Ref("ExpressionSegment"),
+                        ephemeral_name="ValuesClauseElements",
+                    )
+                ),
+            ),
+        ),
+    )
 
 
 class IndexColumnDefinitionSegment(BaseSegment):
@@ -148,4 +410,43 @@ class TableConstraintSegment(ansi.TableConstraintSegment):
             Sequence("INITIALLY", "IMMEDIATE"),
             optional=True,
         ),
+    )
+
+
+class TransactionStatementSegment(ansi.TransactionStatementSegment):
+    """A `COMMIT`, `ROLLBACK` or `TRANSACTION` statement.
+
+    As per https://www.sqlite.org/lang_transaction.html
+    """
+
+    type = "transaction_statement"
+    match_grammar: Matchable = Sequence(
+        OneOf("BEGIN", "COMMIT", "ROLLBACK", "END"),
+        OneOf("TRANSACTION", optional=True),
+        Sequence("TO", "SAVEPOINT", Ref("ObjectReferenceSegment"), optional=True),
+    )
+
+
+class StatementSegment(ansi.StatementSegment):
+    """Overriding StatementSegment to allow for additional segment parsing."""
+
+    match_grammar = ansi.StatementSegment.match_grammar
+
+    parse_grammar: Matchable = OneOf(
+        Ref("AlterTableStatementSegment"),
+        Ref("CreateIndexStatementSegment"),
+        Ref("CreateTableStatementSegment"),
+        Ref("CreateTriggerStatementSegment"),
+        Ref("CreateViewStatementSegment"),
+        Ref("DeleteStatementSegment"),
+        Ref("DropIndexStatementSegment"),
+        Ref("DropTableStatementSegment"),
+        Ref("DropTriggerStatementSegment"),
+        Ref("DropViewStatementSegment"),
+        Ref("ExplainStatementSegment"),
+        Ref("InsertStatementSegment"),
+        Ref("SelectableGrammar"),
+        Ref("TransactionStatementSegment"),
+        Ref("UpdateStatementSegment"),
+        Bracketed(Ref("StatementSegment")),
     )
