18cf1ac147f501361a3b75973035d4942274bef6
Alan Cruickshank <alan@designingoverload.com>
2023-02-25 19:50:03 +0000

    Forgot to import

--- src/sqlfluff/rules/aliasing/__init__.py
+++ src/sqlfluff/rules/aliasing/__init__.py
@@ -1,15 +1,16 @@
-"""The capitalisation plugin bundle."""
+"""The aliasing plugin bundle."""
 
 from sqlfluff.core.plugin import hookimpl
 
-from sqlfluff.rules.capitalisation.CP01 import Rule_CP01
-from sqlfluff.rules.capitalisation.CP02 import Rule_CP02
-from sqlfluff.rules.capitalisation.CP03 import Rule_CP03
-from sqlfluff.rules.capitalisation.CP04 import Rule_CP04
-from sqlfluff.rules.capitalisation.CP05 import Rule_CP05
+from sqlfluff.rules.aliasing.AL01 import Rule_AL01
+from sqlfluff.rules.aliasing.AL02 import Rule_AL02
+from sqlfluff.rules.aliasing.AL03 import Rule_AL03
+from sqlfluff.rules.aliasing.AL04 import Rule_AL04
+from sqlfluff.rules.aliasing.AL05 import Rule_AL05
+from sqlfluff.rules.aliasing.AL06 import Rule_AL06
 
 
 @hookimpl
 def get_rules():
     """Get plugin rules."""
-    return [Rule_CP01, Rule_CP02, Rule_CP03, Rule_CP04, Rule_CP05]
+    return [Rule_AL01, Rule_AL02, Rule_AL03, Rule_AL04, Rule_AL05, Rule_AL06]
