dcd2a217ba97bf742471ae3b03ba5277bbdc1f08
Alan Cruickshank <alan@designingoverload.com>
2023-03-01 14:13:41 +0000

    imports and plugins reg

--- src/sqlfluff/rules/references/__init__.py
+++ src/sqlfluff/rules/references/__init__.py
@@ -7,10 +7,10 @@ from sqlfluff.rules.references.RF02 import Rule_RF02
 from sqlfluff.rules.references.RF03 import Rule_RF03
 from sqlfluff.rules.references.RF04 import Rule_RF04
 from sqlfluff.rules.references.RF05 import Rule_RF05
-from sqlfluff.rules.references.RF05 import Rule_RF05
+from sqlfluff.rules.references.RF06 import Rule_RF06
 
 
 @hookimpl
 def get_rules():
     """Get plugin rules."""
-    return [Rule_RF01, Rule_RF02, Rule_RF03, Rule_RF04, Rule_RF05, Rule_RF05]
+    return [Rule_RF01, Rule_RF02, Rule_RF03, Rule_RF04, Rule_RF05, Rule_RF06]
