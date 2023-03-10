39d86f594249123ed9b2aed580cd888e473d03cf
konnectr <46676677+konnectr@users.noreply.github.com>
2023-03-10 14:02:24 +0500

    Clickhouse support all join types  (#4488)
    Co-authored-by: Barry Pollard <barrypollard@google.com>
    Co-authored-by: Greg Finley <gregory.finley@gmail.com>

---
+++ test/fixtures/dialects/clickhouse/join.sql
@@ -0,0 +1,60 @@
+-- no type join
+SELECT * FROM test1 ALL JOIN test2 ON test2.ty1=test1.ty1;
+SELECT * FROM test1 ANY JOIN test2 ON test2.ty1=test1.ty1;
+SELECT * FROM test1 JOIN test2 ON test2.ty1=test1.ty1;
+-- INNER join
+SELECT * FROM test1 INNER JOIN test2 ON test2.ty1=test1.ty1;
+-- INNER join ...
+SELECT * FROM test1 INNER ALL JOIN test2 ON test2.ty1=test1.ty1;
+SELECT * FROM test1 INNER ANY JOIN test2 ON test2.ty1=test1.ty1;
+-- ... INNER join
+SELECT * FROM test1 ALL INNER JOIN test2 ON test2.ty1=test1.ty1;
+SELECT * FROM test1 ANY INNER JOIN test2 ON test2.ty1=test1.ty1;
+-- LEFT JOIN
+SELECT * FROM test1 LEFT JOIN test2 ON test2.ty1=test1.ty1;
+-- LEFT join ...
+SELECT tbl1.id FROM tbl1 LEFT ANTI join tbl2 on tbl1.id = tbl2.id;
+SELECT * FROM test1 as t1 LEFT SEMI JOIN test2 USING ty1,ty2;
+SELECT * FROM test1 as t1 LEFT ANY JOIN test2 USING ty1,ty2;
+SELECT * FROM test1 as t1 LEFT ALL JOIN test2 USING ty1,ty2;
+SELECT * FROM test1 as t1 LEFT ASOF JOIN test2 USING ty1,ty2;
+-- ... LEFT join
+select tbl1.id from tbl1  ANTI LEFT join tbl2 on tbl1.id = tbl2.id;
+SELECT * FROM test1 as t1 SEMI LEFT JOIN test2 USING ty1,ty2;
+SELECT * FROM test1 as t1 ANY LEFT JOIN test2 USING ty1,ty2;
+SELECT * FROM test1 as t1 ALL LEFT JOIN test2 USING ty1,ty2;
+SELECT * FROM test1 as t1 ASOF LEFT JOIN test2 USING (ty1,ty2);
+-- LEFT join test case OUTER
+SELECT * FROM test1 as t1 LEFT OUTER JOIN test2 USING ty1,ty2;
+SELECT * FROM test1 as t1 LEFT ASOF OUTER JOIN test2 USING ty1,ty2;
+SELECT tbl1.id FROM tbl1 LEFT ANTI OUTER join tbl2 on tbl1.id = tbl2.id;
+SELECT * FROM test1 as t1 LEFT SEMI OUTER JOIN test2 USING ty1,ty2;
+SELECT * FROM test1 as t1 LEFT ANY OUTER JOIN test2 USING ty1,ty2;
+SELECT * FROM test1 as t1 LEFT ALL OUTER JOIN test2 USING ty1,ty2;
+SELECT * FROM test1 as t1 LEFT ASOF OUTER JOIN test2 USING ty1,ty2;
+-- RIGHT JOIN
+SELECT * FROM test1 RIGHT JOIN test2 ON test2.ty1=test1.ty1;
+-- RIGHT join ...
+SELECT tbl1.id FROM tbl1 RIGHT ANTI join tbl2 on tbl1.id = tbl2.id;
+SELECT * FROM test1 as t1 RIGHT SEMI JOIN test2 USING ty1,ty2;
+SELECT * FROM test1 as t1 RIGHT ANY JOIN test2 USING ty1,ty2;
+SELECT * FROM test1 as t1 RIGHT ALL JOIN test2 USING ty1,ty2;
+-- ... RIGHT join
+select tbl1.id from tbl1  ANTI RIGHT join tbl2 on tbl1.id = tbl2.id;
+SELECT * FROM test1 as t1 SEMI RIGHT JOIN test2 USING ty1,ty2;
+SELECT * FROM test1 as t1 ANY RIGHT JOIN test2 USING ty1,ty2;
+SELECT * FROM test1 as t1 ALL RIGHT JOIN test2 USING ty1,ty2;
+-- RIGHT join test case OUTER
+SELECT * FROM test1 as t1 RIGHT OUTER JOIN test2 USING ty1,ty2;
+SELECT * FROM test1 as t1 RIGHT ANTI OUTER JOIN test2 USING ty1,ty2;
+SELECT * FROM test1 as t1 RIGHT SEMI OUTER JOIN test2 USING ty1,ty2;
+SELECT * FROM test1 as t1 RIGHT ANY OUTER JOIN test2 USING ty1,ty2;
+SELECT * FROM test1 as t1 RIGHT ALL OUTER JOIN test2 USING ty1,ty2;
+-- ASOF join
+select tbl1.id from tbl1 ASOF JOIN tbl2 on tbl1.id = tbl2.id;
+-- CROSS join
+SELECT * FROM test1 CROSS JOIN test2;
+-- FULL join
+SELECT * FROM test1 as t1 FULL ALL JOIN test2 USING ty1,ty2;
+SELECT * FROM test1 as t1 FULL JOIN test2 USING ty1,ty2;
+SELECT * FROM test1 as t1 FULL ALL OUTER JOIN test2 USING ty1,ty2;
