757069821cd8ddc8cb2bc2f26b38f97c50974bb4
Gergely Imreh <gergely@imreh.net>
2023-03-12 13:35:37 +0800

    BigQuery: correct query syntax for single column `UNPIVOT` clauses (#4494)

--- src/sqlfluff/dialects/dialect_bigquery.py
+++ src/sqlfluff/dialects/dialect_bigquery.py
@@ -1779,16 +1779,22 @@ class FromUnpivotExpressionSegment(BaseSegment):
             optional=True,
         ),
         OneOf(
+            # single column unpivot
             Bracketed(
                 Ref("SingleIdentifierGrammar"),
                 "FOR",
                 Ref("SingleIdentifierGrammar"),
                 "IN",
                 Bracketed(
-                    Delimited(Ref("SingleIdentifierGrammar")),
-                    Ref("UnpivotAliasExpressionSegment", optional=True),
+                    Delimited(
+                        Sequence(
+                            Delimited(Ref("SingleIdentifierGrammar")),
+                            Ref("UnpivotAliasExpressionSegment", optional=True),
+                        ),
+                    ),
                 ),
             ),
+            # multi column unpivot
             Bracketed(
                 Bracketed(
                     Delimited(
