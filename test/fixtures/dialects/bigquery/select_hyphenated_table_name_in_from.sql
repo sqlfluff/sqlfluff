f936b358516c12db6655e4c2c13c496883ce8b23
Alan Cruickshank <alanmcruickshank@gmail.com>
2023-03-15 23:35:31 +0000

    Bigquery hyphentated identifiers (#4530)

--- test/fixtures/dialects/bigquery/select_hyphenated_table_name_in_from.sql
+++ test/fixtures/dialects/bigquery/select_hyphenated_table_name_in_from.sql
@@ -1,5 +1,9 @@
 SELECT * FROM project-a.dataset-b.table-c JOIN dataset-c.table-d USING (a);
 
+SELECT * FROM a-1a.b.c;
+SELECT * FROM a-1.b.c;
+SELECT * FROM project23-123.dataset7-b1.table-2c JOIN dataset12-c1.table-1d USING (a);
+
 SELECT
    col1-col2 AS newcol1,
    col1 - col2 AS newcol2
