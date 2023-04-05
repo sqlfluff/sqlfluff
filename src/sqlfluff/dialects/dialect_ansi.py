ec6fc39998d4146a6b43e51c42c7d1b821da18df
Germain Lefebvre <glefebvre@ineat.fr>
2023-04-05 22:38:24 +0200

    fix(ansi): Ad single quoted identifier in SingleIdentifierGrammar

--- src/sqlfluff/dialects/dialect_ansi.py
+++ src/sqlfluff/dialects/dialect_ansi.py
@@ -404,7 +404,7 @@ ansi_dialect.add(
     FalseSegment=StringParser("false", LiteralKeywordSegment, type="boolean_literal"),
     # We use a GRAMMAR here not a Segment. Otherwise, we get an unnecessary layer
     SingleIdentifierGrammar=OneOf(
-        Ref("NakedIdentifierSegment"), Ref("QuotedIdentifierSegment")
+        Ref("NakedIdentifierSegment"), Ref("QuotedIdentifierSegment"), Ref("SingleQuotedIdentifierSegment")
     ),
     BooleanLiteralGrammar=OneOf(Ref("TrueSegment"), Ref("FalseSegment")),
     # We specifically define a group of arithmetic operators to make it easier to
