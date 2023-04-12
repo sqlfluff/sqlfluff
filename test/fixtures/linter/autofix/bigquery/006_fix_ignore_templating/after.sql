6a56d6f328f1dd534701e36f06576e0a7f641f2b
Alan Cruickshank <alan@designingoverload.com>
2023-04-12 17:37:47 +0100

    update test cases

--- test/fixtures/linter/autofix/bigquery/006_fix_ignore_templating/after.sql
+++ test/fixtures/linter/autofix/bigquery/006_fix_ignore_templating/after.sql
@@ -2,10 +2,13 @@ SELECT * EXCEPT ({% include query %}) FROM
     (
         SELECT
             tbl1.*,
-            row_number() OVER (
-                PARTITION BY tbl1.the_name, {{ context_columns | join(', ') }}
-                ORDER BY created_at DESC
-            ) AS rnk
+            row_number()
+                OVER (
+                    PARTITION BY
+                        tbl1.the_name, {{ context_columns | join(', ') }}
+                    ORDER BY created_at DESC
+                )
+                AS rnk
             {% if context_columns | default("abc") == "abc" %}
             FROM tbl1
             {% endif %}
