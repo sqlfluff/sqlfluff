54a1a6dd0b26157c3e0109f49691ed8f979bf358
James Johnston <james.johnston@thumbtack.com>
2023-04-13 03:56:32 -0700

    Remove CONCURRENTLY from ANSI keywords
    It's only for PostgreSQL.

--- src/sqlfluff/dialects/dialect_ansi.py
+++ src/sqlfluff/dialects/dialect_ansi.py
@@ -3121,7 +3121,7 @@ class DropIndexStatementSegment(BaseSegment):
     """A `DROP INDEX` statement."""
 
     type = "drop_index_statement"
-    # DROP INDEX <Index name> [CONCURRENTLY] [IF EXISTS] {RESTRICT | CASCADE}
+    # DROP INDEX <Index name> [IF EXISTS] {RESTRICT | CASCADE}
     match_grammar: Matchable = Sequence(
         "DROP",
         "INDEX",
