749bc2a3e256850b3d9df3a87991fdf63889ca7a
Michal Ondras <michal@coinlist.co>
2023-04-18 23:10:12 -0700

    wip12

--- test/rules/std_AM08_test.py
+++ test/rules/std_AM08_test.py
@@ -6,11 +6,12 @@ def test__rules__std_AM08_raised() -> None:
     """Test case for multiple AM08 errors raised with 'consistent' setting."""
     sql = """
     SELECT
-        foo,
+        LAST_VALUE(foo) OVER (PARTITION BY bar ORDER BY baz) as boo,
         bar,
     FROM foo
     ORDER BY 1, 2;
     """
+
     result = sqlfluff.lint(sql)
     results_AM08 = [r for r in result if r["code"] == "AM08"]
     assert len(results_AM08) == 1
@@ -18,3 +19,21 @@ def test__rules__std_AM08_raised() -> None:
          results_AM08[0]["description"]
          == "Unnecessary 'ORDER BY' clauses."
     )
+
+def test__rules__std_AM08_fix() -> None:
+    """Test case for multiple AM08 errors raised with 'consistent' setting."""
+    sql = """
+    SELECT
+        foo,
+        bar,
+    FROM foo
+    ORDER BY 1, 2;
+    """
+
+    result = sqlfluff.fix(sql)
+    assert result == """
+    SELECT
+        foo,
+        bar,
+    FROM foo;
+    """
