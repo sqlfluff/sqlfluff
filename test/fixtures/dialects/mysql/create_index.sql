2cf367728a3b498e0d50dfae66713c93b4da0951
William Felipe Welter <wfelipew@gmail.com>
2023-05-12 15:14:51 +0100

    Adding support for NOCOPY and INSTANT algorithm on CREATE INDEX on MySQL dialect (#4865)

--- test/fixtures/dialects/mysql/create_index.sql
+++ test/fixtures/dialects/mysql/create_index.sql
@@ -8,5 +8,7 @@ CREATE INDEX idx ON tbl (col ASC);
 CREATE INDEX idx ON tbl (col DESC);
 CREATE INDEX part_of_name ON customer (name(10));
 CREATE INDEX idx ON tbl (col) ALGORITHM DEFAULT;
+CREATE INDEX idx ON tbl (col) ALGORITHM NOCOPY;
+CREATE INDEX idx ON tbl (col) ALGORITHM INSTANT;
 CREATE INDEX idx ON tbl (col) LOCK DEFAULT;
 CREATE INDEX idx ON tbl ((col1 + col2), (col1 - col2), col1);
