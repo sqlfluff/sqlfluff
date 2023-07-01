24056e38fc0099c73a62e4e02a1dc5bbbca71fb8
Alan Cruickshank <alan@designingoverload.com>
2023-07-01 08:36:31 +0100

    coverage

--- plugins/sqlfluff-templater-dbt/sqlfluff_templater_dbt/templater.py
+++ plugins/sqlfluff-templater-dbt/sqlfluff_templater_dbt/templater.py
@@ -22,8 +22,10 @@ from dbt.cli.resolvers import default_profiles_dir
 # On the 1.5.x branch this was between 1.5.1 and 1.5.2
 try:
     from dbt.task.contextvars import cv_project_root
-except ImportError:
-    cv_project_root = None
+except ImportError:  # pragma: no cover
+    # This path _is used_ on 1.5.1, but our test suite doesn't cover
+    # that case.
+    cv_project_root = None  
 
 try:
     from dbt.exceptions import (
