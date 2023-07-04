31e5ef37a6d7ac24f163d09c6df38fca68ac9d50
Eric Fung <eric.fung@shopify.com>
2023-07-04 11:28:59 -0400

    Fix how keywords are defined
    Follow example in other dialects to set the reserved and
    non-reserved words, in a way that doesn't break code
    coverage.
    
    However, this resulted in a bunch of default things in the
    parent ANSI dialect breaking, since Trino doesn't support
    a lot of regular SQL (since it's mostly used for querying).

--- src/sqlfluff/dialects/dialect_trino.py
+++ src/sqlfluff/dialects/dialect_trino.py
@@ -1,14 +1,25 @@
-"""The Trino dialect. https://trino.io/docs/current/language.html """
+"""The Trino dialect.
+
+See https://trino.io/docs/current/language.html
+"""
 
 from sqlfluff.core.dialects import load_raw_dialect
 from sqlfluff.core.parser import (
+    Anything,
     BaseSegment,
+    Bracketed,
     Delimited,
+    Matchable,
+    Nothing,
     OneOf,
     Ref,
     Sequence,
-    Matchable,
+    StartsWith,
     TypedParser,
+)
+from sqlfluff.dialects.dialect_trino_keywords import (
+    trino_reserved_keywords,
+    trino_unreserved_keywords,
 )
 from sqlfluff.dialects import dialect_ansi as ansi
 
@@ -20,6 +31,17 @@ trino_dialect.sets("bare_functions").update(
     ["current_date", "current_time", "current_timestamp", "localtime", "localtimestamp"]
 )
 
+# Set keywords
+trino_dialect.sets("unreserved_keywords").clear()
+trino_dialect.update_keywords_set_from_multiline_string(
+    "unreserved_keywords", trino_unreserved_keywords
+)
+
+trino_dialect.sets("reserved_keywords").clear()
+trino_dialect.update_keywords_set_from_multiline_string(
+    "reserved_keywords", trino_reserved_keywords
+)
+
 trino_dialect.replace(
     DateTimeLiteralGrammar=OneOf(
         Sequence(
@@ -29,8 +51,121 @@ trino_dialect.replace(
             ),
         ),
         Ref("IntervalExpressionSegment"),
-    )
+    ),
+    LikeGrammar=Sequence("LIKE"),
+    # TODO: There are no custom SQL functions in Trino! How to handle this?
+    MLTableExpressionSegment=Nothing(),
+    OrderByClauseTerminators=OneOf(
+        "LIMIT",
+        "HAVING",
+        # For window functions
+        "WINDOW",
+        Ref("FrameClauseUnitGrammar"),
+        "FETCH",
+    ),
+    SelectClauseElementTerminatorGrammar=OneOf(
+        "FROM",
+        "WHERE",
+        Sequence("ORDER", "BY"),
+        "LIMIT",
+        Ref("SetOperatorSegment"),
+        "FETCH",
+    ),
+    WhereClauseTerminatorGrammar=OneOf(
+        "LIMIT",
+        Sequence("GROUP", "BY"),
+        Sequence("ORDER", "BY"),
+        "HAVING",
+        "WINDOW",
+        "FETCH",
+    ),
+    # NOTE: This block was copy/pasted from dialect_ansi.py with these changes made:
+    #  - "PRIOR" keyword removed
+    Expression_A_Unary_Operator_Grammar=OneOf(
+        Ref(
+            "SignedSegmentGrammar",
+            exclude=Sequence(Ref("QualifiedNumericLiteralSegment")),
+        ),
+        Ref("TildeSegment"),
+        Ref("NotOperatorGrammar"),
+    ),
 )
+
+
+class DatatypeSegment(BaseSegment):
+    """Data type segment.
+
+    See https://trino.io/docs/current/language/types.html
+    """
+
+    type = "data_type"
+    match_grammar = OneOf(
+        # Boolean
+        "BOOLEAN",
+        # Integer
+        "TINYINT",
+        "SMALLINT",
+        "INTEGER",
+        "BIGINT",
+        # Floating-point
+        "REAL",
+        "DOUBLE",
+        # Fixed-precision
+        Sequence(
+            "DECIMAL",
+            Ref("BracketedArguments", optional=True),
+        ),
+        # String
+        Sequence(
+            OneOf("CHAR", "VARCHAR"),
+            Ref("BracketedArguments", optional=True),
+        ),
+        "VARBINARY",
+        "JSON",
+        # Date and time
+        "DATE",
+        Sequence(
+            OneOf("TIME", "TIMESTAMP"),
+            Bracketed(Ref("NumericLiteralSegment"), optional=True),
+            Sequence(OneOf("WITH", "WITHOUT"), "TIME", "ZONE", optional=True),
+        ),
+        # Structural
+        "ARRAY",
+        "MAP",
+        "ROW",
+        # Others
+        "IPADDRESS",
+        "UUID",
+    )
+
+
+class SelectClauseSegment(ansi.SelectClauseSegment):
+    """A group of elements in a select target statement."""
+
+    match_grammar: Matchable = StartsWith(
+        "SELECT",
+        terminator=OneOf(
+            "FROM",
+            "WHERE",
+            Sequence("ORDER", "BY"),
+            "LIMIT",
+            Ref("SetOperatorSegment"),
+            "FETCH",
+        ),
+        enforce_whitespace_preceding_terminator=True,
+    )
+    parse_grammar = ansi.SelectClauseSegment.parse_grammar
+
+
+class UnorderedSelectStatementSegment(ansi.UnorderedSelectStatementSegment):
+    """A `SELECT` statement without any ORDER clauses or later."""
+
+    match_grammar = ansi.UnorderedSelectStatementSegment.match_grammar
+    parse_grammar = ansi.UnorderedSelectStatementSegment.parse_grammar.copy(
+        remove=[
+            Ref("OverlapsClauseSegment", optional=True),
+        ]
+    )
 
 
 class ValuesClauseSegment(ansi.ValuesClauseSegment):
@@ -82,3 +217,31 @@ class FrameClauseSegment(BaseSegment):
         Ref("FrameClauseUnitGrammar"),
         OneOf(_frame_extent, Sequence("BETWEEN", _frame_extent, "AND", _frame_extent)),
     )
+
+
+class SetOperatorSegment(BaseSegment):
+    """A set operator such as Union, Intersect or Except."""
+
+    type = "set_operator"
+    match_grammar: Matchable = OneOf(
+        Sequence("UNION", OneOf("DISTINCT", "ALL", optional=True)),
+        Sequence(
+            OneOf(
+                "INTERSECT",
+                "EXCEPT",
+            ),
+            Ref.keyword("ALL", optional=True),
+        ),
+        exclude=Sequence("EXCEPT", Bracketed(Anything())),
+    )
+
+
+class StatementSegment(ansi.StatementSegment):
+    """Overriding StatementSegment to allow for additional segment parsing."""
+
+    parse_grammar = ansi.StatementSegment.parse_grammar.copy(
+        remove=[
+            Ref("TransactionStatementSegment"),
+        ],
+    )
+    match_grammar = ansi.StatementSegment.match_grammar
