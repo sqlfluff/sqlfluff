a28a63b60dd6122f2656a396e7d1fdfc513fd845
bfaust2 <benjamin.faust@epsilon.com>
2023-07-08 12:59:52 -0400

    spacing edits

--- test/fixtures/dialects/greenplum/create_table_as.sql
+++ test/fixtures/dialects/greenplum/create_table_as.sql
@@ -1,8 +1,9 @@
 create temp table tester
 with (appendoptimized = true, compresstype = zstd) as
-select column_1
-  , column_2
-  , column_3 as last_column
+select
+    column_1
+    , column_2
+    , column_3 as last_column
 from schema.another_table
 where column_1 = 'test'
 distributed by (column_2, last_column);
