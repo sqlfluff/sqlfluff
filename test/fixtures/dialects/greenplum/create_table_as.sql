4a784b888f7bd45236fc786b87ba659e875e093e
bpfaust <90462500+bpfaust@users.noreply.github.com>
2023-07-08 07:47:31 -0400

    Added create_table_as tests

---
+++ test/fixtures/dialects/greenplum/create_table_as.sql
@@ -0,0 +1,13 @@
+create temp table tester
+with (appendoptimized = true, compresstype = zstd) as
+select column_1
+  , column_2
+  , column_3 as last_column
+from schema.another_table
+where column_1 = 'test'
+distributed by (column_2, last_column);
+
+create table schema.a_new_table as
+select *
+from schema.old_table
+distributed randomly;
