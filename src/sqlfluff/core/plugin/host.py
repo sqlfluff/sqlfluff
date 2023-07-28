2850d86ea2096c144901d271a49cb95915b8cbd1
Alan Cruickshank <alan@designingoverload.com>
2023-07-28 18:55:13 +0100

    Cache plugin manager (~0.6s)

--- src/sqlfluff/core/plugin/host.py
+++ src/sqlfluff/core/plugin/host.py
@@ -1,13 +1,25 @@
 """Defines the plugin manager getter."""
 import pluggy
+from typing import Optional
 
 from sqlfluff.core.plugin.hookspecs import PluginSpec
 from sqlfluff.core.plugin import plugin_base_name, project_name
 
+_plugin_manager: Optional[pluggy.PluginManager] = None
+
 
 def get_plugin_manager() -> pluggy.PluginManager:
-    """Initializes the PluginManager."""
+    """Initializes the PluginManager.
+
+    NOTE: We cache the plugin manager as a global to
+    avoid reloading all the plugins each time.
+    """
+    global _plugin_manager
+    if _plugin_manager:
+        return _plugin_manager
+
     pm = pluggy.PluginManager(plugin_base_name)
     pm.add_hookspecs(PluginSpec)
     pm.load_setuptools_entrypoints(project_name)
+    _plugin_manager = pm
     return pm
