17fb9055261e80ace2bf732a7a1480d574e89ef3
Alan Cruickshank <alan@designingoverload.com>
2023-07-28 19:25:34 +0100

    Save another 0.3s with better caching

--- src/sqlfluff/core/plugin/host.py
+++ src/sqlfluff/core/plugin/host.py
@@ -1,11 +1,12 @@
 """Defines the plugin manager getter."""
+
+from contextvars import ContextVar
 import pluggy
-from typing import Optional
 
 from sqlfluff.core.plugin.hookspecs import PluginSpec
 from sqlfluff.core.plugin import plugin_base_name, project_name
 
-_plugin_manager: Optional[pluggy.PluginManager] = None
+_plugin_manager = ContextVar("_plugin_manager", default=None)
 
 
 def get_plugin_manager() -> pluggy.PluginManager:
@@ -14,12 +15,17 @@ def get_plugin_manager() -> pluggy.PluginManager:
     NOTE: We cache the plugin manager as a global to
     avoid reloading all the plugins each time.
     """
-    global _plugin_manager
-    if _plugin_manager:
-        return _plugin_manager
+    plugin_manager = _plugin_manager.get()
+    if plugin_manager:
+        return plugin_manager
+    plugin_manager = pluggy.PluginManager(plugin_base_name)
+    plugin_manager.add_hookspecs(PluginSpec)
 
-    pm = pluggy.PluginManager(plugin_base_name)
-    pm.add_hookspecs(PluginSpec)
-    pm.load_setuptools_entrypoints(project_name)
-    _plugin_manager = pm
-    return pm
+    # NOTE: We set the plugin manager before loading the
+    # entrypoints. This is because when we load the entry
+    # points, this function gets called again - and we only
+    # want to load the entry points once!
+    _plugin_manager.set(plugin_manager)
+    print(f"get_plugin_manager LOADING ENTRY POINTS")
+    plugin_manager.load_setuptools_entrypoints(project_name)
+    return plugin_manager
