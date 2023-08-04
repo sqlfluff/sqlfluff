094b05948ac38d6d5186869fc8beae0e8a18d74e
Paul Wildenhain <35195136+pwildenhain@users.noreply.github.com>
2023-08-04 05:57:25 -0400

    :white_check_mark: Add strict typing for errors module (#5047)

--- src/sqlfluff/core/errors.py
+++ src/sqlfluff/core/errors.py
@@ -10,7 +10,7 @@ tracking.
 
 https://stackoverflow.com/questions/49715881/how-to-pickle-inherited-exceptions
 """
-from typing import Optional, Tuple, Any, List, Dict, Union, TYPE_CHECKING
+from typing import Optional, Tuple, Any, List, Dict, Type, Union, TYPE_CHECKING
 
 if TYPE_CHECKING:  # pragma: no cover
     from sqlfluff.core.parser import PositionMarker, BaseSegment
@@ -47,11 +47,13 @@ class SQLBaseError(ValueError):
             self.line_pos = line_pos
         super().__init__(self.desc())
 
-    def __reduce__(self):  # pragma: no cover
+    def __reduce__(
+        self,
+    ) -> Tuple[Type["SQLBaseError"], Tuple[Any, ...]]:  # pragma: no cover
         """Prepare the SQLBaseError for pickling."""
         return type(self), (
             self.description,
-            self.pos,
+            None,
             self.line_no,
             self.line_pos,
             self.ignore,
@@ -177,7 +179,9 @@ class SQLParseError(SQLBaseError):
             line_pos=line_pos,
         )
 
-    def __reduce__(self):  # pragma: no cover
+    def __reduce__(
+        self,
+    ) -> Tuple[Type["SQLParseError"], Tuple[Any, ...]]:  # pragma: no cover
         """Prepare the SQLParseError for pickling."""
         return type(self), (self.description, self.segment, self.line_no, self.line_pos)
 
@@ -213,7 +217,9 @@ class SQLLintError(SQLBaseError):
             description=description, pos=segment.pos_marker if segment else None
         )
 
-    def __reduce__(self):  # pragma: no cover
+    def __reduce__(
+        self,
+    ) -> Tuple[Type["SQLLintError"], Tuple[Any, ...]]:  # pragma: no cover
         """Prepare the SQLLintError for pickling."""
         return type(self), (self.description, self.segment, self.rule, self.fixes)
 
