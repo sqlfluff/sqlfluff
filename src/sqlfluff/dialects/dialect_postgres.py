ff340b84b0f01001461239138aa43048f1dd1f5f
Greg Finley <gregory.finley@gmail.com>
2023-08-18 14:15:43 -0700

    Postgres: Allow CREATE TABLE INHERITS with no new columns (#5100)

--- src/sqlfluff/dialects/dialect_postgres.py
+++ src/sqlfluff/dialects/dialect_postgres.py
@@ -1756,8 +1756,8 @@ class CreateTableStatementSegment(ansi.CreateTableStatementSegment):
                                 Ref("ColumnReferenceSegment"),
                                 Ref("DatatypeSegment"),
                                 AnyNumberOf(
-                                    # A single COLLATE segment can come before or after
-                                    # constraint segments
+                                    # A single COLLATE segment can come before or
+                                    # after constraint segments
                                     OneOf(
                                         Ref("ColumnConstraintSegment"),
                                         Sequence(
@@ -1774,6 +1774,7 @@ class CreateTableStatementSegment(ansi.CreateTableStatementSegment):
                                 AnyNumberOf(Ref("LikeOptionSegment"), optional=True),
                             ),
                         ),
+                        optional=True,
                     )
                 ),
                 Sequence(
