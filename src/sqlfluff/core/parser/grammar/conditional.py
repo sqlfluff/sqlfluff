cdcd5e786566236c280ec77dfde4cff3b2b2fe83
Alan Cruickshank <alan@designingoverload.com>
2023-08-18 15:09:56 +0100

    typing fallout in conditional

--- src/sqlfluff/core/parser/grammar/conditional.py
+++ src/sqlfluff/core/parser/grammar/conditional.py
@@ -1,12 +1,11 @@
 """Conditional Grammar."""
 
-from sqlfluff.core.parser.segments import Indent
+from typing import Type
+
+from sqlfluff.core.parser.grammar.base import BaseGrammar
 from sqlfluff.core.parser.match_result import MatchResult
 from sqlfluff.core.parser.match_wrapper import match_wrapper
-
-from sqlfluff.core.parser.grammar.base import (
-    BaseGrammar,
-)
+from sqlfluff.core.parser.segments import Indent
 
 
 class Conditional(BaseGrammar):
@@ -41,15 +40,11 @@ class Conditional(BaseGrammar):
     | "indentation" keys are currently set up.
     """
 
-    def __init__(self, *args, config_type: str = "indentation", **rules):
-        if not all(issubclass(arg, Indent) for arg in args):  # pragma: no cover
-            raise ValueError(
-                "Conditional is only designed to work with Indent segments."
-            )
-        if len(args) != 1:  # pragma: no cover
-            raise ValueError(
-                "Conditional is only designed to work with a single element."
-            )
+    def __init__(self, meta: Type[Indent], config_type: str = "indentation", **rules):
+        assert issubclass(
+            meta, Indent
+        ), "Conditional is only designed to work with Indent/Dedent segments."
+        self._Meta = meta
         if not config_type:  # pragma: no cover
             raise ValueError("Conditional config_type must be set.")
         elif config_type not in ("indentation"):  # pragma: no cover
@@ -60,7 +55,7 @@ class Conditional(BaseGrammar):
             raise ValueError("Conditional requires rules to be set.")
         self._config_type = config_type
         self._config_rules = rules
-        super().__init__(*args)
+        super().__init__()
 
     def is_enabled(self, parse_context) -> bool:
         """Evaluate conditionals and return whether enabled."""
@@ -88,5 +83,4 @@ class Conditional(BaseGrammar):
             return MatchResult.from_unmatched(segments)
 
         # Instantiate the new element and return
-        new_seg = self._elements[0]()
-        return MatchResult((new_seg,), segments)
+        return MatchResult((self._Meta(),), segments)
