5ed58c2ecc94ba9e4a32cae4d6520dcdc49585aa
Alan Cruickshank <alan@designingoverload.com>
2023-08-21 18:18:30 +0100

    slowly slowly

--- src/sqlfluff/rules/ambiguous/AM07.py
+++ src/sqlfluff/rules/ambiguous/AM07.py
@@ -60,13 +60,14 @@ class Rule_AM07(BaseRule):
     groups: Tuple[str, ...] = ("all", "ambiguous")
     crawl_behaviour = SegmentSeekerCrawler({"set_expression"}, provide_raw_stack=True)
 
-    def __resolve_wildcard(
+    def __resolve_wild_query(
         self,
         query: Query,
     ) -> List[Any]:
         """Attempt to resolve the wildcard to a list of selectables."""
         targets = []
         process_queries = query.selectables
+        resolved_wildcard = True
         # if one of the source queries for a query within the set is a
         # set expression, just use the first query. If that first query isn't
         # reflective of the others, that will be caught when that segment
@@ -78,39 +79,9 @@ class Rule_AM07(BaseRule):
         for selectable in process_queries:
             if selectable.get_wildcard_info():
                 for wildcard in selectable.get_wildcard_info():
-                    if wildcard.tables:
-                        for wildcard_table in wildcard.tables:
-                            cte_name = wildcard_table
-                            # Get the AliasInfo for the table referenced in the wildcard
-                            # expression.
-                            alias_info = selectable.find_alias(wildcard_table)
-                            # attempt to resolve alias or table name to a cte
-                            if alias_info:
-                                select_info_target = SelectCrawler.get(
-                                    query, alias_info.from_expression_element
-                                )[0]
-                                if isinstance(select_info_target, str):
-                                    cte_name = select_info_target
-                                else:
-                                    targets += self.__resolve_wildcard(
-                                        select_info_target
-                                    )
-                                    continue
-
-                            cte = query.lookup_cte(cte_name)
-                            if cte:
-                                targets += self.__resolve_wildcard(cte)
-                            else:
-                                targets.append(wildcard)
-                    # if there is no table specified, it is likely a subquery
-                    else:
-                        query_list = SelectCrawler.get(
-                            query, query.selectables[0].selectable
-                        )
-                        for o in query_list:
-                            if isinstance(o, Query):
-                                targets += self.__resolve_wildcard(o)
-                                return targets
+                    targets += self.__resolve_selectable_wildcard(
+                        wildcard, selectable, query
+                    )
             else:
                 assert selectable.select_info
                 targets.extend(
@@ -118,6 +89,46 @@ class Rule_AM07(BaseRule):
                 )
 
         return targets
+
+    def __resolve_selectable_wildcard(self, wildcard, selectable, root_query):
+        # If there is no table specified, it is likely a subquery.
+        # Handle that first.
+        if not wildcard.tables:
+            query_list = SelectCrawler.get(
+                root_query, root_query.selectables[0].selectable
+            )
+            for o in query_list:
+                if isinstance(o, Query):
+                    return self.__resolve_wild_query(o)
+            raise NotImplementedError("I DON'T KNOW HOW TO GET HERE")
+
+        # There might be multiple tables referenced in some wildcard cases.
+        # resolved_wildcard = True
+        select_list = []
+        for wildcard_table in wildcard.tables:
+            cte_name = wildcard_table
+            # Get the AliasInfo for the table referenced in the wildcard
+            # expression.
+            alias_info = selectable.find_alias(wildcard_table)
+            # attempt to resolve alias or table name to a cte
+            if alias_info:
+                select_info_target = SelectCrawler.get(
+                    root_query, alias_info.from_expression_element
+                )[0]
+                if isinstance(select_info_target, str):
+                    cte_name = select_info_target
+                else:
+                    select_list += self.__resolve_wild_query(select_info_target)
+                    continue
+
+            cte = root_query.lookup_cte(cte_name)
+            if cte:
+                select_list += self.__resolve_wild_query(cte)
+            else:
+                # Unable to resolve
+                # resolved_wildcard = False
+                select_list.append(wildcard)
+        return select_list
 
     def __resolve_selectable(
         self, selectable: Selectable, root_query: Query
@@ -128,20 +139,22 @@ class Rule_AM07(BaseRule):
         # If there's no wildcard, just count the columns and move on.
         if not wildcard_info:
             # if there is no wildcard in the query use the count of select targets
-            select_list = selectable.select_info.select_targets
             return len(selectable.select_info.select_targets), True
 
-        # If the set query contains a wildcard, attempt to resolve it to a list of
-        # select targets that can be counted.
-        select_list = self.__resolve_wildcard(
-            root_query,
-        )
+        select_list = []
+        resolved_wildcard = True
+
+        # If the set query contains on or more wildcards, attempt to resolve it to a
+        # list of select targets that can be counted.
+        for wildcard in wildcard_info:
+            select_list += self.__resolve_selectable_wildcard(
+                wildcard, selectable, root_query
+            )
 
         # get the number of resolved targets plus the total number of
         # targets minus the number of wildcards
         # if all wildcards have been resolved this adds up to the
         # total number of select targets in the query
-        resolved_wildcard = True
         for select in select_list:
             if isinstance(select, WildcardInfo):
                 resolved_wildcard = False
