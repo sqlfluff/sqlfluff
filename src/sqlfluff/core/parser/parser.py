08b6a2bab492f65dfab78c267b8c056a5eba4bd6
Alan Cruickshank <alan@designingoverload.com>
2023-09-12 09:45:28 +0100

    mypy

--- src/sqlfluff/core/parser/parser.py
+++ src/sqlfluff/core/parser/parser.py
@@ -45,7 +45,7 @@ class Parser:
         parsed = root_segment.parse(parse_context=ctx)
 
         # Basic Validation, that we haven't dropped anything.
-        check_still_complete(segments, parsed, ())
+        check_still_complete(tuple(segments), parsed, ())
 
         if parse_statistics:  # pragma: no cover
             # NOTE: We use ctx.logger.warning here to output the statistics.
