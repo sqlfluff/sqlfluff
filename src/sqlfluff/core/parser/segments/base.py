a862ec80bdc437a8f891650971808fe27c625f1d
Alan Cruickshank <alan@designingoverload.com>
2023-09-14 17:41:42 +0100

    merge conflict

--- src/sqlfluff/core/parser/segments/base.py
+++ src/sqlfluff/core/parser/segments/base.py
@@ -1499,7 +1499,7 @@ class BaseSegment(metaclass=SegmentMetaclass):
 
         # Rather than fix that here, we simply assert that it has been
         # done. This will raise issues in testing, but shouldn't in use.
-        if not r.can_start_end_non_code and seg_buffer:
+        if not self.can_start_end_non_code and seg_buffer:
             assert self._find_start_or_end_non_code(seg_buffer) is None, (
                 "Found inappropriate fix application: inappropriate "
                 "whitespace positioning. Post `_choose_anchor_segment`. \n\n"
