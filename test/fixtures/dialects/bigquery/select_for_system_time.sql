81698cb5fe07a6a73ef8f10a980f0e44038d1bb4
Greg Finley <gregory.finley@gmail.com>
2023-09-21 13:34:54 -0700

    BigQuery system time syntax (#5220)

--- test/fixtures/dialects/bigquery/select_for_system_time.sql
+++ test/fixtures/dialects/bigquery/select_for_system_time.sql
@@ -2,4 +2,16 @@ SELECT
     user_id
 FROM
     lists_emails AS list_emails
-    FOR SYSTEM_TIME AS OF CAST('2019-12-02T20:52:34+00:00' AS TIMESTAMP)
+    FOR SYSTEM_TIME AS OF CAST('2019-12-02T20:52:34+00:00' AS TIMESTAMP);
+
+SELECT
+    user_id
+FROM
+    `project.dataset.table1`
+    FOR SYSTEM_TIME AS OF CAST('2020-05-11T14:02:52+00:00' AS TIMESTAMP);
+
+SELECT
+    user_id
+FROM
+    `project.dataset.table1`
+    FOR SYSTEM TIME AS OF CAST('2020-05-11T14:02:52+00:00' AS TIMESTAMP)
