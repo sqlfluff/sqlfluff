5ea25f5681fafa04ac45117ee8db9af1fa3941bb
Kyle Andrews <90274941+Kylea650@users.noreply.github.com>
2023-09-26 20:01:24 +0100

    Allow Snowflake pipe integration to be a quoted or unquoted (#5241)

--- test/fixtures/dialects/snowflake/create_pipe.sql
+++ test/fixtures/dialects/snowflake/create_pipe.sql
@@ -6,3 +6,29 @@ create or replace pipe mypipe_s3
   copy into snowpipe_db.public.mytable
   from @snowpipe_db.public.mystage
   file_format = (type = 'JSON');
+
+create or replace pipe test_pipe
+  auto_ingest = true
+  integration = notification_integration
+  as
+  copy into table_name (
+    column1,
+    column2
+  )
+  from (select
+    $1,
+    current_timestamp() as column2
+  from @stage_name/folder);
+
+create or replace pipe test_pipe
+  auto_ingest = true
+  integration = 'notification_integration'
+  as
+  copy into table_name (
+    column1,
+    column2
+  )
+  from (select
+    $1,
+    current_timestamp() as column2
+  from @stage_name/folder);
