72020dd04afed283021fa8d1f7919ec890fe38bf
Romain Petit <63579439+r-petit@users.noreply.github.com>
2023-10-04 00:13:29 +0200

    Add delimited, and any number of to allow calling in any order

--- src/sqlfluff/dialects/dialect_tsql.py
+++ src/sqlfluff/dialects/dialect_tsql.py
@@ -1380,28 +1380,39 @@ class TableOptionSegment(BaseSegment):
                         "SYSTEM_VERSIONING",
                         Ref("EqualsSegment"),
                         "ON",
-                        Sequence(
-                            Bracketed(
-                                "HISTORY_TABLE",
-                                Ref("EqualsSegment"),
-                                Ref("TableReferenceSegment"),
-                                Sequence(
-                                    Ref("CommaSegment"),
-                                    "DATA_CONSISTENCY_CHECK",
-                                    Ref("EqualsSegment"),
-                                    OneOf("ON", "OFF"),
-                                    optional=True,
-                                ),
-                                Sequence(
-                                    Ref("CommaSegment"),
-                                    "HISTORY_RETENTION_PERIOD",
-                                    Ref("EqualsSegment"),
-                                    Ref("NumericLiteralSegment", optional=True),
-                                    Ref("DatetimeUnitSegment"),
-                                    optional=True,
+                        Bracketed(
+                            Delimited(
+                                AnyNumberOf(
+                                    Sequence(
+                                        "HISTORY_TABLE",
+                                        Ref("EqualsSegment"),
+                                        Ref("TableReferenceSegment"),
+                                    ),
+                                    Sequence(
+                                        "HISTORY_RETENTION_PERIOD",
+                                        Ref("EqualsSegment"),
+                                        OneOf(
+                                            "INFINITE",
+                                            Sequence(
+                                                Ref("NumericLiteralSegment", optional=True),
+                                                OneOf(
+                                                    "DAYS",
+                                                    "WEEKS",
+                                                    "MONTHS",
+                                                    "YEARS",
+                                                ),
+                                                optional=True,
+                                            ),
+                                        )
+                                    ),
+                                    Sequence(
+                                        Ref("CommaSegment"),
+                                        "DATA_CONSISTENCY_CHECK",
+                                        Ref("EqualsSegment"),
+                                        OneOf("ON", "OFF"),
+                                    ),
                                 ),
                             ),
-                            optional=True,
                         ),
                     ),
                     Sequence(
