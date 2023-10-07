7a21a7ad323969607d73f04c79d5d626e4d9e9ef
David Wales <daviewales@disroot.org>
2023-10-07 23:43:02 +1100

    Strip block comment markers from start for noqa
    This is required to ensure noqa directives in the following form
    (without -- prefix) are followed:
    
    /* noqa: disable=all */

--- src/sqlfluff/core/linter/noqa.py
+++ src/sqlfluff/core/linter/noqa.py
@@ -148,11 +148,14 @@ class IgnoreMask:
         """Extract ignore mask entries from a comment segment."""
         # Also trim any whitespace
         comment_content = comment.raw_trimmed().strip()
-        # If we have a trailing block comment marker, also strip that.
-        # NOTE: We don't need to strip them from the start because `_parse_noqa`
-        # will handle that for us by looking for `--` markers.
+        # If we have leading or trailing block comment markers, also strip them.
+        # NOTE: We need to strip block comment markers from the start
+        # to ensure that noqa directives in the following form are followed:
+        # /* noqa: disable=all */
         if comment_content.endswith("*/"):
             comment_content = comment_content[:-2].rstrip()
+        if comment_content.startswith("/*"):
+            comment_content = comment_content[2:].lstrip()
         comment_line, comment_pos = comment.pos_marker.source_position()
         result = cls._parse_noqa(
             comment_content, comment_line, comment_pos, reference_map
