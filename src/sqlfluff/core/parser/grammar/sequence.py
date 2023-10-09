7a8978b3f74a9a7282d0c99a321d2eba63d5c2cf
Alan Cruickshank <alan@designingoverload.com>
2023-10-09 17:57:26 +0100

    Resolve #5290

--- src/sqlfluff/core/parser/grammar/sequence.py
+++ src/sqlfluff/core/parser/grammar/sequence.py
@@ -3,7 +3,7 @@
 # NOTE: We rename the typing.Sequence here so it doesn't collide
 # with the grammar class that we're defining.
 from os import getenv
-from typing import Optional, Set, Tuple, Type, Union
+from typing import Optional, Set, Tuple, Type, Union, cast
 from typing import Sequence as SequenceType
 
 from sqlfluff.core.parser.context import ParseContext
@@ -24,6 +24,7 @@ from sqlfluff.core.parser.segments import (
     BaseSegment,
     Indent,
     MetaSegment,
+    TemplateSegment,
     UnparsableSegment,
 )
 from sqlfluff.core.parser.types import ParseMode, SimpleHintType
@@ -31,13 +32,49 @@ from sqlfluff.core.slice_helpers import is_zero_slice
 
 
 def _flush_metas(
-    pre_nc_idx: int, post_nc_idx: int, meta_buffer: SequenceType[Type["MetaSegment"]]
+    pre_nc_idx: int,
+    post_nc_idx: int,
+    meta_buffer: SequenceType[Type["MetaSegment"]],
+    segments: SequenceType[BaseSegment],
 ) -> Tuple[Tuple[int, Type[MetaSegment]], ...]:
-    # Flush any metas...
+    """Position any new meta segments relative to the non code section.
+
+    It's important that we position the new meta segments appropriately
+    around any templated sections and any whitespace so that indentation
+    behaviour works as expected.
+
+    There are four valid locations (which may overlap).
+    1. Before any non-code
+    2. Before the first block templated section (if it's a block opener).
+    3. After the last block templated section (if it's a block closer).
+    4. After any non code.
+
+    If all the metas have a positive indent value then they should go in
+    position 1 or 3, otherwise we're in position 2 or 4. Within each of
+    those scenarios it depends on whether an appropriate block end exists.
+    """
     if all(m.indent_val >= 0 for m in meta_buffer):
-        meta_idx = pre_nc_idx
+        for _idx in range(post_nc_idx, pre_nc_idx, -1):
+            if segments[_idx - 1].is_type("placeholder"):
+                _seg = cast(TemplateSegment, segments[_idx - 1])
+                if _seg.block_type == "block_end":
+                    meta_idx = _idx
+                else:
+                    meta_idx = pre_nc_idx
+                break
+        else:
+            meta_idx = pre_nc_idx
     else:
-        meta_idx = post_nc_idx
+        for _idx in range(pre_nc_idx, post_nc_idx):
+            if segments[_idx].is_type("placeholder"):
+                _seg = cast(TemplateSegment, segments[_idx])
+                if _seg.block_type == "block_start":
+                    meta_idx = _idx
+                else:
+                    meta_idx = post_nc_idx
+                break
+        else:
+            meta_idx = post_nc_idx
     return tuple((meta_idx, meta) for meta in meta_buffer)
 
 
@@ -270,7 +307,7 @@ class Sequence(BaseGrammar):
                 )
 
             # Flush any metas...
-            insert_segments += _flush_metas(matched_idx, _idx, meta_buffer)
+            insert_segments += _flush_metas(matched_idx, _idx, meta_buffer, segments)
             meta_buffer = []
 
             # Otherwise we _do_ have a match. Update the position.
