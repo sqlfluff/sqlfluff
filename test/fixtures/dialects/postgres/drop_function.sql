97426de3ffc656aabe110e70b734d5869a1e281b
João Filipe Cavichiolo Storrer <jofi_casto@hotmail.com>
2023-10-10 07:57:02 -0300

    Change drop function to allow DropBehaviourGrammar with space after function name (#5295)

--- test/fixtures/dialects/postgres/drop_function.sql
+++ test/fixtures/dialects/postgres/drop_function.sql
@@ -1,5 +1,6 @@
-DROP FUNCTION sqrt(integer);
-DROP FUNCTION sqrt(integer), sqrt(bigint);
+DROP FUNCTION sqrt (integer);
+DROP FUNCTION sqrt (integer), sqrt (bigint);
 DROP FUNCTION update_employee_salaries;
-DROP FUNCTION update_employee_salaries();
+DROP FUNCTION update_employee_salaries ();
 DROP FUNCTION IF EXISTS foo (IN my_var integer, VARIADIC my_var_2 text);
+DROP FUNCTION IF EXISTS f_name CASCADE;
