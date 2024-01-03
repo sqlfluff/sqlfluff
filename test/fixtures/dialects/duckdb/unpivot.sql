ce8e5ec2e1ebb409a0d6e3a3f4f6dca953013c88
Cameron Loos <105471409+keraion@users.noreply.github.com>
2024-01-02 22:13:49 -0500

    DuckDB: Added support for `PIVOT` and `UNPIVOT`

---
+++ test/fixtures/dialects/duckdb/unpivot.sql
@@ -0,0 +1,48 @@
+-- Simplified UNPIVOT
+UNPIVOT monthly_sales
+ON jan, feb, mar, apr, may, jun
+INTO
+NAME month
+VALUE sales;
+
+UNPIVOT monthly_sales
+ON COLUMNS (* EXCLUDE (empid, dept))
+INTO
+NAME month
+VALUE sales;
+
+UNPIVOT monthly_sales
+ON (jan, feb, mar) AS q1, (apr, may, jun) AS q2
+INTO
+NAME quarter
+VALUE month_1_sales, month_2_sales, month_3_sales;
+
+WITH unpivot_alias AS (
+    UNPIVOT monthly_sales
+    ON COLUMNS (* EXCLUDE (empid, dept))
+    INTO
+    NAME month
+    VALUE sales
+)
+
+SELECT * FROM unpivot_alias;
+
+-- Standard UNPIVOT
+FROM monthly_sales UNPIVOT (
+    sales
+    FOR month IN (jan, feb, mar, apr, may, jun)
+);
+
+FROM monthly_sales UNPIVOT (
+    sales
+    FOR month IN (COLUMNS (* EXCLUDE (empid, dept)))
+);
+
+FROM monthly_sales
+UNPIVOT (
+    (month_1_sales, month_2_sales, month_3_sales)
+    FOR quarter IN (
+        (jan, feb, mar) AS q1,
+        (apr, may, jun) AS q2
+    )
+);
