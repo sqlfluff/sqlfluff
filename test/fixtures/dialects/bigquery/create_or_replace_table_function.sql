a285505022873215a8d5ba59462765086b52646b
moh-lch <142326232+moh-lch@users.noreply.github.com>
2024-01-09 10:42:35 -0500

    bq_table_function : Added file by mistake

--- test/fixtures/dialects/bigquery/create_or_replace_table_function.sql
+++
@@ -1,8 +0,0 @@
--- CREATE TABLE FUNCTION WITH BASIC SYNTAX
-CREATE OR REPLACE TABLE FUNCTION ds.lchtablefunction (x int, y varchar) AS (
-    SELECT
-        id AS call_id,
-        creation_date AS created_at
-    FROM
-        ds.data
-)
