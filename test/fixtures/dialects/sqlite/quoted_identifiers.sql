410cb67b17af9d9d020c343fa5862805be6b281a
Alan Cruickshank <alanmcruickshank@gmail.com>
2024-04-12 19:38:30 +0100

    Update test/fixtures/dialects/sqlite/quoted_identifiers.sql

--- test/fixtures/dialects/sqlite/quoted_identifiers.sql
+++ test/fixtures/dialects/sqlite/quoted_identifiers.sql
@@ -1,6 +1,7 @@
 SELECT `nih`.`userID`
 FROM `flight_notification_item_history` AS `nih`;
 
+-- NOTE: Normally single quoted items are interpreted as strings rather than objects - but this does still run on SQLite.
 SELECT 'nih'.'userID'
 FROM 'flight_notification_item_history' AS 'nih';
 
