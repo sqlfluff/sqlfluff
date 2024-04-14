392f66abc4b5057d6a4d3c96c3d9475c4938a836
Alan Cruickshank <alan@designingoverload.com>
2024-04-14 19:20:53 +0100

    mypy

--- src/sqlfluff/core/parser/grammar/base.py
+++ src/sqlfluff/core/parser/grammar/base.py
@@ -219,7 +219,7 @@ class BaseGrammar(Matchable):
         self: T,
         insert: Optional[List[Matchable]] = None,
         at: Optional[int] = None,
-        before: Optional[Union[str, Matchable]] = None,
+        before: Optional[Matchable] = None,
         remove: Optional[List[Matchable]] = None,
         terminators: List[Union[str, Matchable]] = [],
         replace_terminators: bool = False,
