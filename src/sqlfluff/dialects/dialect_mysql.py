c9e7865d774cfa5ff9f7815b4f861b91e4d880e6
Xemptuous <99044751+Xemptuous@users.noreply.github.com>
2024-04-18 17:12:06 -0700

    feature: added set transaction to mysql dialect

--- src/sqlfluff/dialects/dialect_mysql.py
+++ src/sqlfluff/dialects/dialect_mysql.py
@@ -1116,6 +1116,7 @@ class StatementSegment(ansi.StatementSegment):
             Ref("DelimiterStatement"),
             Ref("CreateProcedureStatementSegment"),
             Ref("DeclareStatement"),
+            Ref("SetTransactionStatementSegment"),
             Ref("SetAssignmentStatementSegment"),
             Ref("IfExpressionStatement"),
             Ref("WhileStatementSegment"),
@@ -2718,6 +2719,37 @@ class ReturnStatementSegment(BaseSegment):
     match_grammar = Sequence(
         "RETURN",
         Ref("ExpressionSegment"),
+    )
+
+
+class SetTransactionStatementSegment(BaseSegment):
+    """A `SET TRANSACTION` statement.
+
+    As specified in https://dev.mysql.com/doc/refman/8.0/en/set-transaction.html
+    """
+
+    type = "set_transaction_statement"
+    match_grammar: Matchable = Sequence(
+        "SET",
+        OneOf("GLOBAL", "SESSION", optional=True),
+        "TRANSACTION",
+        Delimited(
+            AnySetOf(
+                Sequence(
+                    "ISOLATION",
+                    "LEVEL",
+                    OneOf(
+                        Sequence(
+                            "READ",
+                            OneOf("COMMITTED", "UNCOMMITTED"),
+                        ),
+                        Sequence("REPEATABLE", "READ"),
+                        "SERIALIZABLE",
+                    ),
+                ),
+                Sequence("READ", OneOf("WRITE", "ONLY")),
+            ),
+        ),
     )
 
 
