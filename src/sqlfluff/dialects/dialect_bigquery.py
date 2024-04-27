cdb834b50ab7fa24d1a69a831c7e4862d9558aaf
Kazuhito Osabe <kzosabe@wip.place>
2024-04-27 12:15:16 +0900

    BigQuery: Support CREATE ROW ACCESS POLICY statement

--- src/sqlfluff/dialects/dialect_bigquery.py
+++ src/sqlfluff/dialects/dialect_bigquery.py
@@ -512,6 +512,7 @@ class StatementSegment(ansi.StatementSegment):
             Ref("CreateMaterializedViewAsReplicaOfStatementSegment"),
             Ref("AlterMaterializedViewStatementSegment"),
             Ref("DropMaterializedViewStatementSegment"),
+            Ref("CreateRowAccessPolicyStatementSegment"),
         ],
     )
 
@@ -1559,6 +1560,24 @@ class DefaultCollateSegment(BaseSegment):
     )
 
 
+class GrantToSegment(BaseSegment):
+    """GRANT TO (grantee_list).
+
+    https://cloud.google.com/bigquery/docs/reference/standard-sql/data-definition-language#create_row_access_policy_statement
+    """
+
+    type = "grant_to_segment"
+    match_grammar: Matchable = Sequence(
+        "GRANT",
+        "TO",
+        Bracketed(
+            Delimited(
+                Ref("QuotedLiteralSegment"),
+            ),
+        ),
+    )
+
+
 class OptionsSegment(BaseSegment):
     """OPTIONS clause for a table."""
 
@@ -2433,5 +2452,32 @@ class RaiseStatementSegment(BaseSegment):
             Ref("EqualsSegment"),
             Ref("ExpressionSegment"),
             optional=True,
+        ),
+    )
+
+
+class CreateRowAccessPolicyStatementSegment(BaseSegment):
+    """A `CREATE ROW ACCESS POLICY` statement.
+
+    https://cloud.google.com/bigquery/docs/reference/standard-sql/data-definition-language#create_row_access_policy_statement
+    """
+
+    type = "create_row_access_policy_statement"
+
+    match_grammar: Matchable = Sequence(
+        "CREATE",
+        Ref("OrReplaceGrammar", optional=True),
+        "ROW",
+        "ACCESS",
+        "POLICY",
+        Ref("IfNotExistsGrammar", optional=True),
+        Ref("NakedIdentifierSegment"),  # row_access_policy_name
+        "ON",
+        Ref("TableReferenceSegment"),
+        Ref("GrantToSegment", optional=True),
+        "FILTER",
+        "USING",
+        Bracketed(
+            Ref("ExpressionSegment"),
         ),
     )
