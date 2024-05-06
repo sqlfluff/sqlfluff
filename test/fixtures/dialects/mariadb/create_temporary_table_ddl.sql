59265ad432977093ad02c21fb19eda05bb60d424
Xemptuous <99044751+Xemptuous@users.noreply.github.com>
2024-05-06 12:55:55 -0700

    MariaDB: updated temp table ddl test

--- test/fixtures/dialects/mariadb/create_temporary_table_ddl.sql
+++ test/fixtures/dialects/mariadb/create_temporary_table_ddl.sql
@@ -4,14 +4,14 @@ CREATE TEMPORARY TABLE tbl_name (
     INDEX(col)
 ) AS SELECT id, col FROM table_name;
 
--- CREATE TEMPORARY TABLE tbl_name (
---     id INT PRIMARY KEY AUTO_INCREMENT,
---     col VARCHAR(255) DEFAULT '' NOT NULL,
---     INDEX(col)
--- ) SELECT id, col FROM table_name;
+CREATE TEMPORARY TABLE tbl_name (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    col VARCHAR(255) DEFAULT '' NOT NULL,
+    INDEX(col)
+) SELECT id, col FROM table_name;
 
--- CREATE TEMPORARY TABLE tbl_name (INDEX(col)) AS
---     SELECT id, col FROM table_name;
---
--- CREATE TEMPORARY TABLE tbl_name (INDEX(col))
---     SELECT id, col FROM table_name;
+CREATE TEMPORARY TABLE tbl_name (INDEX(col)) AS
+    SELECT id, col FROM table_name;
+
+CREATE TEMPORARY TABLE tbl_name (INDEX(col))
+    SELECT id, col FROM table_name;
