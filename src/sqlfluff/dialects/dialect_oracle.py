a7bf976cab3494a32016a47b24cf0d498d089f5d
harshsoni2024 <harshsoni2024@gmail.com>
2024-05-16 12:27:29 +0530

    oracle test queries, remove drop view segment

--- src/sqlfluff/dialects/dialect_oracle.py
+++ src/sqlfluff/dialects/dialect_oracle.py
@@ -492,7 +492,6 @@ class StatementSegment(ansi.StatementSegment):
     match_grammar = ansi.StatementSegment.match_grammar.copy(
         insert=[
             Ref("CommentStatementSegment"),
-            Ref("DropMaterializedViewStatementSegment")
         ],
     )
 
@@ -977,19 +976,4 @@ class FunctionNameSegment(BaseSegment):
             delimiter=Ref("AtSignSegment"),
         ),
         allow_gaps=False,
-    )
-
-class DropMaterializedViewStatementSegment(ansi.DropViewStatementSegment):
-    """A oracle `DROP MATERIALIZED VIEW ...` statement.
-
-    https://docs.oracle.com/en/database/oracle/oracle-database/19/sqlrf/DROP-MATERIALIZED-VIEW.html
-    """
-
-    type = "drop_materialized_view_statement"
-    match_grammar = Sequence(
-        "DROP",
-        Sequence("MATERIALIZED", optional=True),
-        "VIEW",
-        Ref("IfExistsGrammar", optional=True),
-        Ref("TableReferenceSegment"),
     )
