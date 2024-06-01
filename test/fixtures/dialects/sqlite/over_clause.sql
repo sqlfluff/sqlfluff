e557c88448f59bd110b36bce5d7598227f46023e
Atishay Jain <atjain@adobe.com>
2024-05-31 22:26:02 -0700

    Add more test cases.

--- test/fixtures/dialects/sqlite/over_clause.sql
+++ test/fixtures/dialects/sqlite/over_clause.sql
@@ -45,3 +45,35 @@ SELECT c, a, b, group_concat(b, '.') FILTER (WHERE c != 'two') OVER (
   ORDER BY a
 ) AS group_concat
 FROM t1 ORDER BY a;
+
+SELECT a                        AS a,
+       row_number() OVER win    AS row_number,
+       rank() OVER win          AS rank,
+       dense_rank() OVER win    AS dense_rank,
+       percent_rank() OVER win  AS percent_rank,
+       cume_dist() OVER win     AS cume_dist
+FROM t2
+WINDOW win AS (ORDER BY a);
+
+SELECT a                        AS a,
+       b                        AS b,
+       ntile(2) OVER win        AS ntile_2,
+       ntile(4) OVER win        AS ntile_4
+FROM t2
+WINDOW win AS (ORDER BY a);
+
+SELECT b                          AS b,
+       lead(b, 2, 'n/a') OVER win AS lead,
+       lag(b) OVER win            AS lag,
+       first_value(b) OVER win    AS first_value,
+       last_value(b) OVER win     AS last_value,
+       nth_value(b, 3) OVER win   AS nth_value_3
+FROM t1
+WINDOW win AS (ORDER BY b ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW);
+
+
+SELECT group_concat(b, '.') OVER (
+  win ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+)
+FROM t1
+WINDOW win AS (PARTITION BY a ORDER BY c);
