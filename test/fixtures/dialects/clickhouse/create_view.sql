8b3bd8b32c89e42281dace0b2bccda6ee58ef255
Dmytro Samodurov <dimasamodurov@gmail.com>
2024-06-10 00:43:00 +0300

    Regenerage create_view.yml after updating sql fixture.

--- test/fixtures/dialects/clickhouse/create_view.sql
+++ test/fixtures/dialects/clickhouse/create_view.sql
@@ -1,23 +1,44 @@
 CREATE VIEW db.view_mv
-AS SELECT column1, column2 FROM db.table_kafka;
+AS SELECT
+    column1,
+    column2
+FROM db.table_kafka;
 
 CREATE VIEW db.view_mv
 ON CLUSTER mycluster
-AS SELECT column1, column2 FROM db.table_kafka;
+AS SELECT
+    column1,
+    column2
+FROM db.table_kafka;
 
 CREATE OR REPLACE VIEW db.view_mv
-AS SELECT column1, column2 FROM db.table_kafka;
+AS SELECT
+    column1,
+    column2
+FROM db.table_kafka;
 
 CREATE OR REPLACE VIEW db.view_mv
 ON CLUSTER mycluster
-AS SELECT column1, column2 FROM db.table_kafka;
+AS SELECT
+    column1,
+    column2
+FROM db.table_kafka;
 
 CREATE VIEW IF NOT EXISTS db.view_mv
-AS SELECT column1, column2 FROM db.table_kafka;
+AS SELECT
+    column1,
+    column2
+FROM db.table_kafka;
 
 CREATE VIEW IF NOT EXISTS db.view_mv
-AS SELECT column1, column2 FROM db.table_kafka;
+AS SELECT
+    column1,
+    column2
+FROM db.table_kafka;
 
 CREATE VIEW IF NOT EXISTS db.view_mv
 ON CLUSTER mycluster
-AS SELECT column1, column2 FROM db.table_kafka;
+AS SELECT
+    column1,
+    column2
+FROM db.table_kafka;
