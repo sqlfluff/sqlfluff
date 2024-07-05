bd329c7796809bdd50dd462a788eb3786bac1999
ulixius9 <mayursingal9@gmail.com>
2024-07-05 12:04:03 +0530

    black

--- src/sqlfluff/dialects/dialect_tsql.py
+++ src/sqlfluff/dialects/dialect_tsql.py
@@ -4158,10 +4158,7 @@ class TableExpressionSegment(BaseSegment):
     type = "table_expression"
     match_grammar: Matchable = OneOf(
         Ref("ValuesClauseSegment"),
-        Sequence(
-            Ref("TableReferenceSegment"),
-            Ref("PostTableExpressionGrammar")
-        ),
+        Sequence(Ref("TableReferenceSegment"), Ref("PostTableExpressionGrammar")),
         Ref("BareFunctionSegment"),
         Ref("FunctionSegment"),
         Ref("OpenRowSetSegment"),
