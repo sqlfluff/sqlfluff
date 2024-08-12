e97feb87a8df58fcd73488f358340058ca096126
ogirardot <olivier.girardot@arkhn.com>
2024-08-12 17:21:15 +0200

    feat(clickhouse): add support for decimal(x,y), decimal32(x) and match

---
+++ test/fixtures/dialects/clickhouse/decimal_support.sql
@@ -0,0 +1,13 @@
+SELECT '12'::Nullable(Decimal(12,0)) as num_value;
+
+SELECT '12'::Nullable(Decimal32(1)) as num_value;
+
+SELECT '12'::Nullable(Decimal64(12)) as num_value;
+
+SELECT '12'::Nullable(Decimal128(12)) as num_value;
+
+SELECT '12'::Nullable(Decimal256(12)) as num_value;
+
+SELECT '12'::Decimal(12,0) as num_value;
+
+SELECT '12'::Nullable(Numeric(12, 0)) as num_value;
