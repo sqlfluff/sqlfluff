b14066989a529c5823d57cd3509c8ef4b07246d5
Alan Cruickshank <alan+git@designingoverload.com>
2024-09-01 10:11:11 +0100

    Move validation to cache routines

--- src/sqlfluff/core/config/cache.py
+++ src/sqlfluff/core/config/cache.py
@@ -26,6 +26,14 @@ else:  # pragma: no cover
 
 COMMA_SEPARATED_PATH_KEYS = ("load_macros_from_path", "loader_search_path")
 RESOLVE_PATH_SUFFIXES = ("_path", "_dir")
+ALLOWABLE_LAYOUT_CONFIG_KEYS = (
+    "spacing_before",
+    "spacing_after",
+    "spacing_within",
+    "line_position",
+    "align_within",
+    "align_scope",
+)
 
 
 def _load_raw_file_as_dict(filepath: str) -> ConfigMappingType:
@@ -78,8 +86,76 @@ def _resolve_paths_in_config(config: ConfigMappingType, filepath: str):
             val = _resolve_path(filepath, val)
 
 
+def _validate_layout_config(config: ConfigMappingType, filepath: str) -> None:
+    """Validate the layout config section of the config.
+
+    We check for valid key values and for the depth of the
+    structure.
+
+    NOTE: For now we don't check that the "type" is a valid one
+    to reference, or that the values are valid. For the values,
+    these are likely to be rejected by the layout routines at
+    runtime. The last risk area is validating that the type is
+    a valid one, but that should be handled by the same as the above.
+    """
+    layout_section = config.get("layout", {})
+    if not layout_section:
+        return None
+
+    preamble = f"Config file {filepath!r} set an invalid `layout` option. "
+    reference = (
+        "See https://docs.sqlfluff.com/en/stable/perma/layout.html"
+        "#configuring-layout for more details."
+    )
+
+    if not isinstance(layout_section, dict):
+        raise SQLFluffUserError(
+            preamble
+            + f"Found value {layout_section!r} instead of a valid layout section. "
+            + reference
+        )
+
+    # The sections within layout can only be "type" (currently).
+    non_type_keys = set(layout_section.keys()) - set(("type",))
+    type_section = layout_section.get("type", {})
+    if non_type_keys or not type_section or not isinstance(type_section, dict):
+        raise SQLFluffUserError(
+            preamble
+            + "Only sections of the form `sqlfluff:layout:type:...` are valid. "
+            + reference
+        )
+
+    for layout_type, layout_section in type_section.items():
+        if not isinstance(layout_section, dict):
+            raise SQLFluffUserError(
+                preamble
+                + f"Layout config for {layout_type!r} is invalid. Expected a section. "
+                + reference
+            )
+
+        invalid_keys = set(layout_section.keys()) - set(ALLOWABLE_LAYOUT_CONFIG_KEYS)
+        if invalid_keys:
+            raise SQLFluffUserError(
+                preamble
+                + f"Layout config for type {layout_type!r} is invalid. "
+                + f"Found the following invalid keys: {invalid_keys}. "
+                + reference
+            )
+
+        for key in ALLOWABLE_LAYOUT_CONFIG_KEYS:
+            if key in layout_section:
+                if isinstance(layout_section[key], dict):
+                    raise SQLFluffUserError(
+                        preamble
+                        + f"Layout config for type {layout_type!r} is invalid. "
+                        + "Found the an unexpected section rather than "
+                        + f"value for {key}. "
+                        + reference
+                    )
+
+
 @cache
-def load_config_file_as_dict(filepath: str):
+def load_config_file_as_dict(filepath: str) -> ConfigMappingType:
     """Load the given config file into a dict and validate.
 
     This method is cached to mitigate being called multiple times.
@@ -92,6 +168,9 @@ def load_config_file_as_dict(filepath: str):
     # The raw loaded files have some path interpolation which is necessary.
     _resolve_paths_in_config(raw_config, filepath)
     # Validate the config for any removed values
-    validate_config_dict_for_removed(raw_config, filepath=filepath)
+    validate_config_dict_for_removed(raw_config, filepath)
+    # Validate layout section
+    _validate_layout_config(raw_config, filepath)
 
-    # TODO: Still need to validate the layout configs.
+    # Return dict object (which will be cached)
+    return raw_config
