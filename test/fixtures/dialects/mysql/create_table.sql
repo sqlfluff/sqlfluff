9cf36039428d9038b5ea0e9e43116a79dc23cd40
Danny Jones <51742311+WittierDinosaur@users.noreply.github.com>
2024-09-10 14:56:13 +0200

    MYSQL: Create Table Optional AS (#6109)
    Co-authored-by: Alan Cruickshank <alanmcruickshank@gmail.com>

--- test/fixtures/dialects/mysql/create_table.sql
+++ test/fixtures/dialects/mysql/create_table.sql
@@ -13,3 +13,23 @@ create table `tickets` (
 );
 
 create table _ (a int);
+
+CREATE TABLE foo SELECT * FROM bar;
+
+CREATE TEMPORARY TABLE tbl_name (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    col VARCHAR(255) DEFAULT '' NOT NULL,
+    INDEX(col)
+) AS SELECT id, col FROM table_name;
+
+CREATE TEMPORARY TABLE tbl_name (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    col VARCHAR(255) DEFAULT '' NOT NULL,
+    INDEX(col)
+) SELECT id, col FROM table_name;
+
+CREATE TEMPORARY TABLE tbl_name (INDEX(col)) AS
+    SELECT id, col FROM table_name;
+
+CREATE TEMPORARY TABLE tbl_name (INDEX(col))
+    SELECT id, col FROM table_name;
