eed25b25000cbd8620a4675d68028fc53332b06c
troshnev <troshnev@gmail.com>
2024-09-11 20:27:24 +0300

    # Add UTF8 support for identifiers in Vertica dialect # https://docs.vertica.com/24.3.x/en/sql-reference/language-elements/identifiers/ : # Unquoted SQL identifiers must begin with one of the following: # * Non-Unicode letters: A–Z or a-z # -- /actually Vertica accepts also non-ASCII UTF-8 Unicode characters here, which is not well documented/ # * Underscore (_) # Subsequent characters in an identifier can be any combination of the following: # * Non-Unicode letters: A–Z or a-z # * Underscore (_) # * Digits(0–9) # * Unicode letters (letters with diacriticals or not in the Latin alphabet), unsupported for model names # * Dollar sign ($), unsupported for model names # # Vertica accepts **non-ASCII UTF-8 Unicode characters** for table names, column names, and other identifiers, # extending the cases where upper/lower case distinctions are ignored (case-folded) to all alphabets, # including Latin, Cyrillic, and Greek. # # Changes to be committed: #	modified:   src/sqlfluff/dialects/dialect_vertica.py #	new file:   test/fixtures/dialects/vertica/utf8.sql

---
+++ test/fixtures/dialects/vertica/utf8.sql
@@ -0,0 +1,4 @@
+--https://docs.vertica.com/24.3.x/en/sql-reference/language-elements/identifiers/
+CREATE TABLE straße(x int, y int);
+SELECT Фамилия FROM Клиенты ORDER BY 1;
+select * from Παραγγελίες;
