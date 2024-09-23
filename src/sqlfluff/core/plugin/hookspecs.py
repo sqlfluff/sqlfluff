b7ddb7aaf6e5518be788a8dfe403c02ee87cddd8
Alan Cruickshank <alan+git@designingoverload.com>
2024-09-23 17:37:56 +0200

    Finish the job

--- src/sqlfluff/core/plugin/hookspecs.py
+++ src/sqlfluff/core/plugin/hookspecs.py
@@ -1,10 +1,14 @@
 """Defines the specification to implement a plugin."""
 
 from abc import abstractmethod
+from typing import TYPE_CHECKING, Any, Dict, List, Type
 
 import pluggy
 
 from sqlfluff.core.plugin import plugin_base_name
+
+if TYPE_CHECKING:
+    from sqlfluff.core.rules import BaseRule
 
 hookspec = pluggy.HookspecMarker(plugin_base_name)
 
@@ -14,15 +18,18 @@ class PluginSpec:
 
     @hookspec
     @abstractmethod
-    def get_rules(self):
+    def get_rules(self) -> List[Type["BaseRule"]]:
         """Get plugin rules."""
 
     @hookspec
     @abstractmethod
-    def load_default_config(self) -> dict:
+    def load_default_config(self) -> Dict[str, Any]:
         """Loads the default configuration for the plugin."""
 
     @hookspec
     @abstractmethod
-    def get_configs_info(self) -> dict:
+    # TODO: This type annotation could probably be more specific but that would
+    # require making the config info object something more like a namedTuple rather
+    # than a dict.
+    def get_configs_info(self) -> Dict[str, Dict[str, Any]]:
         """Get rule config validations and descriptions."""
