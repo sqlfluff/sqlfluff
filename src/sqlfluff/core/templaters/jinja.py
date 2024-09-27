22e49aa8ecc06c1fbb13794be45ee4e77607cb58
Alan Cruickshank <alan+git@designingoverload.com>
2024-09-27 08:47:16 +0100

    revert the linting issue

--- src/sqlfluff/core/templaters/jinja.py
+++ src/sqlfluff/core/templaters/jinja.py
@@ -920,9 +920,9 @@ class JinjaTemplater(PythonTemplater):
                     # (here that is options[0]).
                     new_value = "True" if options[0] == branch + 1 else "False"
                     new_source = f"{{% {raw_file_slice.tag} {new_value} %}}"
-                    tracer_trace.raw_slice_info[
-                        raw_file_slice
-                    ].alternate_code = new_source
+                    tracer_trace.raw_slice_info[raw_file_slice].alternate_code = (
+                        new_source
+                    )
                     override_raw_slices.append(branch)
                     length_deltas[raw_file_slice.source_idx] = len(new_source) - len(
                         raw_file_slice.raw
