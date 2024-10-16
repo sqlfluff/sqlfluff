1aed73950d93e451330ee54b9dc13df97983faf0
Alan Cruickshank <alan+git@designingoverload.com>
2024-10-16 21:13:58 +0100

    Update the docstring

--- src/sqlfluff/rules/structure/ST10.py
+++ src/sqlfluff/rules/structure/ST10.py
@@ -1,22 +1,59 @@
 """Implementation of Rule ST10."""
-from typing import List, Optional, Tuple
 
-from sqlfluff.core.parser import (
-    KeywordSegment,
-    SymbolSegment,
-    WhitespaceSegment,
-    WordSegment,
-)
-from sqlfluff.core.parser.segments.base import BaseSegment
+from typing import Optional
+
 from sqlfluff.core.rules import BaseRule, LintFix, LintResult, RuleContext
-from sqlfluff.core.rules.crawlers import SegmentSeekerCrawler
-from sqlfluff.utils.functional import FunctionalContext, Segments, sp
 
 
 class Rule_ST10(BaseRule):
     """Unused tables in joins should be removed.
 
-    This rule will check if there are any tables that are joined in but not used in the select statement.
+    This rule will check if there are any tables that are referenced in the
+    ``FROM`` or ``JOIN`` clause of a ``SELECT`` statement, but where no
+    columns from that table are referenced in the any of the other clauses.
+
+    This rule relies on all of the column references in the ``SELECT``
+    statement being qualified with at least the table name, and so is
+    designed to work alongside :sqlfluff:ref:`references.qualification`
+    (:sqlfluff:ref:`RF02`).
+
+    This rule does not propose a fix, because it assumes that it an unused
+    table is a mistake, but doesn't know whether the mistake was the join,
+    or the mistake was not using it.
+
+    **Anti-pattern**
+
+    In this example, the table ``bar`` is included in the ``JOIN`` clause
+    but not columns from it are referenced in
+
+    .. code-block:: sql
+
+        SELECT
+            foo.a,
+            foo.b
+        FROM foo
+        LEFT JOIN bar ON foo.a = bar.a
+
+    **Best practice**
+
+    Remove the join, or use the table.
+
+    .. code-block:: sql
+
+        SELECT foo.a, vee.b
+        FROM foo;
+
+        SELECT
+            foo.a,
+            foo.b,
+            bar.c
+        FROM foo
+        LEFT JOIN bar ON foo.a = bar.a
+
+    In the (*very rare*) situations that it is logically necessary to include
+    a table in a join clause, but not otherwise refer to it (likely for
+    granularity reasons), we recommend ignoring this rule for that specific
+    line by using ``-- noqa: ST10`` at the end of the line.
     """
 
     def _eval(self, context: RuleContext) -> Optional[LintResult]:
