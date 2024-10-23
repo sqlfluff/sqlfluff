b35c903fc215d5b0d0e87766244f3b99cb7f0fea
Łukasz Rogalski <rogalski.91@gmail.com>
2024-10-23 23:57:01 +0200

    New Rule: const expression checker

---
+++ src/sqlfluff/rules/structure/ST10.py
@@ -0,0 +1,54 @@
+"""Implementation of Rule ST10."""
+
+from typing import Iterator, Tuple
+
+from sqlfluff.core.rules import BaseRule, EvalResultType, LintResult, RuleContext
+from sqlfluff.core.rules.crawlers import SegmentSeekerCrawler
+
+
+class Rule_ST10(BaseRule):
+    """Const expression rule."""
+
+    name = "structure.const_expression"
+    aliases = ()
+    groups: Tuple[str, ...] = ("all", "structure")
+    config_keywords = []
+    crawl_behaviour = SegmentSeekerCrawler({"expression"})
+    is_fix_compatible = False
+
+    def _eval(self, context: RuleContext) -> EvalResultType:
+        return [lint_result for lint_result in self._eval_gen(context)]
+
+    def _eval_gen(self, context: RuleContext) -> Iterator[LintResult]:
+        assert context.segment.is_type("expression")
+        subsegments = context.segment.segments
+        count_subsegments = len(subsegments)
+        for idx, seg in enumerate(context.segment.segments):
+            if seg.is_type("comparison_operator"):
+                if seg.raw not in ("=", "!=", "<>"):
+                    continue
+                lhs = next(
+                    (
+                        subsegments[i]
+                        for i in range(idx - 1, -1, -1)
+                        if not subsegments[i].is_whitespace
+                    ),
+                    None,
+                )
+                rhs = next(
+                    (
+                        subsegments[i]
+                        for i in range(idx + 1, count_subsegments, 1)
+                        if not subsegments[i].is_whitespace
+                    ),
+                    None,
+                )
+                if not lhs or not rhs:
+                    continue
+                if lhs.raw_normalized() != rhs.raw_normalized():
+                    continue
+
+                expr_str = f"{lhs.raw_normalized()} {seg.raw} {rhs.raw_normalized()}"
+                if expr_str in ("1 = 1", "1 = 0"):
+                    continue
+                yield LintResult(context.segment)
