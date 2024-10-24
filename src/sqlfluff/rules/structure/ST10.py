b9a4bf674ab8aba6fac33b363c84c923ad3ea076
Łukasz Rogalski <rogalski.91@gmail.com>
2024-10-24 21:21:26 +0200

    Improve docs, test cases, literal allowlist

--- src/sqlfluff/rules/structure/ST10.py
+++ src/sqlfluff/rules/structure/ST10.py
@@ -7,25 +7,39 @@ from sqlfluff.core.rules.crawlers import SegmentSeekerCrawler
 
 
 class Rule_ST10(BaseRule):
-    """Const expression rule.
+    """Redundant constant expression.
+
+    Including an expression that always evaluates to
+    either ``TRUE`` or ``FALSE`` regardless of the input columns is
+    unnecessary and makes statements harder to read and understand.
+
+    Constant conditions are sometimes mistakes (by mistyping the column
+    name intended), and sometimes the result of incorrect information that
+    they are necessary in some circumstances. In the former case, they can
+    sometimes result in a cartesian join if it was supposed to be a join
+    condition. Given the ambiguity of intent, this rule does not suggest
+    an automatic fix, and instead invites the user to resolve the problem
+    manually.
 
     **Anti-pattern**
 
     .. code-block:: sql
-
-         -- TODO
-        SELECT 1;
+        SELECT *
+        FROM my_table
+        -- This following WHERE clause is redundant.
+        WHERE my_table.col = my_table.col
 
     **Best practice**
 
     .. code-block:: sql
-
-         -- TODO
-        SELECT 1;
-
+        SELECT *
+        FROM my_table
+        -- Replace with a condition that includes meaningful logic,
+        -- or remove the condition entirely.
+        WHERE my_table.col > 3
     """
 
-    name = "structure.const_expression"
+    name = "structure.constant_expression"
     aliases = ()
     groups: Tuple[str, ...] = ("all", "structure")
     config_keywords = []
@@ -62,10 +76,16 @@ class Rule_ST10(BaseRule):
                 if not lhs or not rhs:
                     # Should be unreachable with correctly parsed tree
                     continue  # pragma: no cover
-                if lhs.raw_normalized() != rhs.raw_normalized():
-                    continue
 
-                expr_str = f"{lhs.raw_normalized()} {seg.raw} {rhs.raw_normalized()}"
-                if expr_str in ("1 = 1", "1 = 0"):
-                    continue
-                yield LintResult(context.segment)
+                # literals need explicit handling (due to well-defined allow-list)
+                if lhs.is_type("literal") and rhs.is_type("literal"):
+                    expr_s = f"{lhs.raw_normalized()} {seg.raw} {rhs.raw_normalized()}"
+                    if expr_s in ("1 = 1", "1 = 0"):
+                        # ignore based on allowlist
+                        continue
+                else:
+                    if lhs.raw_normalized() != rhs.raw_normalized():
+                        continue
+
+                # attach violation to eq/ne operator in expression
+                yield LintResult(seg)
