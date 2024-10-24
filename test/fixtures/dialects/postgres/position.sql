41246f86449944c94c1f6d6e016d85ab9b165497
Cameron <105471409+keraion@users.noreply.github.com>
2024-10-23 23:05:46 -0400

    Postgres: Handle expressions that occur in `IN` functions

--- test/fixtures/dialects/postgres/position.sql
+++ test/fixtures/dialects/postgres/position.sql
@@ -4,4 +4,7 @@ select
   p.product_id
 from user_tb as u
 inner join product_tb as p on u.user_id = p.user_id
-  and position('@domain' in u.user_email) = 0
+  and position('@domain' in u.user_email) = 0;
+
+
+SELECT position('#' IN tbl.col_a -> 'b') AS hash_pos FROM tbl;
