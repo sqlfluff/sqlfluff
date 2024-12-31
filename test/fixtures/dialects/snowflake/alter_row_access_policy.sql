570dc9d4f9283d6507be0ffad144584095d9da03
Logan Price <logan.price@kiewit.com>
2024-12-31 10:07:16 -0600

    Filled in gap

---
+++ test/fixtures/dialects/snowflake/alter_row_access_policy.sql
@@ -0,0 +1,15 @@
+-- ALTER ROW ACCESS POLICY my_access_policy RENAME TO other_access_policy;
+
+-- ALTER ROW ACCESS POLICY IF EXISTS my_access_policy RENAME TO other_access_policy;
+
+-- ALTER ROW ACCESS POLICY my_access_policy SET BODY -> EXISTS(some_val);
+
+-- ALTER ROW ACCESS POLICY my_access_policy SET TAG tag_name = 'tag_value';
+
+-- ALTER ROW ACCESS POLICY my_access_policy SET TAG tag_name = 'tag_value', tag_name = 'tag_value';
+
+-- ALTER ROW ACCESS POLICY my_access_policy UNSET TAG tag_name, tag_name;
+
+-- ALTER ROW ACCESS POLICY my_access_policy SET COMMENT = 'comment';
+
+-- ALTER ROW ACCESS POLICY my_access_policy UNSET COMMENT;
