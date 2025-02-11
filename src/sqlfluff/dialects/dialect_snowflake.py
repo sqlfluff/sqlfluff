6666db9ed97f45161fb318f901392d9a214808d2
LoganPrice344 <125611162+LoganPrice344@users.noreply.github.com>
2025-02-11 10:12:29 -0600

    Allows Snowflake `CREATE ROW ACCESS POLICY` to Parse Quoted Policy Names (#6648)
    Co-authored-by: Danny Jones <51742311+WittierDinosaur@users.noreply.github.com>

--- src/sqlfluff/dialects/dialect_snowflake.py
+++ src/sqlfluff/dialects/dialect_snowflake.py
@@ -8974,7 +8974,7 @@ class CreateRowAccessPolicyStatementSegment(BaseSegment):
         "ACCESS",
         "POLICY",
         Ref("IfNotExistsGrammar", optional=True),
-        Ref("NakedIdentifierSegment"),
+        OneOf(Ref("NakedIdentifierSegment"), Ref("QuotedIdentifierSegment")),
         "AS",
         Ref("FunctionParameterListGrammar"),
         "RETURNS",
