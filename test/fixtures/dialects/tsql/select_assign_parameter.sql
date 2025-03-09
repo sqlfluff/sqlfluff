c2c31650575ff87cf48a11678239f50e6177a196
Jason Welch <jwelch@calromltd.onmicrosoft.com>
2025-03-09 14:30:52 +0000

    More assignment test cases

--- test/fixtures/dialects/tsql/select_assign_parameter.sql
+++ test/fixtures/dialects/tsql/select_assign_parameter.sql
@@ -1,11 +1,23 @@
+-- T-SQL alternative alias syntax (AltAliasExpression)
+
 select userid = c.id
 from
 	mydb.myschema.customer c
 where
 	c.name = 'drjwelch';
 
+-- T-SQL parameter assignment, previously (<3.1.0) parsed as AltAliasExpression
+
 select @userid_parameter = c.id
 from
 	mydb.myschema.customer c
 where
 	c.name = 'drjwelch';
+
+-- T-SQL parameter assignment from sequence
+
+select @userid_parameter = NEXT VALUE FOR myschema.customer_ids;
+
+-- NULL assignment to parameter using SELECT is not linted as a comparison (issue #6000)
+
+select @userid_parameter = NULL;
