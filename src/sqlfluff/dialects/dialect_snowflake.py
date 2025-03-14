1f319d2c5c1936e6dff8436ce66a8de2662f23ab
juli4nb4dillo <julian.badillo@numat.com>
2025-03-14 15:24:21 -0500

    Add rsa_public_key to snowflake dialect

--- src/sqlfluff/dialects/dialect_snowflake.py
+++ src/sqlfluff/dialects/dialect_snowflake.py
@@ -5201,12 +5201,18 @@ class CreateUserSegment(BaseSegment):
             Sequence(
                 "RSA_PUBLIC_KEY",
                 Ref("EqualsSegment"),
-                Ref("ObjectReferenceSegment"),
+                OneOf(
+                    Ref("ObjectReferenceSegment"),
+                    Ref("QuotedLiteralSegment"),
+                ),
             ),
             Sequence(
                 "RSA_PUBLIC_KEY_2",
                 Ref("EqualsSegment"),
-                Ref("ObjectReferenceSegment"),
+                OneOf(
+                    Ref("ObjectReferenceSegment"),
+                    Ref("QuotedLiteralSegment"),
+                ),
             ),
             Sequence(
                 "TYPE",
