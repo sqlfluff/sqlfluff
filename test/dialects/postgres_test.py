55f2528a120d93e277edb33be95833bb77739ce6
Inger van Boeijen <boeijen@nfu.nl>
2025-04-18 16:25:58 +0200

    should ignore indent errors for the postgress test

--- test/dialects/postgres_test.py
+++ test/dialects/postgres_test.py
@@ -65,7 +65,9 @@ def test_epoch_datetime_unit(raw: str) -> None:
     """Test the EPOCH keyword for postgres dialect."""
     # Don't test for new lines or capitalisation
     cfg = FluffConfig(
-        configs={"core": {"exclude_rules": "LT12,LT05,LT09", "dialect": "postgres"}}
+        configs={
+            "core": {"exclude_rules": "LT12,LT05,LT09,LT02", "dialect": "postgres"}
+        }
     )
     lnt = Linter(config=cfg)
     result = lnt.lint_string(raw)
@@ -82,7 +84,9 @@ def test_epoch_datetime_unit(raw: str) -> None:
 def test_space_is_not_reserved(raw: str) -> None:
     """Ensure that SPACE is not treated as reserved."""
     cfg = FluffConfig(
-        configs={"core": {"exclude_rules": "LT12,LT05,AL07", "dialect": "postgres"}}
+        configs={
+            "core": {"exclude_rules": "LT12,LT05,AL07,LT02", "dialect": "postgres"}
+        }
     )
     lnt = Linter(config=cfg)
     result = lnt.lint_string(raw)
