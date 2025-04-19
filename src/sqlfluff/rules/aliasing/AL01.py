eedb5cad5b733d09e539c38e32dd47ad9e44a634
Inger van Boeijen <boeijen@nfu.nl>
2025-04-19 15:41:33 +0200

    revert back to original

--- src/sqlfluff/rules/aliasing/AL01.py
+++ src/sqlfluff/rules/aliasing/AL01.py
@@ -63,13 +63,13 @@ class Rule_AL01(BaseRule):
 
         assert context.segment.is_type("alias_expression")
         if context.parent_stack[-1].is_type(*self._target_parent_types):
-
             # Search for an AS keyword.
-            as_keyword: Optional[BaseSegment] = None
-            for segment in context.segment.raw_segments:
-                if segment.raw_upper == "AS":
-                    as_keyword = segment
+            as_keyword: Optional[BaseSegment]
+            for as_keyword in context.segment.segments:
+                if as_keyword.raw_upper == "AS":
                     break
+            else:
+                as_keyword = None
 
             if as_keyword:
                 if self.aliasing == "implicit":
