733c287999cfd500c117a6f059533fb66e2dbcf2
shane.mazur <shane.mazur@scalepad.com>
2025-05-06 19:41:59 -0400

    Allow lateral statements on exploded results in sparksql

--- src/sqlfluff/dialects/dialect_sparksql.py
+++ src/sqlfluff/dialects/dialect_sparksql.py
@@ -2254,9 +2254,10 @@ class LateralViewClauseSegment(BaseSegment):
     type = "lateral_view_clause"
 
     match_grammar = Sequence(
+        Ref("CommaSegment", optional=True),
         Indent,
         "LATERAL",
-        "VIEW",
+        OneOf("VIEW",optional=True),
         Ref.keyword("OUTER", optional=True),
         Ref("FunctionSegment"),
         OneOf(
