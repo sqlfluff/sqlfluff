04731fe01ebbc7c6946c23835f127c2e8911ad9f
Peter Budai <peterbudai@hotmail.com>
2025-06-25 10:19:21 +0200

    Add support for CREATE DATABASE statement in T-SQL dialect
    - Introduced new segments for handling CREATE DATABASE syntax, including file specifications, options, and collations.
    - Added PercentSegment for file growth percentage.
    - Updated dialect keywords to include new options related to database creation.
    - Created comprehensive test cases for various CREATE DATABASE scenarios, ensuring coverage for different configurations and options.

---
+++ test/fixtures/dialects/tsql/create_database.sql
@@ -0,0 +1,154 @@
+CREATE DATABASE mytest;
+
+CREATE DATABASE Sales
+ON
+( NAME = Sales_dat,
+    FILENAME = 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\DATA\saledat.mdf',
+    SIZE = 10,
+    MAXSIZE = 50,
+    FILEGROWTH = 5 )
+LOG ON
+( NAME = Sales_log,
+    FILENAME = 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\DATA\salelog.ldf',
+    SIZE = 5MB,
+    MAXSIZE = 25MB,
+    FILEGROWTH = 5MB
+    ) ;
+GO
+
+CREATE DATABASE Archive
+ON
+PRIMARY
+    (NAME = Arch1,
+    FILENAME = 'D:\SalesData\archdat1.mdf',
+    SIZE = 100MB,
+    MAXSIZE = 200,
+    FILEGROWTH = 20),
+    ( NAME = Arch2,
+    FILENAME = 'D:\SalesData\archdat2.ndf',
+    SIZE = 100MB,
+    MAXSIZE = 200,
+    FILEGROWTH = 20),
+    ( NAME = Arch3,
+    FILENAME = 'D:\SalesData\archdat3.ndf',
+    SIZE = 100MB,
+    MAXSIZE = 200,
+    FILEGROWTH = 20)
+LOG ON
+  (NAME = Archlog1,
+    FILENAME = 'D:\SalesData\archlog1.ldf',
+    SIZE = 100MB,
+    MAXSIZE = 200,
+    FILEGROWTH = 20),
+  (NAME = Archlog2,
+    FILENAME = 'D:\SalesData\archlog2.ldf',
+    SIZE = 100MB,
+    MAXSIZE = 200,
+    FILEGROWTH = 20) ;
+GO
+
+CREATE DATABASE Sales
+ON PRIMARY
+( NAME = SPri1_dat,
+    FILENAME = 'D:\SalesData\SPri1dat.mdf',
+    SIZE = 10,
+    MAXSIZE = 50,
+    FILEGROWTH = 15% ),
+( NAME = SPri2_dat,
+    FILENAME = 'D:\SalesData\SPri2dt.ndf',
+    SIZE = 10,
+    MAXSIZE = 50,
+    FILEGROWTH = 15% ),
+FILEGROUP SalesGroup1
+( NAME = SGrp1Fi1_dat,
+    FILENAME = 'D:\SalesData\SG1Fi1dt.ndf',
+    SIZE = 10,
+    MAXSIZE = 50,
+    FILEGROWTH = 5 ),
+( NAME = SGrp1Fi2_dat,
+    FILENAME = 'D:\SalesData\SG1Fi2dt.ndf',
+    SIZE = 10,
+    MAXSIZE = 50,
+    FILEGROWTH = 5 ),
+FILEGROUP SalesGroup2
+( NAME = SGrp2Fi1_dat,
+    FILENAME = 'D:\SalesData\SG2Fi1dt.ndf',
+    SIZE = 10,
+    MAXSIZE = 50,
+    FILEGROWTH = 5 ),
+( NAME = SGrp2Fi2_dat,
+    FILENAME = 'D:\SalesData\SG2Fi2dt.ndf',
+    SIZE = 10,
+    MAXSIZE = 50,
+    FILEGROWTH = 5 )
+LOG ON
+( NAME = Sales_log,
+    FILENAME = 'E:\SalesLog\salelog.ldf',
+    SIZE = 5MB,
+    MAXSIZE = 25MB,
+    FILEGROWTH = 5MB ) ;
+GO
+
+
+CREATE DATABASE sales_snapshot0600 ON
+    ( NAME = SPri1_dat, FILENAME = 'D:\SalesData\SPri1dat_0600.ss'),
+    ( NAME = SPri2_dat, FILENAME = 'D:\SalesData\SPri2dt_0600.ss'),
+    ( NAME = SGrp1Fi1_dat, FILENAME = 'D:\SalesData\SG1Fi1dt_0600.ss'),
+    ( NAME = SGrp1Fi2_dat, FILENAME = 'D:\SalesData\SG1Fi2dt_0600.ss'),
+    ( NAME = SGrp2Fi1_dat, FILENAME = 'D:\SalesData\SG2Fi1dt_0600.ss'),
+    ( NAME = SGrp2Fi2_dat, FILENAME = 'D:\SalesData\SG2Fi2dt_0600.ss')
+AS SNAPSHOT OF Sales ;
+GO
+
+CREATE DATABASE MyOptionsTest
+COLLATE French_CI_AI
+WITH TRUSTWORTHY ON, DB_CHAINING ON;
+
+CREATE DATABASE AdventureWorks2022 ON
+    (FILENAME = 'c:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\Data\AdventureWorks2022_data.mdf'),
+    (FILENAME = 'c:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\Data\AdventureWorks2022_log.ldf'),
+    (FILENAME = 'c:\myFTCatalogs\AdvWksFtCat')
+FOR ATTACH;
+GO
+
+CREATE DATABASE [BlobStore1]
+CONTAINMENT = NONE
+ON PRIMARY
+(
+    NAME = N'BlobStore1',
+    FILENAME = N'C:\BlobStore\BlobStore1.mdf',
+    SIZE = 100MB,
+    MAXSIZE = UNLIMITED,
+    FILEGROWTH = 1MB
+),
+FILEGROUP [FS] CONTAINS FILESTREAM DEFAULT
+(
+    NAME = N'FS1',
+    FILENAME = N'C:\BlobStore\FS1',
+    MAXSIZE = UNLIMITED
+),
+(
+    NAME = N'FS2',
+    FILENAME = N'C:\BlobStore\FS2',
+    MAXSIZE = 100MB
+)
+LOG ON
+(
+    NAME = N'BlobStore1_log',
+    FILENAME = N'C:\BlobStore\BlobStore1_log.ldf',
+    SIZE = 100MB,
+    MAXSIZE = 1GB,
+    FILEGROWTH = 1MB
+);
+GO
+
+CREATE DATABASE [different-collations] COLLATE SQL_Latin1_General_CP1_CS_AS
+
+CREATE DATABASE [same-collations] COLLATE SQL_Latin1_General_CP1_CS_AS
+WITH CATALOG_COLLATION = DATABASE_DEFAULT;
+
+CREATE DATABASE Archive
+  ON
+  (FILENAME = 'D:\SalesData\archdat1.mdf')
+  FOR ATTACH ;
+GO
