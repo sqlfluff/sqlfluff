a7936a22caf85c6f0a11aca6c03142bb770d586c
Cameron <105471409+keraion@users.noreply.github.com>
2025-07-16 02:39:13 -0400

    Oracle: Add `RELIES_ON` and `POLYMORPHIC` keywords (#7015)

--- test/fixtures/dialects/oracle/create_package.sql
+++ test/fixtures/dialects/oracle/create_package.sql
@@ -88,3 +88,38 @@ PROCEDURE increase_comm(employee_id NUMBER, comm_incr NUMBER) IS
    END;
 END emp_mgmt;
 /
+
+-- Use of RESULT_CACHE keyword
+CREATE OR REPLACE PACKAGE test_package IS
+
+FUNCTION any_function_with_result_cache
+RETURN VARCHAR2 RESULT_CACHE;
+
+END test_package;
+/
+
+-- Use of POLYMORPHIC keyword
+CREATE PACKAGE skip_col_pkg AS
+
+  -- OVERLOAD 1: Skip by name --
+  FUNCTION skip_col(tab TABLE,
+                    col COLUMNS)
+           RETURN TABLE PIPELINED ROW POLYMORPHIC USING skip_col_pkg;
+
+  FUNCTION describe(tab IN OUT DBMS_TF.TABLE_T,
+                    col        DBMS_TF.COLUMNS_T)
+           RETURN DBMS_TF.DESCRIBE_T;
+
+  -- OVERLOAD 2: Skip by type --
+  FUNCTION skip_col(tab       TABLE,
+                    type_name VARCHAR2,
+                    flip      VARCHAR2 DEFAULT 'False')
+           RETURN TABLE PIPELINED ROW POLYMORPHIC USING skip_col_pkg;
+
+  FUNCTION describe(tab       IN OUT DBMS_TF.TABLE_T,
+                    type_name        VARCHAR2,
+                    flip             VARCHAR2 DEFAULT 'False')
+           RETURN DBMS_TF.DESCRIBE_T;
+
+END skip_col_pkg;
+/
