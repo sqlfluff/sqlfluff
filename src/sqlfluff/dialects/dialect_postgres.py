3056acf5f1d6d38f50ce1c3b03b2fa9833709769
Fran Lozano <fjlozanos@gmail.com>
2025-08-09 20:30:19 +0200

    postgres: Fix dialect to allow CTEs in COPY

--- src/sqlfluff/dialects/dialect_postgres.py
+++ src/sqlfluff/dialects/dialect_postgres.py
@@ -5795,9 +5795,7 @@ class CopyStatementSegment(BaseSegment):
                 _postgres9_compatible_stdin_options,
             ),
             Sequence(
-                OneOf(
-                    _table_definition, Bracketed(Ref("UnorderedSelectStatementSegment"))
-                ),
+                OneOf(_table_definition, Bracketed(Ref("SelectableGrammar"))),
                 "TO",
                 OneOf(
                     _target_subset,
@@ -5806,9 +5804,7 @@ class CopyStatementSegment(BaseSegment):
                 _option,
             ),
             Sequence(
-                OneOf(
-                    _table_definition, Bracketed(Ref("UnorderedSelectStatementSegment"))
-                ),
+                OneOf(_table_definition, Bracketed(Ref("SelectableGrammar"))),
                 "TO",
                 OneOf(
                     Ref("QuotedLiteralSegment"),
