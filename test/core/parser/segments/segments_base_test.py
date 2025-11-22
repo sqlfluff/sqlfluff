fed27b45d4a875b4650c81024e2799f0194ac307
Peter Budai <peterbudai@hotmail.com>
2025-11-22 14:51:25 +0100

    fix(parse): make position info optional in tuple serialization

--- test/core/parser/segments/segments_base_test.py
+++ test/core/parser/segments/segments_base_test.py
@@ -46,8 +46,7 @@ def test__parser__base_segments_to_tuple_a(raw_segments, DummySegment, DummyAuxS
     test_seg = DummySegment([DummyAuxSegment(raw_segments)])
     assert test_seg.to_tuple() == (
         "dummy",
-        (("dummy_aux", (("raw", (), None), ("raw", (), None)), None),),
-        None,
+        (("dummy_aux", (("raw", ()), ("raw", ()))),),
     )
 
 
@@ -58,18 +57,7 @@ def test__parser__base_segments_to_tuple_b(raw_segments, DummySegment, DummyAuxS
     )
     assert test_seg.to_tuple() == (
         "dummy",
-        (
-            (
-                "dummy_aux",
-                (
-                    ("raw", (), None),
-                    ("raw", (), None),
-                    ("dummy_aux", (("raw", (), None),), None),
-                ),
-                None,
-            ),
-        ),
-        None,
+        (("dummy_aux", (("raw", ()), ("raw", ()), ("dummy_aux", (("raw", ()),)))),),
     )
 
 
@@ -84,14 +72,12 @@ def test__parser__base_segments_to_tuple_c(raw_segments, DummySegment, DummyAuxS
             (
                 "dummy_aux",
                 (
-                    ("raw", "foobar", None),
-                    ("raw", ".barfoo", None),
-                    ("dummy_aux", (("raw", "foobar", None),), None),
+                    ("raw", "foobar"),
+                    ("raw", ".barfoo"),
+                    ("dummy_aux", (("raw", "foobar"),)),
                 ),
-                None,
             ),
         ),
-        None,
     )
 
 
@@ -220,9 +206,9 @@ def test__parser__base_segments_raw(raw_seg):
         "        'foobar'\n"
     )
     # Check tuple
-    assert raw_seg.to_tuple() == ("raw", (), None)
+    assert raw_seg.to_tuple() == ("raw", ())
     # Check tuple
-    assert raw_seg.to_tuple(show_raw=True) == ("raw", "foobar", None)
+    assert raw_seg.to_tuple(show_raw=True) == ("raw", "foobar")
 
 
 def test__parser__base_segments_base(raw_segments, fresh_ansi_dialect, DummySegment):
@@ -244,7 +230,6 @@ def test__parser__base_segments_base(raw_segments, fresh_ansi_dialect, DummySegm
     assert base_seg.to_tuple() == (
         "dummy",
         (raw_segments[0].to_tuple(), raw_segments[1].to_tuple()),
-        None,
     )
     # Check Formatting and Stringification
     assert str(base_seg) == repr(base_seg) == "<DummySegment: ([L:  1, P:  1])>"
