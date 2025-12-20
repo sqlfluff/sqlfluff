28df9f1b64c3be2f5e68acd78b7c80c2df546960
Annebelle Olminkhof <20630578+annebelleo@users.noreply.github.com>
2025-12-20 23:48:47 +0100

    linting

--- test/fixtures/dialects/postgres/function_expansion.sql
+++ test/fixtures/dialects/postgres/function_expansion.sql
@@ -3,7 +3,7 @@ SELECT (JSONB_EACH_TEXT(data)).* FROM table1;
 SELECT (JSONB_EACH_TEXT(w.inventory_events)).* FROM public.widget AS w;
 SELECT (JSON_EACH(config)).* FROM settings;
 SELECT (JSONB_EACH(items)).* FROM inventory;
-SELECT 
+SELECT
     id,
     (JSONB_EACH_TEXT(data)).*,
     (JSON_EACH(metadata)).*
