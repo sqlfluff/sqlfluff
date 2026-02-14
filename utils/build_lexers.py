9ffc84c712cad8938c9ad00f7cb46aec4e987e84
Darrick Purfield <dpurfield@users.noreply.github.com>
2026-02-14 07:33:56 -0800

    Update build_lexers.py

--- utils/build_lexers.py
+++ utils/build_lexers.py
@@ -95,7 +95,7 @@ def _as_rust_lexer_matcher(lexer_matcher: LexerType, dialect: str, is_subdivide=
         "['x','X','.','0','1','2','3','4','5','6','7','8','9', "
         "'-', '+', '$', '¢', '£', '¤', '¥', '৲', '৳', '฿', '៛', "
         "'₠', '₡', '₢', '₣', '₤', '₥', '₦', '₧', '₨', '₩', '₪', "
-        "'₫', '€', '₭', '₮', '₯', '₰', '₱', '﷼	', '﹩', '＄', "
+        "'₫', '€', '₭', '₮', '₯', '₰', '₱', '﹩', '＄', "
         "'￠', '￡', '￥', '￦'])",
         "inline_comment": "|input| input.starts_with(['#','-','/'])",
         "escaped_single_quote": "|input| input.starts_with(['E', 'e'])",
