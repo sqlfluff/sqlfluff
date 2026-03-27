47d7861d50f3e57aafe1bf94cee5bb4921715355
Peter Budai <peterbudai@hotmail.com>
2026-03-27 21:49:46 +0100

    Fix linting

--- test/fixtures/dialects/sqlite/create_virtual_table_check.sql
+++ test/fixtures/dialects/sqlite/create_virtual_table_check.sql
@@ -23,4 +23,3 @@ CREATE VIRTUAL TABLE IF NOT EXISTS sample_schema.email USING fts5(
 CREATE VIRTUAL TABLE ft USING fts5(a, b, c, content='');
 
 CREATE VIRTUAL TABLE ft2 USING fts5(a, content = main.emails);
-
