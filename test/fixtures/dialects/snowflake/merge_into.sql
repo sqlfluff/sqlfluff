6acf7938bafaee4051e4d00eb2cf01fe8864c60c
ninazacharia-toast <nina.zacharia@toasttab.com>
2026-04-15 15:51:09 -0400

    adjust merge

--- test/fixtures/dialects/snowflake/merge_into.sql
+++ test/fixtures/dialects/snowflake/merge_into.sql
@@ -10,3 +10,21 @@ merge into t1 using t2 on t1.t1key = t2.t2key
 
 merge into t1 using t2 on t1.t1key = t2.t2key
     when not matched and t2.marked = 1 then insert (marked) values (1);
+
+MERGE INTO target_table AS t
+USING source_table AS s
+    ON targ.id = src.id
+WHEN MATCHED THEN UPDATE ALL BY NAME
+WHEN NOT MATCHED THEN INSERT ALL BY NAME;
+
+MERGE INTO target_table AS t
+USING source_table AS s
+    ON t.id = s.id
+WHEN MATCHED THEN UPDATE ALL BY NAME
+WHEN NOT MATCHED THEN INSERT (id, name) VALUES (s.id, s.name);
+
+MERGE INTO target_table AS t
+USING source_table AS s
+    ON t.id = s.id
+WHEN MATCHED THEN UPDATE SET t.name = s.name
+WHEN NOT MATCHED THEN INSERT ALL BY NAME;
