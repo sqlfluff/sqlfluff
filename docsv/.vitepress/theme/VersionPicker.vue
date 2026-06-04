b6654d779c36697da9d1cb5ccf26af58bd9969c0
Alan Cruickshank <alan+git@designingoverload.com>
2026-06-04 16:08:30 +0100

    Improve versioned docs publishing

--- docsv/.vitepress/theme/VersionPicker.vue
+++ docsv/.vitepress/theme/VersionPicker.vue
@@ -92,6 +92,8 @@ function onChange(event: Event): void {
     window.location.href = version.path
 }
 
+const currentLabel = computed(() => selectedVersion.value?.label || 'Version')
+
 onMounted(async () => {
     currentPath.value = normalizeBase(window.location.pathname || route.path, '/')
 
@@ -104,23 +106,27 @@ onMounted(async () => {
 </script>
 
 <template>
-        <div v-if="versions.length" :class="['version-picker', { 'version-picker--inline': props.inline }]">
+    <div v-if="versions.length" :class="['version-picker', { 'version-picker--inline': props.inline }]">
         <label v-if="props.showLabel" class="version-picker__label" for="version-picker-select">Version</label>
-                <select
-                        id="version-picker-select"
-                        class="version-picker__select"
-                        :value="selectedVersion?.key"
-                        @change="onChange"
+        <div class="version-picker__control">
+            <span v-if="props.inline" class="version-picker__current">{{ currentLabel }}</span>
+            <select
+                id="version-picker-select"
+                class="version-picker__select"
+                :value="selectedVersion?.key"
+                :aria-label="props.showLabel ? undefined : 'Version'"
+                @change="onChange"
+            >
+                <option
+                    v-for="version in versions"
+                    :key="version.key"
+                    :value="version.key"
                 >
-                        <option
-                                v-for="version in versions"
-                                :key="version.key"
-                                :value="version.key"
-                        >
-                                {{ version.label }}
-                        </option>
-                </select>
+                    {{ version.label }}
+                </option>
+            </select>
         </div>
+    </div>
 </template>
 
 <style scoped>
@@ -141,19 +147,49 @@ onMounted(async () => {
     color: var(--vp-c-text-2);
 }
 
+.version-picker__control {
+    position: relative;
+    display: inline-flex;
+    align-items: center;
+}
+
+.version-picker__control::after {
+    position: absolute;
+    right: 0.7rem;
+    width: 0.45rem;
+    height: 0.45rem;
+    border-right: 1.5px solid var(--vp-c-text-2);
+    border-bottom: 1.5px solid var(--vp-c-text-2);
+    content: "";
+    pointer-events: none;
+    transform: translateY(-0.15rem) rotate(45deg);
+}
+
+.version-picker__current {
+    position: absolute;
+    left: 0.7rem;
+    color: var(--vp-c-text-1);
+    font-size: 0.875rem;
+    font-weight: 600;
+    pointer-events: none;
+}
+
 .version-picker__select {
-    min-width: 7rem;
-    padding: 0.35rem 2rem 0.35rem 0.65rem;
+    min-width: 7.5rem;
+    height: 2rem;
+    padding: 0.35rem 2rem 0.35rem 0.7rem;
     border: 1px solid var(--vp-c-divider);
-    border-radius: 999px;
+    border-radius: 6px;
+    appearance: none;
     background: var(--vp-c-bg-soft);
     color: var(--vp-c-text-1);
     font-size: 0.875rem;
+    line-height: 1.25rem;
 }
 
 .version-picker--inline .version-picker__select {
-    min-width: 5.5rem;
-    padding-right: 1.5rem;
+    min-width: 6.5rem;
+    color: transparent;
     background: transparent;
 }
 
