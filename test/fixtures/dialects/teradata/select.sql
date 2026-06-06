a29e5c56d26ba671b429ffa57ae20de485f58f48
henyanagar <henyanagar@gmail.com>
2026-06-06 19:34:15 +0300

    Fix Teradata date cast parsing (#7919)

--- test/fixtures/dialects/teradata/select.sql
+++ test/fixtures/dialects/teradata/select.sql
@@ -2,8 +2,9 @@ SELECT DATE;
 
 CREATE TABLE t1 (f1 DATE);
 
-SELECT DATE (FORMAT 'MMMbdd,bYYYY'); -- (CHAR(12), UC);  -- https://docs.teradata.com/r/S0Fw2AVH8ff3MDA0wDOHlQ/ryoeKJsEr22NqKahaktP5g
--- Disabled CHAR(12, UC) for now, see #1665
+-- https://docs.teradata.com/r/Enterprise_IntelliFlex_VMware/SQL-Data-Types-and-Literals/Data-Type-Formats-and-Format-Phrases/FORMAT-Phrase-and-DateTime-Formats/Examples-Querying-Using-Date-Formats
+SELECT DATE (FORMAT 'MMMbdd,bYYYY');
+SELECT DATE (FORMAT 'MMMbdd,bYYYY') (CHAR(12), UC);
 
 SELECT
     ADD_MONTHS(abandono.FEC_CIERRE_EST, -12) AS FEC_CIERRE_EST_ULT12,
