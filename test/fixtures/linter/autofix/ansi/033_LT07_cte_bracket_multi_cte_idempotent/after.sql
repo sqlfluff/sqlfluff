c95242a1296dbad7814a6374c4c1f4cc4654b33d
Vincent Gao <gaobing1230@gmail.com>
2026-07-04 23:12:24 +0200

    Fix LT07 non-idempotent fix for CTE brackets that become multi-line

---
+++ test/fixtures/linter/autofix/ansi/033_LT07_cte_bracket_multi_cte_idempotent/after.sql
@@ -0,0 +1,18 @@
+WITH foo AS (
+    SELECT
+        a,
+        b
+    FROM t1
+),
+
+bar AS (
+    SELECT
+        c,
+        d
+    FROM t2
+)
+
+SELECT
+    foo.a,
+    bar.c
+FROM foo CROSS JOIN bar
