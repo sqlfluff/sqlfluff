45178c1590efa1623877d9b09d0e7defb2164ff4
Кирилл Ветров <vetrovk@gmail.com>
2026-07-04 16:52:34 +0300

    fix: parse oracle sqlplus line commands

---
+++ test/fixtures/dialects/oracle/sqlplus_commands.sql
@@ -0,0 +1,6 @@
+PROMPT this is an Oracle SQL newline delimited prompt statement
+ACCEPT var
+ACC short_var
+REMARK this is a SQL*Plus remark
+REM this is a SQL*Plus remark abbreviation
+SELECT job_id FROM employees;
