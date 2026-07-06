062170229440943ce24312e03310c85042f063a3
Кирилл Ветров <vetrovk@gmail.com>
2026-07-06 07:01:51 +0300

    fix: preserve sqlplus command text parsing

--- src/sqlfluff/dialects/dialect_oracle.py
+++ src/sqlfluff/dialects/dialect_oracle.py
@@ -124,7 +124,8 @@ oracle_dialect.insert_lexer_matchers(
             "accept_command",
             (
                 r"[aA][cC][cC](?:[eE][pP][tT])?"
-                r"[^\S\r\n]+[^;(\r\n)]*((?=\n)|(?=\r\n)|$)"
+                r"[^\S\r\n]+(?![^(\r\n)]*;[^\S\r\n]*((?=\n)|(?=\r\n)|$))"
+                r"[^(\r\n)]*((?=\n)|(?=\r\n)|$)"
             ),
             CommentSegment,
         ),
@@ -132,7 +133,9 @@ oracle_dialect.insert_lexer_matchers(
             "remark_command",
             (
                 r"[rR][eE][mM](?:[aA][rR][kK])?"
-                r"[^\S\r\n]+[^;(\r\n)]*((?=\n)|(?=\r\n)|$)"
+                r"(?:[^\S\r\n]+"
+                r"(?![^(\r\n)]*;[^\S\r\n]*((?=\n)|(?=\r\n)|$))"
+                r"[^(\r\n)]*)?((?=\n)|(?=\r\n)|$)"
             ),
             CommentSegment,
         ),
--- test/fixtures/dialects/oracle/sqlplus_commands.sql
+++ test/fixtures/dialects/oracle/sqlplus_commands.sql
@@ -1,7 +1,10 @@
 PROMPT this is an Oracle SQL newline delimited prompt statement
 ACCEPT var
 ACC short_var
+ACCEPT myvar PROMPT 'Enter value; then press Enter'
+REMARK
 REMARK this is a SQL*Plus remark
+REM
 REM this is a SQL*Plus remark abbreviation
 SELECT job_id FROM employees;
 
