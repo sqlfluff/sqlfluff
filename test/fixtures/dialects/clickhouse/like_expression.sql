d7ab7ea3c3fff2d450587d725c9dc944d0b0119d
Kurgansky Andrey <andrewkurg@gmail.com>
2026-07-28 00:33:27 +0300

    test(clickhouse): add fixtures for regexp, like and ilike operators

---
+++ test/fixtures/dialects/clickhouse/like_expression.sql
@@ -0,0 +1,80 @@
+-- REGEXP
+SELECT 'string' REGEXP '[a-zAZ]';
+
+SELECT
+CASE
+    WHEN field = 0 THEN 'false'
+    ELSE 'true'
+END
+FROM (
+   SELECT CAST(('string' REGEXP '[0-9]') AS String) REGEXP '[0123]' AS field
+) AS foo;
+
+SELECT name
+FROM system.columns
+WHERE database REGEXP '^sys.*$';
+
+-- LIKE
+SELECT 'string' LIKE 's%';
+
+SELECT
+CASE
+    WHEN field = 0 THEN 'false'
+    ELSE 'true'
+END
+FROM (
+   SELECT CAST(('string' LIKE 's%') AS String) LIKE '%0%' AS field
+) AS foo;
+
+SELECT name
+FROM system.columns
+WHERE database LIKE 'sys%';
+
+-- NOT LIKE
+SELECT 'string' NOT LIKE 's%';
+
+SELECT
+CASE
+    WHEN field = 0 THEN 'false'
+    ELSE 'true'
+END
+FROM (
+   SELECT CAST(('string' LIKE 's%') AS String) NOT LIKE '%0%' AS field
+) AS foo;
+
+SELECT name
+FROM system.columns
+WHERE database NOT LIKE 'sys%';
+
+-- ILIKE
+SELECT 'string' ILIKE 'S%';
+
+SELECT
+CASE
+    WHEN field = 0 THEN 'false'
+    ELSE 'true'
+END
+FROM (
+   SELECT CAST(('string' ILIKE 'S%') AS String) LIKE '%0%' AS field
+) AS foo;
+
+SELECT name
+FROM system.columns
+WHERE database ILIKE 'sYs%';
+
+
+-- NOT ILIKE
+SELECT 'string' NOT ILIKE 'S%';
+
+SELECT
+CASE
+    WHEN field = 0 THEN 'false'
+    ELSE 'true'
+END
+FROM (
+   SELECT CAST(('string' NOT ILIKE 'S%') AS String) LIKE '%0%' AS field
+) AS foo;
+
+SELECT name
+FROM system.columns
+WHERE database NOT ILIKE 'sYs%';
