a36f5f2b1c66237e6562550ea7c899870d1f9ef6
AndrewKurg <48150548+AndrewKurg@users.noreply.github.com>
2026-07-28 12:01:09 +0300

    feat(clickhouse): add support for REGEXP operator (#8235)

--- src/sqlfluff/dialects/dialect_clickhouse.py
+++ src/sqlfluff/dialects/dialect_clickhouse.py
@@ -339,6 +339,27 @@ clickhouse_dialect.replace(
             optional=True,
         ),
     ),
+    LikeGrammar=OneOf("LIKE", "ILIKE", "REGEXP"),
+    LikeExpressionGrammar=Sequence(
+        OneOf(
+            Sequence(
+                Ref.keyword("NOT", optional=True),
+                # REGEXP does not support the NOT keyword
+                Ref("LikeGrammar", exclude=Ref.keyword("REGEXP")),
+                Ref("Expression_A_Grammar"),
+                Sequence(
+                    "ESCAPE",
+                    Ref("Tail_Recurse_Expression_A_Grammar"),
+                    optional=True,
+                ),
+            ),
+            # REGEXP does not support the ESCAPE keyword
+            Sequence(
+                "REGEXP",
+                Ref("Tail_Recurse_Expression_A_Grammar"),
+            ),
+        ),
+    ),
 )
 
 # Set the datetime units
