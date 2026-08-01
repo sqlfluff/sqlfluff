a94fd13edf2826854ae3b1a5d58277f226a2356b
Guan Tong <tguan@gmail.com>
2026-08-02 02:39:31 +0800

    fix: align multiline segment endpoints

--- test/rules/std_LT01_alignment_test.py
+++ test/rules/std_LT01_alignment_test.py
@@ -46,3 +46,35 @@ align_scope = bracketed
     )
 
     assert sqlfluff.fix(sql, config=config) == expected
+
+
+def test_lt01_alignment_uses_multiline_segment_endpoint() -> None:
+    """A multiline segment ending beside an alias should set the baseline."""
+    sql = (
+        "SELECT\n"
+        "    'alpha\n"
+        "a_long_tail' AS first_column,\n"
+        "    c AS second_column\n"
+        "FROM foo\n"
+    )
+    expected = (
+        "SELECT\n"
+        "    'alpha\n"
+        "a_long_tail' AS first_column,\n"
+        "    c        AS second_column\n"
+        "FROM foo\n"
+    )
+    config = FluffConfig.from_string(
+        """
+[sqlfluff]
+dialect = ansi
+rules = LT01
+
+[sqlfluff:layout:type:alias_expression]
+spacing_before = align
+align_within = select_clause
+align_scope = bracketed
+"""
+    )
+
+    assert sqlfluff.fix(sql, config=config) == expected
