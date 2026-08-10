f878214cf3b1a74c850e75b305b5740e78ecf05c
AndrewKurg <48150548+AndrewKurg@users.noreply.github.com>
2026-08-10 09:45:10 +0300

    fix(clickhouse): a couple of fixes for the `CREATE DICTIONARY` statement (#8298)

--- test/fixtures/dialects/clickhouse/create_dictionary.sql
+++ test/fixtures/dialects/clickhouse/create_dictionary.sql
@@ -15,3 +15,59 @@ LAYOUT(HASHED())
 LIFETIME(MIN 300 MAX 600)
 SETTINGS(min_idle_time = 10, max_block_size = 10000)
 COMMENT 'Country dictionary with defaults and expressions';
+
+-- https://fiddle.clickhouse.com/a82027e0-c8b1-4240-a792-e536cabb883d
+-- SOURCE, LAYOUT, LIFETIME
+CREATE DICTIONARY test_db.test_dict (
+  id UInt64,
+  id2 UInt64,
+  val String
+)
+PRIMARY KEY `id`, id2
+SOURCE(NULL())
+LAYOUT(COMPLEX_KEY_HASHED())
+LIFETIME(100);
+
+-- SOURCE, LAYOUT, LIFETIME, RANGE, COMMENT
+CREATE OR REPLACE DICTIONARY test_db.test_dict (
+  id UInt64,
+  id2 UInt64,
+  val String,
+  discount_start_date Date,
+  discount_end_date Date
+)
+PRIMARY KEY (`id`, "id2")
+LIFETIME(100)
+LAYOUT(COMPLEX_KEY_RANGE_HASHED())
+SOURCE(NULL())
+RANGE(MIN "discount_start_date" MAX discount_end_date)
+COMMENT 'comment';
+
+-- SOURCE, LAYOUT, LIFETIME, SETTINGS
+CREATE OR REPLACE DICTIONARY test_db.test_dict (
+  id UInt64,
+  id2 UInt64,
+  val String
+)
+PRIMARY KEY id, id2
+SETTINGS(min_idle_time = 10)
+LAYOUT(COMPLEX_KEY_HASHED())
+SOURCE(CLICKHOUSE(HOST 'localhost' PORT 9000 USER 'default' PASSWORD '' QUERY $$
+    SELECT 1 AS id, 1 AS id2, 'test' AS val
+$$))
+LIFETIME(100);
+
+-- SOURCE, LAYOUT, LIFETIME, RANGE, SETTINGS
+CREATE OR REPLACE DICTIONARY test_db.test_dict (
+  id UInt64,
+  id2 UInt64,
+  val String,
+  discount_start_date Date,
+  discount_end_date Date
+)
+PRIMARY KEY (id, id2)
+LIFETIME(MIN 10 MAX 100)
+LAYOUT(COMPLEX_KEY_RANGE_HASHED())
+SETTINGS(min_idle_time = 10)
+RANGE(MIN `discount_start_date` MAX `discount_end_date`)
+SOURCE(NULL());
