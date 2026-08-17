f3ca9e4bf34ba07fbf145066b2b880558635ca37
arose26 <axelrose20262026@gmail.com>
2026-08-17 11:34:23 -0400

    DuckDB: parse INSTALL and LOAD statements
    INSTALL had no grammar, and LOAD was inherited from Postgres where it
    takes a quoted filename rather than a bare extension name.

---
+++ test/fixtures/dialects/duckdb/install_and_load.sql
@@ -0,0 +1,11 @@
+INSTALL spatial;
+
+FORCE INSTALL spatial;
+
+INSTALL h3 FROM community;
+
+FORCE INSTALL h3 FROM community;
+
+INSTALL custom_extension FROM 'https://my-custom-extension-repository';
+
+LOAD spatial;
