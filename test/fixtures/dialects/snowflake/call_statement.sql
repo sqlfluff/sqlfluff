0025ad7cecf2b9cdde8decdd57038db0bffa9600
askarshabdanov1-collab <askarshabdanov1@gmail.com>
2026-08-18 21:03:32 +0300

    Snowflake: support INTO variable clause in CALL statements (#8364)

--- test/fixtures/dialects/snowflake/call_statement.sql
+++ test/fixtures/dialects/snowflake/call_statement.sql
@@ -3,3 +3,18 @@ CALL sv_proc1('Manitoba', 127.4);
 
 SET Variable1 = 49;
 CALL sv_proc2($Variable1);
+
+CALL sv_proc1('Manitoba', 127.4) INTO :ret1;
+CALL sv_proc1('Manitoba', 127.4) INTO ret1;
+CALL my_db.my_schema.sv_proc1() INTO :result;
+CALL my_db.my_schema.sv_proc1() INTO result;
+CALL sv_proc1(a => 1, b => 'x') INTO :ret1;
+CALL sv_proc1(a => 1, b => 'x') INTO ret1;
+
+DECLARE
+    myvar VARCHAR DEFAULT NULL;
+BEGIN
+    CALL myproc('arg') INTO :myvar;
+    CALL myproc('arg') INTO myvar;
+    RETURN :myvar;
+END;
