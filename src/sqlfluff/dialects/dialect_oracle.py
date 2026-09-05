68d49be015e8f115dbaf8c73036850c3914a3b2c
sambai-dev <yozamono@gmail.com>
2026-09-05 17:22:34 +1200

    fix(parser): reject bare SELECT expressions

--- src/sqlfluff/dialects/dialect_oracle.py
+++ src/sqlfluff/dialects/dialect_oracle.py
@@ -255,11 +255,14 @@ oracle_dialect.add(
     PivotInGrammar=Sequence(
         "IN",
         Bracketed(
-            Delimited(
-                Sequence(
-                    Ref("Expression_D_Grammar"),
-                    Ref("AliasExpressionSegment", optional=True),
-                )
+            OneOf(
+                Ref("SelectStatementSegment"),
+                Delimited(
+                    Sequence(
+                        Ref("Expression_D_Grammar"),
+                        Ref("AliasExpressionSegment", optional=True),
+                    )
+                ),
             )
         ),
     ),
@@ -795,6 +798,7 @@ oracle_dialect.replace(
                     # We're using the expression segment here rather than the grammar so
                     # that in the parsed structure we get nested elements.
                     Ref("ExpressionSegment"),
+                    Ref("SelectStatementExpressionSegment"),
                     Ref("SelectableGrammar"),
                     Delimited(
                         Ref(
@@ -809,8 +813,6 @@ oracle_dialect.replace(
                 ),
                 parse_mode=ParseMode.GREEDY,
             ),
-            # Allow potential select statement without brackets
-            Ref("SelectStatementSegment"),
             Ref("LiteralGrammar"),
             Ref("IntervalExpressionSegment"),
             Ref("TypedStructLiteralSegment"),
@@ -828,7 +830,7 @@ oracle_dialect.replace(
                 Bracketed(Delimited(Ref("ExpressionSegment"))),
             ),
             Sequence(
-                Ref("DatatypeSegment"),
+                Ref("DatatypeSegment", exclude=Ref.keyword("SELECT")),
                 # Don't use the full LiteralGrammar here
                 # because only some of them are applicable.
                 # Notably we shouldn't use QualifiedNumericLiteralSegment
