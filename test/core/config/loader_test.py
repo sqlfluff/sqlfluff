f2416a9f6b4636d7cb780e8b150582fc196ed209
Sam Mosleh <sam.mosleh.d@gmail.com>
2026-09-09 11:42:56 +0400

    fix: use absolute paths in XDG_CONFIG_HOME

--- test/core/config/loader_test.py
+++ test/core/config/loader_test.py
@@ -38,7 +38,9 @@ config_a = {
 @pytest.fixture
 def mock_xdg_home(monkeypatch):
     """Sets the XDG_CONFIG_HOME variable."""
-    monkeypatch.setenv("XDG_CONFIG_HOME", "~/.config/my/special/path")
+    monkeypatch.setenv(
+        "XDG_CONFIG_HOME", os.path.expanduser("~/.config/my/special/path")
+    )
 
 
 def test__config__load_file_dir():
