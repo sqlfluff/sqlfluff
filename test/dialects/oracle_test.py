21efaa2941ee63020b3ad2d01e5e359797ae1cc9
adhavan18 <sk.tamiladhavan@gmail.com>
2026-09-24 09:04:36 +0530

    test(oracle): cover incomplete merge delete

--- test/dialects/oracle_test.py
+++ test/dialects/oracle_test.py
@@ -35,6 +35,14 @@ def test_merge_insert_requires_values_clause() -> None:
     )
 
 
+def test_merge_delete_requires_where_clause() -> None:
+    """Oracle MERGE delete clauses cannot omit WHERE."""
+    assert _violations(
+        "MERGE INTO target t USING source s ON (t.id = s.id) "
+        "WHEN MATCHED THEN UPDATE SET t.value = s.value DELETE;"
+    )
+
+
 # OraclePhysicalAttributesSegment - table-level attributes
 @pytest.mark.parametrize(
     "sql",
